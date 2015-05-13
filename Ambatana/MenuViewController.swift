17c2b5ed13cd28ff09f2ee11d39c0297741aab52
Albert Hernández López <albert@letgo.com>
2015-05-13 16:26:24 +0200

    updated copys for contact to contact info+ios@letgo.com

--- Ambatana/MenuViewController.swift
+++ Ambatana/MenuViewController.swift
@@ -216,7 +216,7 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
         if MFMailComposeViewController.canSendMail() {
             let mailComposerController: MFMailComposeViewController! = MFMailComposeViewController()
             mailComposerController.mailComposeDelegate = self
-            mailComposerController.setToRecipients(["barbara@letgo.com"])
+            mailComposerController.setToRecipients(["info+ios@letgo.com"])
             mailComposerController.setSubject(translate("feedback_letgo_user"))
             mailComposerController.setMessageBody(translate("type_your_message_here"), isHTML: true)
             self.presentViewController(mailComposerController, animated: true, completion: nil)
