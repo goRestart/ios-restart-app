e88ef237ee938297b5041dc082f1f0ca7969e0da
Albert Hernández López <albert@letgo.com>
2015-06-26 14:18:49 +0200

    ABIOS-261 TabBarToolTip files created

---
+++ Ambatana/src/ui/TabBarToolTip.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarToolTip.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 26/06/15.
+//  Copyright (c) 2015 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class TabBarToolTip: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
