c57250c68417d9096d72c3edbd74a96a98b93074
Albert Hernández López <albert@letgo.com>
2015-06-30 09:48:25 +0200

    updated pods LGCoreKit to 0.6.1

--- Pods/LGCoreKit/LGCoreKit/Model/ProductCategory.swift
+++ Pods/LGCoreKit/LGCoreKit/Model/ProductCategory.swift
@@ -9,5 +9,5 @@
 @objc public enum ProductCategory: Int {
     case Electronics = 1, CarsAndMotors = 2, SportsLeisureAndGames = 3, HomeAndGarden = 4, MoviesBooksAndMusic = 5, FashionAndAccesories = 6, BabyAndChild = 7, Other = 8
     
-    static func allValues() -> [ProductCategory] { return [.Electronics, .CarsAndMotors, .SportsLeisureAndGames, .HomeAndGarden, .MoviesBooksAndMusic, .FashionAndAccesories, .BabyAndChild, .Other] }
+    public static func allValues() -> [ProductCategory] { return [.Electronics, .CarsAndMotors, .SportsLeisureAndGames, .HomeAndGarden, .MoviesBooksAndMusic, .FashionAndAccesories, .BabyAndChild, .Other] }
 }
