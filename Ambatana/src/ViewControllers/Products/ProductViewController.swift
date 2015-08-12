1583fb010666646b2addd622f7301280ea52cb73
Albert Hernández López <albert@letgo.com>
2015-08-12 10:29:47 +0200

    ABIOS-325 created new product (detail) view controller with its view model

---
+++ Ambatana/src/ViewControllers/Products/ProductViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProductViewController.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 12/08/15.
+//  Copyright (c) 2015 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ambatana/src/ViewControllers/Products/ProductViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductViewModel.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 12/08/15.
+//  Copyright (c) 2015 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewModel: BaseViewModel {
+   
+}
