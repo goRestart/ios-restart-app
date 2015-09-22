dac4875c45accad2d0641240edf7ee29c05e75de
Dídac <didac@letgo.com>
2015-09-22 19:18:21 +0200

    ABIOS-407 Kahuna updated from 2.0.3 to 2.0.4

--- Ambatana/src/Manager/Tracking/KahunaTracker.swift
+++ Ambatana/src/Manager/Tracking/KahunaTracker.swift
@@ -52,9 +52,9 @@ public class KahunaTracker: Tracker {
     }
     
     public func trackEvent(event: TrackerEvent) {
-        if event.shouldTrack {
-    
-        }
+//        if event.shouldTrack {
+//    
+//        }
     }
     
     public func updateCoordinates() {
