7f2ab07f56702181724595b1154b7835e905be54
Albert Hernández López <albert@letgo.com>
2015-10-02 14:14:41 +0200

    ABIOS-478 modified new help URLs

--- Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
+++ Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
@@ -8,26 +8,45 @@
 
 public class HelpViewModel: BaseViewModel {
    
-    private static let availableHelpLangs = ["de", "el", "en", "es", "fi", "fr", "hu", "it", "nb", "nl", "ru", "sv", "tr", "vi"]
+    private static let langsCountryDict = [
+        "de":"de",  // http://de.letgo.com/de/help_app
+        "el":"gr",  // http://gr.letgo.com/el/help_app
+        "en":"us",  // http://us.letgo.com/en/help_app
+        "es":"es",  // http://es.letgo.com/es/help_app
+        "fi":"fi",  // http://fi.letgo.com/fi/help_app
+        "fr":"fr",  // http://fr.letgo.com/fr/help_app
+        "hu":"hu",  // http://hu.letgo.com/hu/help_app
+        "it":"it",  // http://it.letgo.com/it/help_app
+        "nb":"no",  // http://no.letgo.com/nb/help_app
+        "nl":"nl",  // http://nl.letgo.com/nl/help_app
+        "ru":"ru",  // http://ru.letgo.com/ru/help_app
+        "sv":"se",  // http://se.letgo.com/sv/help_app
+        "tr":"tr",  // http://tr.letgo.com/tr/help_app
+        "vi":"vn",  // http://vn.letgo.com/vi/help_app
+        "pt":"pt",  // http://pt.letgo.com/pt/help_app
+        "ko":"kr"   // http://kr.letgo.com/ko/help_app
+    ]
     private static let defaultLang = "en"
+    private static let defaultCountry = "us"
     
     public var url: NSURL? {
         let preferredLanguages = NSLocale.preferredLanguages()
-        var language: String = HelpViewModel.defaultLang
+        
+        var language = HelpViewModel.defaultLang
+        var country = HelpViewModel.defaultCountry
+        
         if !preferredLanguages.isEmpty {
             language = preferredLanguages[0] as? String ?? HelpViewModel.defaultLang
             
             // In case it's like es-ES, just take the first "es"
             let components = language.componentsSeparatedByString("-")
-            if components.count > 0 {
-                language = components[0]
-            }
-            // If the language is not supported the set English as default
-            if !contains(HelpViewModel.availableHelpLangs, language) {
+            language = components.count > 0 ? components[0] : HelpViewModel.defaultLang
+            if !contains(HelpViewModel.langsCountryDict.keys, language) {
                 language = HelpViewModel.defaultLang
             }
+            country = HelpViewModel.langsCountryDict[language] ?? HelpViewModel.defaultCountry
         }
-        let urlString = String(format: Constants.helpURL, arguments: [language])
+        let urlString = String(format: Constants.helpURL, arguments: [country, language])
         return NSURL(string: urlString)
     }
 }
