4c50bb7b1bd4b9c447adb24bf7e34e65b981b4dc
Eli Kohen <eli.kohen@letgo.com>
2015-12-01 17:02:42 +0100

    ABIOS-596 Created A/B test for product cell details

--- Ambatana/src/Manager/ABTesting/ABTests.swift
+++ Ambatana/src/Manager/ABTesting/ABTests.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 public struct ABTests {
+    
+    public static let mainProductsJustImages = OptimizelyABLiveVariable.boolVariable("MainProductsJustImages", boolValue: false)
     public static let navBarTintColor = OptimizelyABLiveVariable.colorVariable("NavBarTintColor", colorValue: StyleHelper.red)
     public static let productsWithinFilterEnabled = OptimizelyABLiveVariable.boolVariable("ProductsWithinFilter", boolValue: true)
 
-    public static let allValues = [ABTests.navBarTintColor, ABTests.productsWithinFilterEnabled]
+    public static let allValues = [ABTests.mainProductsJustImages, ABTests.navBarTintColor, ABTests.productsWithinFilterEnabled]
 }
