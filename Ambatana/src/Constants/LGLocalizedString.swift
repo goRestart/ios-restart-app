01465f51d4f8b452e9509d2df7270a43ec8b050a
Albert Hernández López <albert@letgo.com>
2016-01-05 17:16:27 +0100

    ABIOS-808 updated strings, erased unused icon, applied always rendering for check w circle icon to be able to tint it

--- Ambatana/src/Constants/LGLocalizedString.swift
+++ Ambatana/src/Constants/LGLocalizedString.swift
@@ -408,12 +408,28 @@ struct LGLocalizedString {
 		return NSLocalizedString("custom_permission_chat_title", comment: "")
 	}
 	
-	static var customPermissionListMessage: String {
-		return NSLocalizedString("custom_permission_list_message", comment: "")
+    static var customPermissionListMessage: String {
+        return NSLocalizedString("custom_permission_list_message_A", comment: "")
+    }
+    
+	static var customPermissionListMessageA: String {
+		return NSLocalizedString("custom_permission_list_message_A", comment: "")
 	}
+    
+    static var customPermissionListMessageB: String {
+        return NSLocalizedString("custom_permission_list_message_B", comment: "")
+    }
 	
 	static var customPermissionListTitle: String {
-		return NSLocalizedString("custom_permission_list_title", comment: "")
+		return NSLocalizedString("custom_permission_list_title_A", comment: "")
+    }
+    
+	static var customPermissionListTitleA: String {
+		return NSLocalizedString("custom_permission_list_title_A", comment: "")
+	}
+	
+	static var customPermissionListTitleB: String {
+		return NSLocalizedString("custom_permission_list_title_B", comment: "")
 	}
 	
 	static var customPermissionSellMessage: String {
