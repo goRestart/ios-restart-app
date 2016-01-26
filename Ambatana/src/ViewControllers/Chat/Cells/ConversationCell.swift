8278a37e07dfe47f4c6d84138eb476ac623f05f1
Isaac Roldán <isaac@letgo.com>
2016-01-26 16:22:19 +0100

    Add new style in conversation cells for deactivated accounts

--- Ambatana/src/ViewControllers/Chat/Cells/ConversationCell.swift
+++ Ambatana/src/ViewControllers/Chat/Cells/ConversationCell.swift
@@ -75,32 +75,41 @@ public class ConversationCell: UITableViewCell {
         
         // time / deleted
         var timeLabelValue: String = ""
-
-        switch chat.product.status {
-        case .Deleted:
-            timeLabelValue = LGLocalizedString.commonProductDeleted
-            timeLabel.font = StyleHelper.conversationProductDeletedFont
-            timeLabel.textColor = StyleHelper.conversationProductDeletedColor
-            statusImageView.image = UIImage(named: "icAlertCopy")
-            statusImageView.hidden = false
-            separationStatusImageToTimeLabel.constant = 4
-        case .Pending, .Approved, .Discarded:
-            if let lastUpdated = chat.updatedAt {
-                timeLabelValue = lastUpdated.relativeTimeString()
-            }
-            statusImageView.hidden = true
+        
+        if chat.forbidden {
+            timeLabelValue = LGLocalizedString.accountDeactivated
             timeLabel.font = StyleHelper.conversationTimeFont
-            timeLabel.textColor = StyleHelper.conversationTimeColor
-
+            timeLabel.textColor = StyleHelper.conversationAccountDeactivatedColor
+            statusImageView.hidden = true
             separationStatusImageToTimeLabel.constant = -statusImageView.frame.width
-        case .Sold, .SoldOld:
-            timeLabelValue = LGLocalizedString.commonProductSold
-            timeLabel.font = StyleHelper.conversationProductSoldFont
-            timeLabel.textColor = StyleHelper.conversationProductSoldColor
-            statusImageView.image = UIImage(named: "oval45")
-            statusImageView.hidden = false
-            separationStatusImageToTimeLabel.constant = 4
+        } else {
+            switch chat.product.status {
+            case .Deleted:
+                timeLabelValue = LGLocalizedString.commonProductDeleted
+                timeLabel.font = StyleHelper.conversationProductDeletedFont
+                timeLabel.textColor = StyleHelper.conversationProductDeletedColor
+                statusImageView.image = UIImage(named: "icAlertCopy")
+                statusImageView.hidden = false
+                separationStatusImageToTimeLabel.constant = 4
+            case .Pending, .Approved, .Discarded:
+                if let lastUpdated = chat.updatedAt {
+                    timeLabelValue = lastUpdated.relativeTimeString()
+                }
+                statusImageView.hidden = true
+                timeLabel.font = StyleHelper.conversationTimeFont
+                timeLabel.textColor = StyleHelper.conversationTimeColor
+                
+                separationStatusImageToTimeLabel.constant = -statusImageView.frame.width
+            case .Sold, .SoldOld:
+                timeLabelValue = LGLocalizedString.commonProductSold
+                timeLabel.font = StyleHelper.conversationProductSoldFont
+                timeLabel.textColor = StyleHelper.conversationProductSoldColor
+                statusImageView.image = UIImage(named: "oval45")
+                statusImageView.hidden = false
+                separationStatusImageToTimeLabel.constant = 4
+            }
         }
+        
         
         timeLabel.text = timeLabelValue
         
