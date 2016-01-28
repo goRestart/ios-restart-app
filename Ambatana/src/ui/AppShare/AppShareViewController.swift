45317ee49e993039cf460d843541cee993c858c7
Eli Kohen <eli.kohen@letgo.com>
2016-01-28 16:20:56 +0100

    ABIOS-866 Prepared actions on AppShareviewController

--- Ambatana/src/ui/AppShare/AppShareViewController.swift
+++ Ambatana/src/ui/AppShare/AppShareViewController.swift
@@ -9,6 +9,36 @@
 import UIKit
 
 class AppShareViewController: UIViewController {
+    @IBOutlet weak var contentContainer: UIView!
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+
+    @IBOutlet weak var inviteFBMessengerBtn: UIButton!
+    @IBOutlet weak var inviteFBMessengerIcon: UIImageView!
+    @IBOutlet weak var inviteFBMessengerHeight: NSLayoutConstraint!
+    @IBOutlet weak var inviteFBMessengerTop: NSLayoutConstraint!
+
+    @IBOutlet weak var inviteWhatsappBtn: UIButton!
+    @IBOutlet weak var inviteWhatsappIcon: UIImageView!
+    @IBOutlet weak var inviteWhatsappHeight: NSLayoutConstraint!
+    @IBOutlet weak var inviteWhatsappTop: NSLayoutConstraint!
+
+    @IBOutlet weak var inviteEmailBtn: UIButton!
+    @IBOutlet weak var closeBtn: UIButton!
+
+    private let socialMessage: SocialMessage
+
+    init(socialMessage: SocialMessage) {
+        self.socialMessage = socialMessage
+        super.init(nibName: "AppShareViewController", bundle: nil)
+        modalPresentationStyle = .OverCurrentContext
+        modalTransitionStyle = .CrossDissolve
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +46,46 @@ class AppShareViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    // MARK: - Actions
+
+    @IBAction func onInviteFBMessenger(sender: AnyObject) {
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onInviteWhatsapp(sender: AnyObject) {
     }
-    */
 
+    @IBAction func onInviteEmail(sender: AnyObject) {
+    }
+
+    @IBAction func onClose(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+    // MARK: - Private methods
+
+    private func setupUI() {
+        contentContainer.layer.cornerRadius = StyleHelper.defaultCornerRadius
+
+        inviteFBMessengerBtn.setCustomButtonStyle()
+        inviteWhatsappBtn.setCustomButtonStyle()
+        inviteEmailBtn.setCustomButtonStyle()
+
+        //TODO: i18n
+
+        if !SocialHelper.canShareInFBMessenger() {
+            inviteFBMessengerHeight.constant = 0
+            inviteFBMessengerTop.constant = 0
+            inviteFBMessengerBtn.hidden = true
+            inviteFBMessengerIcon.hidden = true
+        }
+
+        if !SocialHelper.canShareInWhatsapp() {
+            inviteWhatsappHeight.constant = 0
+            inviteWhatsappTop.constant = 0
+            inviteWhatsappBtn.hidden = true
+            inviteWhatsappIcon.hidden = true
+        }
+    }
 }
