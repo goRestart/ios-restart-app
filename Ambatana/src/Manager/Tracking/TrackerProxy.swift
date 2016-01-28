e483d23dc7df922b62be61e004fa8bb49174d119
Albert Hernández López <albert@letgo.com>
2016-01-28 12:24:56 +0100

    ABIOS-909 a bit of cleanup in TrackerProxy, made loops to look a bit more functionally 😎

--- Ambatana/src/Manager/Tracking/TrackerProxy.swift
+++ Ambatana/src/Manager/Tracking/TrackerProxy.swift
@@ -10,23 +10,21 @@ import LGCoreKit
 import CoreLocation
 
 public class TrackerProxy: Tracker {
-
-    // Constants
-    // TODO : uncomment GANTracker once is working
     // Amplitude must be initiated before OptimizelyTracker or the integration between them won't work
     private static let defaultTrackers: [Tracker] = [AmplitudeTracker(), AppsflyerTracker(), FacebookTracker(),
         GoogleConversionTracker(), NanigansTracker(), KahunaTracker(), OptimizelyTracker(), CrashlyticsTracker(),
         GANTracker(), AdjustTracker()]
 
-    // iVars
+    public static let sharedInstance = TrackerProxy()
     public var trackers: [Tracker] = []
 
-    public static let sharedInstance = TrackerProxy()
+
+    // MARK: - Lifecycle
 
     public init(trackers: [Tracker] = TrackerProxy.defaultTrackers) {
         self.trackers = trackers
 
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateCoordinatesFromNotification:",
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "locationUpdate:",
             name: LocationManager.Notification.LocationUpdate.rawValue, object: nil)
 
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "sessionUpdate:",
@@ -35,63 +33,50 @@ public class TrackerProxy: Tracker {
             name: SessionManager.Notification.Logout.rawValue, object: nil)
     }
 
+
     // MARK: - Tracker
 
     public func application(application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) {
-            for tracker in trackers {
-                tracker.application(application, didFinishLaunchingWithOptions: launchOptions)
-            }
+            trackers.forEach { $0.application(application, didFinishLaunchingWithOptions: launchOptions) }
     }
 
     public func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?,
         annotation: AnyObject?) {
-            for tracker in trackers {
-                tracker.application(application, openURL: url, sourceApplication: sourceApplication,
-                    annotation: annotation)
+            trackers.forEach {
+                $0.application(application, openURL: url, sourceApplication: sourceApplication,
+                annotation: annotation)
             }
     }
 
     public func applicationDidEnterBackground(application: UIApplication) {
-        for tracker in trackers {
-            tracker.applicationDidEnterBackground(application)
-        }
+        trackers.forEach { $0.applicationDidEnterBackground(application) }
     }
 
     public func applicationWillEnterForeground(application: UIApplication) {
-        for tracker in trackers {
-            tracker.applicationWillEnterForeground(application)
-        }
-
+        trackers.forEach { $0.applicationWillEnterForeground(application) }
     }
 
     public func applicationDidBecomeActive(application: UIApplication) {
-        for tracker in trackers {
-            tracker.applicationDidBecomeActive(application)
-        }
+        trackers.forEach { $0.applicationDidBecomeActive(application) }
     }
 
     public func setUser(user: MyUser?) {
-        for tracker in trackers {
-            tracker.setUser(user)
-        }
+        trackers.forEach { $0.setUser(user) }
     }
 
     public func trackEvent(event: TrackerEvent) {
-        for tracker in trackers {
-            tracker.trackEvent(event)
-        }
+        trackers.forEach { $0.trackEvent(event) }
     }
 
     public func updateCoordinates() {
-        for tracker in trackers {
-            tracker.updateCoordinates()
-        }
+        trackers.forEach { $0.updateCoordinates() }
     }
 
-    // MARK: private methods
 
-    private dynamic func updateCoordinatesFromNotification(notification: NSNotification) {
+    // MARK: Private methods
+
+    private dynamic func locationUpdate(_: NSNotification) {
         updateCoordinates()
     }
 
