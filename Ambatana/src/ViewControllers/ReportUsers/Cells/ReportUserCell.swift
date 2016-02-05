ea1ddfe79eac0a1a3f733d92fb304c30f424dc0c
Eli Kohen <eli.kohen@letgo.com>
2016-02-05 13:00:24 +0100

    ABIOS-684 finished ReportUserCell and added all required assets

--- Ambatana/src/ViewControllers/ReportUsers/Cells/ReportUserCell.swift
+++ Ambatana/src/ViewControllers/ReportUsers/Cells/ReportUserCell.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ReportUserCell: UICollectionViewCell {
+class ReportUserCell: UICollectionViewCell, ReusableCell {
+
+    @IBOutlet weak var reportIcon: UIImageView!
+    @IBOutlet weak var reportSelected: UIImageView!
+    @IBOutlet weak var reportText: UILabel!
+
+
+    // MARK: - Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.setupUI()
+        self.resetUI()
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.resetUI()
+    }
+
+
+    // MARK: - Static methods
+
+    static func reusableID() -> String {
+        return "ReportUserCell"
+    }
+
+
+    // MARK: - Private methods
+
+    private func setupUI() {
+        reportSelected.layer.cornerRadius = reportSelected.width/2
+    }
+
+    private func resetUI() {
+        reportIcon.image = nil
+        reportText.text = nil
+    }
 }
