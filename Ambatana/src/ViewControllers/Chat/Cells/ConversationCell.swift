aa2af4607558c7048f51bc17c22302944ad517bb
Dídac <didac@letgo.com>
2016-02-16 11:16:52 +0100

    HOTFIX - Conversation cells separator - using layer instead of border - removed cell background color on select while editing

--- Ambatana/src/ViewControllers/Chat/Cells/ConversationCell.swift
+++ Ambatana/src/ViewControllers/Chat/Cells/ConversationCell.swift
@@ -24,40 +24,54 @@ public class ConversationCell: UITableViewCell {
 
     static let defaultHeight: CGFloat = 76
 
+    var lines: [CALayer] = []
+
+
     // MARK: - Lifecycle
-    
+
     override public func awakeFromNib() {
         super.awakeFromNib()
         self.setupUI()
         self.resetUI()
     }
-    
+
     override public func prepareForReuse() {
         super.prepareForReuse()
         self.resetUI()
     }
 
-    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        // Redraw the lines
+        for line in lines {
+            line.removeFromSuperlayer()
+        }
+        lines = []
+        lines.append(contentView.addBottomBorderWithWidth(1, color: StyleHelper.lineColor))
+    }
+
+
     // MARK: - Overrides
-    
+
     public override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
         if (selected && !editing) {
-            setSelected(false, animated: animated)
+            super.setSelected(false, animated: animated)
+        } else {
+            super.setSelected(selected, animated: animated)
         }
     }
 
 
     // MARK: - Public methods
-       
+
     public func setupCellWithChat(chat: Chat, myUser: User, indexPath: NSIndexPath) {
         let tag = indexPath.hash
-    
+
         var otherUser: User?
         if let myUserId = myUser.objectId, let userFromId = chat.userFrom.objectId, let _ = chat.userTo.objectId {
             otherUser = (myUserId == userFromId) ? chat.userTo : chat.userFrom
         }
-        
+
         // thumbnail
         if let thumbURL = chat.product.thumbnail?.fileURL {
             thumbnailImageView.sd_setImageWithURL(thumbURL, placeholderImage: UIImage(named: "no_photo")) {
@@ -68,7 +82,7 @@ public class ConversationCell: UITableViewCell {
                 }
             }
         }
-        
+
         if let avatarURL = otherUser?.avatar?.fileURL {
             avatarImageView.sd_setImageWithURL(avatarURL, placeholderImage: UIImage(named: "no_photo")) {
                 [weak self] (image, error, cacheType, url)  in
@@ -77,11 +91,10 @@ public class ConversationCell: UITableViewCell {
                 }
             }
         }
-        
-        
+
         productLabel.text = chat.product.name ?? ""
         userLabel.text = otherUser?.name ?? ""
-        
+
         if chat.msgUnreadCount > 0 {
             timeLabel.font = StyleHelper.conversationTimeUnreadFont
             productLabel.font = StyleHelper.conversationProductUnreadFont
@@ -112,7 +125,7 @@ public class ConversationCell: UITableViewCell {
             statusImageView.hidden = true
             separationStatusImageToTimeLabel.constant = -statusImageView.frame.width
         }
-        
+
         let badge: String? = chat.msgUnreadCount > 0 ? String(chat.msgUnreadCount) : nil
         badgeLabel.text = badge
         badgeView.hidden = (badge == nil)
@@ -120,7 +133,7 @@ public class ConversationCell: UITableViewCell {
 
 
     // MARK: - Private methods
-    
+
     private func setupUI() {
         thumbnailImageView.layer.cornerRadius = StyleHelper.defaultCornerRadius
         avatarImageView.layer.cornerRadius = avatarImageView.width/2
@@ -128,17 +141,14 @@ public class ConversationCell: UITableViewCell {
         productLabel.font = StyleHelper.conversationProductFont
         userLabel.font = StyleHelper.conversationUserNameFont
         timeLabel.font = StyleHelper.conversationTimeFont
-        
+
         productLabel.textColor = StyleHelper.conversationProductColor
         userLabel.textColor = StyleHelper.conversationUserNameColor
         timeLabel.textColor = StyleHelper.conversationTimeColor
 
         badgeView.layer.cornerRadius = badgeView.height/2
-
-        contentView.layer.borderColor = StyleHelper.lineColor.CGColor
-        contentView.layer.borderWidth = 0.25
     }
-    
+
     private func resetUI() {
         thumbnailImageView.image = UIImage(named: "no_photo")
         avatarImageView.image = UIImage(named: "no_photo")
@@ -149,11 +159,17 @@ public class ConversationCell: UITableViewCell {
         badgeView.backgroundColor = StyleHelper.badgeBgColor
         badgeLabel.text = ""
         badgeLabel.font = StyleHelper.conversationBadgeFont
+        lines = []
     }
 
     override public func setEditing(editing: Bool, animated: Bool) {
+        if (editing) {
+            let bgView = UIView()
+            selectedBackgroundView = bgView
+        } else {
+            selectedBackgroundView = nil
+        }
         super.setEditing(editing, animated: animated)
         tintColor = StyleHelper.primaryColor
     }
-
 }
