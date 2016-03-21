9418fe7cd6b2ae8e11f7e097c4d7419b856c8380
Albert Hernández López <albert@letgo.com>
2016-03-21 14:17:53 +0100

    ABIOS-1022 removed guard

--- Ambatana/src/ViewControllers/Products/ProductViewModel.swift
+++ Ambatana/src/ViewControllers/Products/ProductViewModel.swift
@@ -184,7 +184,7 @@ class ProductViewModel: BaseViewModel {
         return false // temporary disable commercializer
         // TODO: Activate when Commercializer API returns real data
         guard let countryCode = product.value.postalAddress.countryCode else { return false }
-        guard let templates = commercializerRepository.templatesForCountryCode(countryCode) else { return false }
+        let templates = commercializerRepository.templatesForCountryCode(countryCode)
         return !templates.isEmpty
     }
 
