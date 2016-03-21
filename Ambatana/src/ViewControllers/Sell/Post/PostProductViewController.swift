800470c2d8c2b1ce806ae0ecb2d2cddd3b344b5d
Isaac Roldán <isaac@letgo.com>
2016-03-21 18:53:02 +0100

    fix tab selection after opening post product

--- Ambatana/src/ViewControllers/Sell/Post/PostProductViewController.swift
+++ Ambatana/src/ViewControllers/Sell/Post/PostProductViewController.swift
@@ -72,6 +72,13 @@ UITextFieldDelegate {
         setupView()
     }
 
+    override func viewDidLayoutSubviews() {
+        // We need to update the viewPager after we are sure the layout has been finished
+        let lastIndex = UserDefaultsManager.sharedInstance.loadLastPostProductTabSelected()
+        self.viewPager.delegate = self
+        self.viewPager.selectTabAtIndex(lastIndex)
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         cameraView.active = true
@@ -332,10 +339,6 @@ extension PostProductViewController: LGViewPagerDataSource, LGViewPagerDelegate,
         setupViewPagerConstraints()
 
         viewPager.reloadData()
-        
-        let lastIndex = UserDefaultsManager.sharedInstance.loadLastPostProductTabSelected()
-        viewPager.selectTabAtIndex(lastIndex)
-        viewPager.delegate = self
     }
 
     private func setupViewPagerConstraints() {
