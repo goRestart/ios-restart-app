bae9641f027fb4019a5e19f8b4ea35cc21759169
Isaac Roldán <isaac@letgo.com>
2016-03-23 15:49:01 +0100

    change 🌶 for 🍆  beacuse 🌶 is not in the standard

--- Ambatana/src/ViewControllers/Log In/SignUpLogInViewModel.swift
+++ Ambatana/src/ViewControllers/Log In/SignUpLogInViewModel.swift
@@ -185,7 +185,7 @@ public class SignUpLogInViewModel: BaseViewModel {
     }
     
     public func logIn() {
-        if email == "🌶🌶🌶🌶🌶" && password == "wat" {
+        if email == "🍆🍆🍆🍆🍆" && password == "wat" {
             delegate?.viewModelShowHiddenPasswordAlert(self)
             return
         }
