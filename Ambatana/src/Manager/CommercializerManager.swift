34f38e6f886d0888674de54ccb1733fa664a8662
Eli Kohen <eli.kohen@letgo.com>
2016-04-01 15:44:55 +0200

    ABIOS-997 Created commercializer manager

---
+++ Ambatana/src/Manager/CommercializerManager.swift
@@ -0,0 +1,16 @@
+//
+//  CommercializerManager.swift
+//  LetGo
+//
+//  Created by Eli Kohen on 01/04/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+public class CommercializerManager {
+
+    // Singleton
+    public static let sharedInstance: CommercializerManager = CommercializerManager()
+
+
+
+}
