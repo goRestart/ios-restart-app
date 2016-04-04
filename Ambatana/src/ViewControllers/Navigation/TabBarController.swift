6547805168d9ba6d078659dfeefcd398fd6b69bb
Albert Hernández López <albert@letgo.com>
2016-04-04 11:55:45 +0200

    ABIOS-1022 floating sell button is not hidden in my profile

--- Ambatana/src/ViewControllers/Navigation/TabBarController.swift
+++ Ambatana/src/ViewControllers/Navigation/TabBarController.swift
@@ -317,11 +317,11 @@ UIGestureRecognizerDelegate {
             let vcIdx = (viewControllers as NSArray).indexOfObject(navigationController)
             if let tab = Tab(rawValue: vcIdx) {
                 switch tab {
-                case .Home, .Categories, .Sell:
+                case .Home, .Categories, .Sell, .Profile:
                     //In case of those 4 sections, show if ctrl is root, or if its the MainProductsViewController
                     let showBtn = (viewController == rootViewCtrl) || (viewController is MainProductsViewController)
                     setSellFloatingButtonHidden(!showBtn, animated: animate)
-                case .Chats, .Profile:
+                case .Chats:
                     setSellFloatingButtonHidden(true, animated: false)
                 }
             }
