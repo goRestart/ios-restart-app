593e1067453e969ad2af6ea16a7494cbfe66c40c
Albert Hernández López <albert@letgo.com>
2016-04-12 11:04:51 +0200

    ABIOS-1116 country helper extension to get country language code for a product

---
+++ Ambatana/src/Helper/CountryHelper+Product.swift
@@ -0,0 +1,19 @@
+//
+//  CountryHelper+Product.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 12/04/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import LGCoreKit
+
+extension CountryHelper {
+    func countryLanguageForProduct(product: Product) -> String? {
+        guard let countryCode = product.postalAddress.countryCode else { return nil }
+        guard let countryInfo = strongSelf.countryHelper.countryInfoForCountryCode(countryCode) else { return nil }
+        guard let locale = countryInfo.locale else { return nil }
+        guard let languageCode = locale.objectForKey(NSLocaleLanguageCode) as? String else { return nil }
+        return languageCode
+    }
+}
