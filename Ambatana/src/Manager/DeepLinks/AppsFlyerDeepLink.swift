01c79667308e59b56460311584ba8d3fc4cdf994
Eli Kohen <eli.kohen@letgo.com>
2016-04-14 18:07:35 +0200

    ABIOS-1192 Added appsflyer deeplink that parses appsflyer urls to get the urischeme parameter on it

---
+++ Ambatana/src/Manager/DeepLinks/AppsFlyerDeepLink.swift
@@ -0,0 +1,35 @@
+//
+//  AppsFlyerDeepLink.swift
+//  LetGo
+//
+//  Created by Eli Kohen on 14/04/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+struct AppsFlyerDeepLink {
+
+    let deepLink: DeepLink
+
+    static func buildFromUserActivity(userActivity: NSUserActivity) -> AppsFlyerDeepLink? {
+        guard let url = userActivity.webpageURL else { return nil }
+        return AppsFlyerDeepLink.buildFromUrl(url)
+    }
+
+    /**
+     Initializer using Appsflyer urls https://letgo.onelink.me/...
+
+     */
+    private static func buildFromUrl(url: NSURL) -> AppsFlyerDeepLink? {
+
+        guard let host = url.host where host == "letgo.onelink.me" else {
+            //Any nil object or host different than letgo.onelink.me will be treated as error
+            return nil
+        }
+        let params = url.queryParameters
+        guard let urlSchemeString = params["af_dp"] else { return nil }
+        guard let schemeUrl = NSURL(string: urlSchemeString) else { return nil }
+        guard let uriScheme = UriScheme.buildFromUrl(schemeUrl) else { return nil }
+        return AppsFlyerDeepLink(deepLink: uriScheme.deepLink)
+    }
+}
+
