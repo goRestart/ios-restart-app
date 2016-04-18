0565268917167c6c0eeb0581062608ce37504f69
Eli Kohen <eli.kohen@letgo.com>
2016-04-18 14:36:54 +0200

    ABIOS-1168 Added snapchatProdDetail feature flag + abtest

--- Ambatana/src/Manager/FeatureFlags.swift
+++ Ambatana/src/Manager/FeatureFlags.swift
@@ -17,6 +17,15 @@ struct FeatureFlags {
         #endif
         return ABTests.directChatActive.value
     }
+
+    static var snapchatPrductDetail: Bool {
+        #if GOD_MODE
+            if FTSFlipTheSwitch.overridesABTests {
+                return FTSFlipTheSwitch.snapchatPrductDetail
+            }
+        #endif
+        return ABTests.snapchatProductDetail.value
+    }
 }
 
 private extension FTSFlipTheSwitch {
@@ -26,5 +35,9 @@ private extension FTSFlipTheSwitch {
 
     static var directChatActive: Bool {
         return FTSFlipTheSwitch.sharedInstance().isFeatureEnabled("direct_chat_active")
+    }
+
+    static var snapchatPrductDetail: Bool {
+        return FTSFlipTheSwitch.sharedInstance().isFeatureEnabled("snapchat_product_detail")
     }
 }
---
+++ Ambatana/src/ViewControllers/Products/ProductDetailFactory.swift
@@ -0,0 +1,9 @@
+//
+//  ProductDetailFactory.swift
+//  LetGo
+//
+//  Created by Eli Kohen on 18/04/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import Foundation
