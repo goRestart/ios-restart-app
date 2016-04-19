fd1f0daa622fb931968d87139ddbad4a1ecf6e98
Eli Kohen <eli.kohen@letgo.com>
2016-04-19 11:14:41 +0200

    ABIOS-1168 Separated favorites product list requester from the rest of user requesters

--- Ambatana/src/ViewControllers/Profile/ProfileProductListViewModel.swift
+++ Ambatana/src/ViewControllers/Profile/ProfileProductListViewModel.swift
@@ -23,36 +23,31 @@ public class ProfileProductListViewModel: ProductListViewModel {
     public var emptyStateButtonTitle: String?
     public var emptyStateButtonAction: (() -> ())?
 
-    private let type: ProfileProductListViewType
-
     // Repositories
     let productRepository: ProductRepository
-    let productRequester: ProfileProductListRequester
+    let productRequester: UserProductListRequester
 
 
     // MARK: - Lifecycle
     
     public init(myUserRepository: MyUserRepository, user: User?, type: ProfileProductListViewType,
-        locationManager: LocationManager, productRepository: ProductRepository) {
-            self.user = user ?? myUserRepository.myUser
-            self.type = type
-            self.productRepository = productRepository
-            self.productRequester = ProfileProductListRequester(type: type, productRepository: productRepository,
-                                                                locationManager: locationManager)
+                locationManager: LocationManager, productRepository: ProductRepository) {
+        self.productRepository = productRepository
+        switch type {
+        case .Selling:
+            self.productRequester = ProfileProductListRequester(productRepository: productRepository,
+                                                locationManager: locationManager, statuses: [.Pending, .Approved])
+        case .Sold:
+            self.productRequester = ProfileProductListRequester(productRepository: productRepository,
+                                                locationManager: locationManager, statuses: [.Sold, .SoldOld])
+        case .Favorites:
+            self.productRequester = FavoritesProductListRequester(productRepository: productRepository,
+                                                                  locationManager: locationManager)
+        }
+        self.user = user ?? myUserRepository.myUser
 
         super.init(requester: self.productRequester, locationManager: locationManager, productRepository: productRepository,
                 myUserRepository: myUserRepository, cellDrawer: ProductCellDrawerFactory.drawerForProduct(false))
-
-            switch type {
-            case .Selling:
-                statuses = [.Pending, .Approved]
-            case .Sold:
-                statuses = [.Sold, .SoldOld]
-            case .Favorites:
-                break
-            }
-
-            sortCriteria = .Creation
     }
     
     public convenience init(user: User? = nil, type: ProfileProductListViewType = .Selling) {
@@ -62,88 +57,68 @@ public class ProfileProductListViewModel: ProductListViewModel {
         self.init(myUserRepository: myUserRepository, user: user, type: type,
             locationManager: locationManager, productRepository: productRepository)
     }
+}
 
+protocol UserProductListRequester: ProductListRequester {
+    var userObjectId: String? { get set }
+}
 
-    // MARK: - Public methods
+class FavoritesProductListRequester: UserProductListRequester {
 
-    override func productsRetrieval(offset offset: Int, completion: ProductsCompletion?) {
-        guard let userId = user?.objectId else { return }
+    var userObjectId: String? = nil
+    let productRepository: ProductRepository
+    let locationManager: LocationManager
 
-        switch type {
-        case .Selling, .Sold:
-            super.productsRetrieval(offset: offset, completion: completion)
-        case .Favorites:
-            productRepository.indexFavorites(userId, completion: completion)
-        }
+    init(productRepository: ProductRepository, locationManager: LocationManager) {
+        self.productRepository = productRepository
+        self.locationManager = locationManager
     }
 
-    override func didSucceedRetrievingProducts() {
-        super.didSucceedRetrievingProducts()
+    func productsRetrieval(offset offset: Int, completion: ProductsCompletion?) {
+        guard let userId = userObjectId else { return }
+        productRepository.indexFavorites(userId, completion: completion)
+    }
 
-        switch type {
-        case .Selling, .Sold:
-            break
-        case .Favorites:
-            isLastPage = true
-        }
+    func isLastPage(resultCount: Int) -> Bool {
+        return true
     }
 }
 
 
-class ProfileProductListRequester: ProductListRequester {
+class ProfileProductListRequester: UserProductListRequester {
 
-    let type: ProfileProductListViewType
+    let statuses: [ProductStatus]
     let productRepository: ProductRepository
     let locationManager: LocationManager
 
     var userObjectId: String? = nil
 
-    private var queryCoordinates: LGLocationCoordinates2D? {
-        guard let currentLocation = locationManager.currentLocation else { return nil }
-        return LGLocationCoordinates2D(location: currentLocation)
-    }
-    private var retrieveProductsParams: RetrieveProductsParams {
-        var params: RetrieveProductsParams = RetrieveProductsParams()
-        params.coordinates = queryCoordinates
-        params.countryCode = locationManager.currentPostalAddress?.countryCode
-        params.sortCriteria = .Creation
-        switch type {
-        case .Selling:
-            params.statuses = [.Pending, .Approved]
-        case .Sold:
-            params.statuses = [.Sold, .SoldOld]
-        case .Favorites:
-            break
-        }
-        params.userObjectId = userObjectId
-        return params
-    }
-
-
-    init(type: ProfileProductListViewType, productRepository: ProductRepository, locationManager: LocationManager) {
+    init(productRepository: ProductRepository, locationManager: LocationManager, statuses: [ProductStatus]) {
         self.productRepository = productRepository
         self.locationManager = locationManager
-        self.type = type
+        self.statuses = statuses
     }
 
     func productsRetrieval(offset offset: Int, completion: ProductsCompletion?) {
-        guard let userId = userObjectId else { return }
-
-        switch type {
-        case .Selling, .Sold:
-            productRepository.index(retrieveProductsParams, pageOffset: offset, completion: completion)
-        case .Favorites:
-            productRepository.indexFavorites(userId, completion: completion)
-        }
+        guard let params = retrieveProductsParams else { return }
+        productRepository.index(params, pageOffset: offset, completion: completion)
     }
 
     func isLastPage(resultCount: Int) -> Bool {
-        switch type {
-        case .Selling, .Sold:
-            return resultCount == 0
-        case .Favorites:
-            return false
+        return resultCount == 0
+    }
+
+    private var retrieveProductsParams: RetrieveProductsParams? {
+        guard let userId = userObjectId else { return nil }
+        var params: RetrieveProductsParams = RetrieveProductsParams()
+        if let currentLocation = locationManager.currentLocation {
+            params.coordinates = LGLocationCoordinates2D(location: currentLocation)
         }
+        params.countryCode = locationManager.currentPostalAddress?.countryCode
+        params.sortCriteria = .Creation
+        params.statuses = statuses
+        params.userObjectId = userId
+        return params
     }
 }
 
