f87f9c5eb4ddbe4e33176c52a8391fe3f8ad7533
Eli Kohen <eli.kohen@letgo.com>
2016-05-02 15:28:51 +0200

    Hotfix: Actually fixed tabbar rx badges map

--- Ambatana/src/ViewControllers/Navigation/TabBarController.swift
+++ Ambatana/src/ViewControllers/Navigation/TabBarController.swift
@@ -280,12 +280,12 @@ final class TabBarController: UITabBarController, UITabBarControllerDelegate, UI
 
         let chatsTab = vcs[Tab.Chats.index].tabBarItem
         NotificationsManager.sharedInstance.unreadMessagesCount.asObservable().map {
-            $0.flatMap{String($0)}
+            $0.flatMap { $0 > 0 ? String($0) : nil }
         }.bindTo(chatsTab.rx_badgeValue).addDisposableTo(disposeBag)
 
         let notificationsTab = vcs[Tab.Notifications.index].tabBarItem
         NotificationsManager.sharedInstance.unreadNotificationsCount.asObservable().map {
-            $0.flatMap{String($0)}
+            $0.flatMap { $0 > 0 ? String($0) : nil }
         }.bindTo(notificationsTab.rx_badgeValue).addDisposableTo(disposeBag)
     }
 
