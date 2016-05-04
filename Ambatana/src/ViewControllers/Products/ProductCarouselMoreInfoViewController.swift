5cd2b0a961022c8d3e652d0a7bc2e6ed57c62221
Isaac Roldán <isaac@letgo.com>
2016-05-04 16:21:22 +0200

    created empty files for MoreInfo veiew

---
+++ Ambatana/src/ViewControllers/Products/ProductCarouselMoreInfoViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ProductCarouselMoreInfoViewController.swift
+//  LetGo
+//
+//  Created by Isaac Roldan on 4/5/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+class ProductCarouselMoreInfoViewController: BaseViewController {
+    
+}
---
+++ Ambatana/src/ViewControllers/Products/ProductCarouselMoreInfoViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ProductCarouselMoreInfoViewModel.swift
+//  LetGo
+//
+//  Created by Isaac Roldan on 4/5/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+class ProductCarouselMoreInfoViewModel: BaseViewModel {
+    
+}
