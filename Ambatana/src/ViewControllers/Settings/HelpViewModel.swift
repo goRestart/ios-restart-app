270abece1dfdcdeb6aca98abc196732515c6d089
Isaac Roldán <isaac@letgo.com>
2016-06-08 11:01:27 +0200

    removed contact us from Help section

--- Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
+++ Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
@@ -6,8 +6,22 @@
 //  Copyright (c) 2015 Ambatana. All rights reserved.
 //
 
+
+import LGCoreKit
+import DeviceUtil
+
 public class HelpViewModel: BaseViewModel {
    
+    let myUserRepository: MyUserRepository
+    
+    convenience override init() {
+        self.init(myUserRepository: Core.myUserRepository)
+    }
+    
+    init(myUserRepository: MyUserRepository) {
+        self.myUserRepository = myUserRepository
+    }
+    
     public var url: NSURL? {
         return LetgoURLHelper.composeURL(Constants.helpURL)
     }
@@ -18,5 +32,25 @@ public class HelpViewModel: BaseViewModel {
     
     var privacyURL: NSURL? {
         return LetgoURLHelper.composeURL(Constants.privacyURL)
+    }
+    
+    var contactUsURL: NSURL? {
+        guard let  url = LetgoURLHelper.composeURL(Constants.contactUs) else { return nil }
+        guard let urlComponents = NSURLComponents(URL: url, resolvingAgainstBaseURL: false) else { return nil }
+        urlComponents.percentEncodedQuery = buildContactParameters()
+        return urlComponents.URL
+    }
+
+    private func buildContactParameters() -> String? {
+        var param: [String: String] = [:]
+        param["app_version"] = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
+        param["os_version"] = UIDevice.currentDevice().systemVersion
+        param["device_model"] = DeviceUtil.hardwareDescription()
+        param["user_id"] = myUserRepository.myUser?.objectId
+        param["user_name"] = myUserRepository.myUser?.name
+        param["user_email"] = myUserRepository.myUser?.email
+        return param.map{"\($0)=\($1)"}
+            .joinWithSeparator("&")
+            .stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
     }
 }
