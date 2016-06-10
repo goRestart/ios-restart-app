0c296d665a3d6af8a49cfec6d355b4bf7c0091b4
Isaac Roldán <isaac@letgo.com>
2016-06-10 15:47:26 +0200

    use the web contact us in the rating view

--- Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
+++ Ambatana/src/ViewControllers/Settings/HelpViewModel.swift
@@ -35,22 +35,6 @@ public class HelpViewModel: BaseViewModel {
     }
     
     var contactUsURL: NSURL? {
-        guard let  url = LetgoURLHelper.composeURL(Constants.contactUs) else { return nil }
-        guard let urlComponents = NSURLComponents(URL: url, resolvingAgainstBaseURL: false) else { return nil }
-        urlComponents.percentEncodedQuery = buildContactParameters()
-        return urlComponents.URL
-    }
-
-    private func buildContactParameters() -> String? {
-        var param: [String: String] = [:]
-        param["app_version"] = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
-        param["os_version"] = UIDevice.currentDevice().systemVersion
-        param["device_model"] = DeviceUtil.hardwareDescription()
-        param["user_id"] = myUserRepository.myUser?.objectId
-        param["user_name"] = myUserRepository.myUser?.name
-        param["user_email"] = myUserRepository.myUser?.email
-        return param.map{"\($0)=\($1)"}
-            .joinWithSeparator("&")
-            .stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        return LetgoURLHelper.buildContactUsURL(myUserRepository.myUser)
     }
 }
