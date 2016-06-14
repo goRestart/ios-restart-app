968a6c0c8f5b334c13d7d5a20ee366e02e42fae1
Eli Kohen <eli.kohen@letgo.com>
2016-06-14 18:29:18 +0200

    ABIOS-1336 Created chat other info cell

---
+++ Ambatana/src/ViewControllers/Chat/ChatDetail/Cells/ChatOtherInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatOtherInfoCell.swift
+//  LetGo
+//
+//  Created by Eli Kohen on 14/06/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class ChatOtherInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
