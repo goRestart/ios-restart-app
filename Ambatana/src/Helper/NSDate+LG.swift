02229ba007677fe988581dccec665e184fa7689d
Dídac <didac@letgo.com>
2016-06-14 10:00:25 +0200

    ABIOS-1300 Cloudsight real time - Added titleDisclaimerStatus .Clean (for when there's no disclaimer) - added request to get the auto generated title

--- Ambatana/src/Helper/NSDate+LG.swift
+++ Ambatana/src/Helper/NSDate+LG.swift
@@ -151,10 +151,10 @@ extension NSDate {
     }
 
     func isFromLast24h() -> Bool {
-        return isOlderThan(86400)
+        return isNewerThan(86400)
     }
 
-    func isOlderThan(seconds: NSTimeInterval) -> Bool {
+    func isNewerThan(seconds: NSTimeInterval) -> Bool {
         let time = self.timeIntervalSince1970
         let now = NSDate().timeIntervalSince1970
         return (now-time) < seconds
