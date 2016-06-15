a32cf760d993a93cca8512a967aec5c5076f1052
Isaac Roldán <isaac@letgo.com>
2016-06-15 15:10:44 +0200

    don't set the stripe data beacuse we are not going to use it

--- Ambatana/src/ViewControllers/Products/CellDrawers/ImageProductCellDrawer.swift
+++ Ambatana/src/ViewControllers/Products/CellDrawers/ImageProductCellDrawer.swift
@@ -31,22 +31,5 @@ class ImageProductCellDrawer: BaseCollectionCellDrawer<ProductCell>, ProductCell
         if let thumbURL = data.thumbUrl {
             cell.setImageUrl(thumbURL)
         }
-
-        // Status (stripe info)
-        switch data.status {
-        case .Sold, .SoldOld:
-            cell.stripeImageView.image = UIImage(named: "stripe_white")
-            cell.stripeLabel.textColor = StyleHelper.soldColor
-            cell.stripeLabel.text = LGLocalizedString.productListItemSoldStatusLabel.capitalizedString
-            cell.stripeIcon.image = UIImage(named: "ic_sold_stripe")
-        case .Pending, .Approved, .Discarded, .Deleted:
-            if let createdAt = data.date where
-                NSDate().timeIntervalSinceDate(createdAt) < Constants.productListNewLabelThreshold {
-                    cell.stripeImageView.image = UIImage(named: "stripe_white")
-                    cell.stripeLabel.textColor = StyleHelper.primaryColor
-                    cell.stripeLabel.text = createdAt.simpleTimeStringForDate()
-                    cell.stripeIcon.image = UIImage(named: "ic_new_stripe")
-            }
-        }
     }
 }
