f8b6e0d90449575eef033a5970258ea71656f41e
Albert Hernández López <albert@letgo.com>
2016-07-21 16:28:44 +0200

    ABIOS-1417 added some doc to roundNearest func

--- Ambatana/src/Helper/Float+Rounding.swift
+++ Ambatana/src/Helper/Float+Rounding.swift
@@ -7,6 +7,15 @@
 //
 
 extension Float {
+    /* Rounds to the nearest given decimal. Examples:
+        
+     Float(4.24).roundNearest(0.5) -> 4
+     Float(4.5).roundNearest(0.5)  -> 4.5
+     Float(4.74).roundNearest(0.5) -> 4.5
+     Float(4.76).roundNearest(0.5) -> 5
+
+     Float(4.24).roundNearest(0.1) -> 4.2
+     Float(4.25).roundNearest(0.1) -> 4.3 */
     func roundNearest(nearest: Float) -> Float {
         let n = 1/nearest
         return round(self * n) / n
