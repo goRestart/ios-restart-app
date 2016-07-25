fdd886f687b5dd91f5a436ff42609604a97ea4d3
Dídac <didac@letgo.com>
2016-07-25 10:38:59 +0200

    ABIOS-1419 Updated pods  ( LGCoreKit to 0.27.6 )

--- Pods/LGCoreKit/LGCoreKit/Routers/UserRatingRouter.swift
+++ Pods/LGCoreKit/LGCoreKit/Routers/UserRatingRouter.swift
@@ -10,6 +10,7 @@ enum UserRatingRouter: URLRequestAuthenticable {
 
     static let ratingsEndpoint = "/rating"
 
+    case Show(objectId: String)
     case Index(params: [String : AnyObject])
     case Create(params: [String : AnyObject])
     case Update(objectId: String, params: [String : AnyObject])
@@ -17,7 +18,7 @@ enum UserRatingRouter: URLRequestAuthenticable {
 
     var endpoint: String {
         switch self {
-        case .Index, .Create, .Update:
+        case .Show, .Index, .Create, .Update:
             return UserRatingRouter.ratingsEndpoint
         case let .Report(objectId):
             return UserRatingRouter.ratingsEndpoint+"/\(objectId)/report"
@@ -29,7 +30,7 @@ enum UserRatingRouter: URLRequestAuthenticable {
         switch self {
         case .Index:
             return .Installation
-        case .Create, .Update, .Report:
+        case .Show, .Create, .Update, .Report:
             return .User
         }
     }
@@ -38,12 +39,15 @@ enum UserRatingRouter: URLRequestAuthenticable {
 
     var URLRequest: NSMutableURLRequest {
         switch self {
+        case let .Show(objectId):
+            return Router<UserRatingsBaseURL>.Show(endpoint: endpoint, objectId: objectId).URLRequest
         case let .Index(params):
             return Router<UserRatingsBaseURL>.Index(endpoint: endpoint, params: params).URLRequest
         case let .Create(params):
             return Router<UserRatingsBaseURL>.Create(endpoint: endpoint, params: params, encoding: nil).URLRequest
         case let .Update(objectId, params):
-            return Router<UserRatingsBaseURL>.Update(endpoint: endpoint, objectId: objectId, params: params, encoding: nil).URLRequest
+            return Router<UserRatingsBaseURL>.Update(endpoint: endpoint, objectId: objectId, params: params,
+                                                     encoding: nil).URLRequest
         case .Report:
             return Router<UserRatingsBaseURL>.BatchUpdate(endpoint: endpoint, params: [:], encoding: nil).URLRequest
         }
