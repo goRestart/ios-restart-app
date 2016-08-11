7bfc55d5a72437caea5fc70b6a38dca8f6923a3d
Eli Kohen <eli.kohen@letgo.com>
2016-08-11 13:22:37 +0200

    ABIOS-1498 Added A/B Tests

--- Ambatana/src/Manager/ABTest/ABTests.swift
+++ Ambatana/src/Manager/ABTest/ABTests.swift
@@ -12,16 +12,21 @@ public struct ABTests {
 
     static let trackingData = Variable<[(String, AnyObject)]>([])
 
+    static var bigFavoriteIcon = BoolABDynamicVar(key: "bigFavoriteIcon", defaultValue: false)
     static var showRelatedProducts = BoolABDynamicVar(key: "showRelatedProducts", defaultValue: false)
     static var showPriceOnListings = BoolABDynamicVar(key: "showPriceOnListings", defaultValue: false)
 
     static func registerVariables() {
+        let _ = bigFavoriteIcon.value
         let _ = showRelatedProducts.value
         let _ = showPriceOnListings.value
     }
 
     static func variablesUpdated() {
         var result: [(String, AnyObject)] = []
+        if let bigFavoriteIconData = bigFavoriteIcon.trackingData {
+            result.append(bigFavoriteIconData)
+        }
         if let relatedProductsData = showRelatedProducts.trackingData {
             result.append(relatedProductsData)
         }
