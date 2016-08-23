b722d8a028f392005dfe6e1ce2545437193ada9e
Albert Hernández López <albert@letgo.com>
2016-08-23 15:04:49 +0200

    ABIOS-1550 added accessibility id constants struct

---
+++ Ambatana/src/Constants/AccessibilityId.swift
@@ -0,0 +1,28 @@
+//
+//  AccessibilityId.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 23/08/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+/**
+ Defines the accessibility identifiers used for automated UI testing. The format is the following:
+    <screen><element-name> = "<element-name>"
+ 
+ i.e:
+    static var SignUpLoginEmailButton = "EmailButton"
+ */
+struct AccessibilityId {
+    /** ABIOS-1554 */
+    // ...
+
+    /** ABIOS-1555 */
+    // ...
+
+    /** ABIOS-1556 */
+    // ...
+
+    /** ABIOS-1557 */
+    // ...
+}
