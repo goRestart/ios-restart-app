8581a5d8aaefe04029d38df4a43528ed9841e6f8
Isaac Roldán <isaac@letgo.com>
2016-08-26 15:23:18 +0200

    Empty view added

---
+++ Ambatana/src/ViewControllers/NPS/NPSViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NPSViewController.swift
+//  LetGo
+//
+//  Created by Isaac Roldan on 26/8/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import Foundation
+
+
+class NPSViewController: BaseViewController {
+    
+}
