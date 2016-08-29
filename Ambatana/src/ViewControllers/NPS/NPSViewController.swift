481f4c016fd788d61afb4393642658234e9debd7
Isaac Roldán <isaac@letgo.com>
2016-08-29 14:18:29 +0200

    nps view basic design

--- Ambatana/src/ViewControllers/NPS/NPSViewController.swift
+++ Ambatana/src/ViewControllers/NPS/NPSViewController.swift
@@ -10,5 +10,34 @@ import Foundation
 
 
 class NPSViewController: BaseViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var notLikelyLabel: UILabel!
+    @IBOutlet weak var extremelyLikelyLabel: UILabel!
+    @IBOutlet weak var notLikelyImage: UIImageView!
+    @IBOutlet weak var extremelyLikelyImage: UIImageView!
+    @IBOutlet var npsButtons: [UIButton]!
     
+    var viewModel: NPSViewModel
+    
+    init(viewModel: NPSViewModel) {
+        self.viewModel = viewModel
+        super.init(viewModel: viewModel, nibName: "NPSViewController")
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        npsButtons.forEach {
+            $0.layer.cornerRadius = $0.height/2
+            $0.backgroundColor = UIColor.whiteColor()
+            $0.layer.borderColor = UIColor.primaryColor.CGColor
+            $0.layer.borderWidth = 1.0
+            $0.setTitle(String($0.tag), forState: .Normal)
+            $0.setTitleColor(UIColor.primaryColor, forState: .Normal)
+        }
+    }
 }
---
+++ Ambatana/src/ViewControllers/NPS/NPSViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NPSViewModel.swift
+//  LetGo
+//
+//  Created by Isaac Roldan on 29/8/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import Foundation
+
+class NPSViewModel: BaseViewModel {
+    
+}
