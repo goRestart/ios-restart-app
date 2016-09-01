0a8240cf2a14f3ac494c1fbb2e4a297839b0b9be
Dídac <didac@letgo.com>
2016-09-01 12:30:08 +0200

    ABIOS-1580 Fix distance bubble - removed duplicated conversion from meters to Km/Mi

--- Ambatana/src/ViewControllers/Main/FilteredProductListRequester.swift
+++ Ambatana/src/ViewControllers/Main/FilteredProductListRequester.swift
@@ -102,20 +102,12 @@ class FilteredProductListRequester: ProductListRequester {
     func distanceFromProductCoordinates(productCoords: LGLocationCoordinates2D) -> Double {
 
         var meters = 0.0
-
         if let coordinates = queryCoordinates {
             let quadKeyStr = coordinates.coordsToQuadKey(LGCoreKitConstants.defaultQuadKeyPrecision)
             let actualQueryCoords = LGLocationCoordinates2D(fromCenterOfQuadKey: quadKeyStr)
             meters = productCoords.distanceTo(actualQueryCoords)
         }
-
-        let distanceType = DistanceType.systemDistanceType()
-        switch (distanceType) {
-        case .Km:
-            return meters * 0.001
-        case .Mi:
-            return meters * 0.000621371
-        }
+        return meters
     }
 }
 
