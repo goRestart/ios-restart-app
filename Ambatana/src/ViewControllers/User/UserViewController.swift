d2af9daca3a119a7f852f6781bcea6a183cd20a6
Eli Kohen <eli.kohen@letgo.com>
2016-09-01 14:50:37 +0200

    ABIOS-1545 Added AccesibilityIDs

--- Ambatana/src/ViewControllers/User/UserViewController.swift
+++ Ambatana/src/ViewControllers/User/UserViewController.swift
@@ -237,6 +237,7 @@ extension UserViewController {
         headerContainer?.header?.myUserFacebookButton.accessibilityId = .UserHeaderExpandedVerifyFacebookButton
         headerContainer?.header?.myUserGoogleButton.accessibilityId = .UserHeaderExpandedVerifyGoogleButton
         headerContainer?.header?.myUserEmailButton.accessibilityId = .UserHeaderExpandedVerifyEmailButton
+        headerContainer?.header?.buildTrustButton.accessibilityId = .UserHEaderExpandedBuildTrustButton
         headerContainer?.header?.sellingButton.accessibilityId = .UserSellingTab
         headerContainer?.header?.soldButton.accessibilityId = .UserSoldTab
         headerContainer?.header?.favoritesButton.accessibilityId = .UserFavoritesTab
