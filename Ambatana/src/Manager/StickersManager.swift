5009408c22ce428005804663f9fddc00e21fc1b6
Eli Kohen <eli.kohen@letgo.com>
2016-09-07 14:56:30 +0200

    ABIOS-1572 Injected Image downloader

--- Ambatana/src/Manager/StickersManager.swift
+++ Ambatana/src/Manager/StickersManager.swift
@@ -14,15 +14,17 @@ class StickersManager {
     static let sharedInstance = StickersManager()
 
     private let stickersRepository: StickersRepository
+    private let imageDownloader: ImageDownloader
     private let disposeBag = DisposeBag()
     private var cachedImageUrls = Set<String>()
 
     convenience init() {
-        self.init(stickersRepository: Core.stickersRepository)
+        self.init(stickersRepository: Core.stickersRepository, imageDownloader: ImageDownloader.sharedInstance)
     }
 
-    init(stickersRepository: StickersRepository) {
+    init(stickersRepository: StickersRepository, imageDownloader: ImageDownloader) {
         self.stickersRepository = stickersRepository
+        self.imageDownloader = imageDownloader
     }
 
     func setup() {
@@ -36,7 +38,7 @@ class StickersManager {
                                 .filter { !(self?.cachedImageUrls.contains($0) ?? true) }
                                 .flatMap { NSURL(string: $0) }
             }.bindNext { [weak self] urls in
-                ImageDownloader.sharedInstance.downloadImagesWithURLs(urls)
+                self?.imageDownloader.downloadImagesWithURLs(urls)
                 urls.forEach { self?.cachedImageUrls.insert($0.absoluteString) }
             }.addDisposableTo(disposeBag)
     }
