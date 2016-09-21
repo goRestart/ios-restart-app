cb980562a3b4fbb5c2bc218f91f9fc3d9176144b
Dídac <didac@letgo.com>
2016-09-21 15:11:22 +0200

    Changed approach, blocking user interaction when the safety tips view is created and restoring it when is shown

--- Ambatana/src/ViewControllers/Chat/ChatDetail/OldChatViewController.swift
+++ Ambatana/src/ViewControllers/Chat/ChatDetail/OldChatViewController.swift
@@ -31,7 +31,6 @@ class OldChatViewController: SLKTextViewController {
     let disposeBag = DisposeBag()
 
     var stickersTooltip: Tooltip?
-    var chatSafetyTipsView: ChatSafetyTipsView?
 
     var blockedToastOffset: CGFloat {
         return relationInfoView.hidden ? 0 : RelationInfoView.defaultHeight
@@ -98,7 +97,6 @@ class OldChatViewController: SLKTextViewController {
         super.viewWillDisappear(animated)
         viewModel.active = false
         removeStickersTooltip()
-        removeSafetyTips()
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -636,11 +634,13 @@ extension OldChatViewController {
     
     private func showSafetyTips() {
         guard let navCtlView = navigationController?.view else { return }
-        chatSafetyTipsView = ChatSafetyTipsView.chatSafetyTipsView()
-        guard let chatSafetyTipsView = chatSafetyTipsView else { return }
-        
+        guard let chatSafetyTipsView = ChatSafetyTipsView.chatSafetyTipsView() else { return }
+
+        navCtlView.userInteractionEnabled = false
+
         // Delay is needed in order not to mess with the kb show/hide animation
         delay(0.5) { [weak self] in
+            navCtlView.userInteractionEnabled = true
             self?.showKeyboard(false, animated: true)
             chatSafetyTipsView.dismissBlock = { [weak self] in
                 self?.viewModel.safetyTipsDismissed()
@@ -650,14 +650,6 @@ extension OldChatViewController {
             chatSafetyTipsView.frame = navCtlView.frame
             navCtlView.addSubview(chatSafetyTipsView)
             chatSafetyTipsView.show()
-        }
-    }
-
-    private func removeSafetyTips() {
-        guard let navCtlView = navigationController?.view else { return }
-        guard let chatSafetyTipsView = chatSafetyTipsView else { return }
-        if navCtlView.subviews.contains(chatSafetyTipsView) {
-            chatSafetyTipsView.removeFromSuperview()
         }
     }
 }
