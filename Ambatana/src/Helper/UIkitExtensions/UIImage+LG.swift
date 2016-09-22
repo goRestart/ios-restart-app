0b3fbf3bce09dc5c06c5c83bdd3a770cc5e0b44d
Albert Hernández López <albert@letgo.com>
2016-09-22 17:59:45 +0200

    ABIOS-1659 fixed ! in UIImage+LG

--- Ambatana/src/Helper/UIkitExtensions/UIImage+LG.swift
+++ Ambatana/src/Helper/UIkitExtensions/UIImage+LG.swift
@@ -11,21 +11,20 @@ extension UIImage {
     func rotatedImage(clockWise: Bool = true) -> UIImage {
         
         UIGraphicsBeginImageContext(CGSize(width: size.height, height: size.width))
-        let context = UIGraphicsGetCurrentContext()
+        guard let context = UIGraphicsGetCurrentContext() else { return self }
         
-        CGContextTranslateCTM(context!, 0.5 * size.height, 0.5 * size.width)
+        CGContextTranslateCTM(context, 0.5 * size.height, 0.5 * size.width)
         if clockWise {
-            CGContextRotateCTM(context!, CGFloat(M_PI_2))
+            CGContextRotateCTM(context, CGFloat(M_PI_2))
         } else {
-            CGContextRotateCTM(context!, CGFloat(-M_PI_2))
+            CGContextRotateCTM(context, CGFloat(-M_PI_2))
         }
-        CGContextTranslateCTM(context!, -0.5 * size.width, -0.5 * size.height)
+        CGContextTranslateCTM(context, -0.5 * size.width, -0.5 * size.height)
         
         drawAtPoint(CGPointMake(0, 0))
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
-        return image!
+        return image ?? self
     }
 }
