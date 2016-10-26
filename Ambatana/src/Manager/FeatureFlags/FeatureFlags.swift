ff033c33bc13d352495c163802569e1c68fc4133
Albert Hernández López <albert@letgo.com>
2016-10-26 11:58:08 +0200

    added AB test for notification center

--- Ambatana/src/Manager/FeatureFlags/FeatureFlags.swift
+++ Ambatana/src/Manager/FeatureFlags/FeatureFlags.swift
@@ -25,7 +25,7 @@ struct FeatureFlags {
         if Bumper.enabled {
             return Bumper.notificationsSection
         }
-        return ABTests.notificationCenterEnabled
+        return ABTests.notificationCenterEnabled.value
     }()
 
     static var userRatings: Bool {
