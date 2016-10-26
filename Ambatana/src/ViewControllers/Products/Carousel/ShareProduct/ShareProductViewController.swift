08cf2ebc14b8de5689bf3653130ae27d78f52316
Dídac <didac@letgo.com>
2016-10-26 14:10:24 +0200

    ABIOS-1812 Full Screen Share - Added assets - VC WiP

---
+++ Ambatana/src/ViewControllers/Products/Carousel/ShareProduct/ShareProductViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ShareProductViewController.swift
+//  LetGo
+//
+//  Created by Dídac on 26/10/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class ShareProductViewController: BaseViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var shareButtonsContainer: UIView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closeButtonPressed(sender: AnyObject) {
+    }
+
+}
