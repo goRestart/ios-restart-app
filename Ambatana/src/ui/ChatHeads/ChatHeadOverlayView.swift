01fd779b8021bbc46bc07786607e5e9284b1732b
Albert Hernández López <albert@letgo.com>
2016-11-03 14:17:27 +0100

    ABIOS-1663 added chat head classes

---
+++ Ambatana/src/ui/ChatHeads/ChatHeadOverlayView.swift
@@ -0,0 +1,13 @@
+//
+//  ChatHeadOverlayView.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 03/11/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+final class ChatHeadOverlayView: UIView {
+    
+}
---
+++ Ambatana/src/ui/ChatHeads/ChatHeadView.swift
@@ -0,0 +1,13 @@
+//
+//  ChatHeadView.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 03/11/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+final class ChatHeadView: UIView {
+
+}
