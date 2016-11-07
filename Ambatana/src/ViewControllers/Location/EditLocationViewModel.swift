262833dc17d36892a0d785eb8bd63a596c22c115
Dídac <didac@letgo.com>
2016-11-07 16:45:12 +0100

    ABIOS-1233 Show state - removed duplicated state info

--- Ambatana/src/ViewControllers/Location/EditLocationViewModel.swift
+++ Ambatana/src/ViewControllers/Location/EditLocationViewModel.swift
@@ -386,13 +386,12 @@ extension Place {
                 }
                 result += city
             }
-        }
-
-        if let state = postalAddress?.state {
-            if !result.isEmpty {
-                result += ", "
+            if let state = postalAddress?.state {
+                if !result.isEmpty {
+                    result += ", "
+                }
+                result += state
             }
-            result += state
         }
 
         if let country = postalAddress?.countryCode {
@@ -401,6 +400,7 @@ extension Place {
             }
             result += country
         }
+
         return result
     }
 }
