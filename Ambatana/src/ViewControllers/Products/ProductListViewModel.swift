223ed527926e86b9f519a6592f98c307ef09f8f8
Eli Kohen <eli.kohen@letgo.com>
2016-11-08 11:28:18 +0100

    ABIOS-1858 Improved resetUI by using clearList method

--- Ambatana/src/ViewControllers/Products/ProductListViewModel.swift
+++ Ambatana/src/ViewControllers/Products/ProductListViewModel.swift
@@ -174,14 +174,13 @@ class ProductListViewModel: BaseViewModel {
     }
 
     func resetUI() {
-        objects = []
         pageNumber = 0
         refreshing = false
         state = .Loading
         isLastPage = false
         isLoading = false
         isOnErrorState = false
-        delegate?.vmReloadData(self)
+        clearList()
     }
  
 
