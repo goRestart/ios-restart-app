96dcd5591f58e678e0031d835688334682c27001
Juan Iglesias <juan.iglesias@letgo.com>
2016-11-16 10:32:41 +0100

    ABIOS-1654: Move guards to top.

--- Ambatana/src/Helper/3rdPartyExtensions/Leanplum+LG.swift
+++ Ambatana/src/Helper/3rdPartyExtensions/Leanplum+LG.swift
@@ -32,12 +32,12 @@ extension Leanplum {
         // ofKind: LeanplumActionKind | kLeanplumActionKindAction  need to be set as rawValue.
         Leanplum.defineAction(leamplumCustomPopUp, ofKind: LeanplumActionKind(rawValue: 0b11), withArguments: arguments, withResponder:  { context in
             guard let context = context else { return false }
-            let okAction = UIAction(interface: .StyledText(context.stringNamed(buttonTextIdentifier), .Default),
-                                    action: { context.runTrackedActionNamed(actionIdentifier) },
-                                    accessibilityId: .AcceptPopUpButton)
             guard let title = context.stringNamed(titleIdentifier) else { return false}
             guard let message = context.stringNamed(messageTextIdentifier) else { return false}
             guard let image = context.fileNamed(imageIdentifier) else { return false}
+            let okAction = UIAction(interface: .StyledText(context.stringNamed(buttonTextIdentifier), .Default),
+                                    action: { context.runTrackedActionNamed(actionIdentifier) },
+                                    accessibilityId: .AcceptPopUpButton)
             presenter.showLeanplumAlert(title, text:message, image:image, action: okAction)
             return true
         })
