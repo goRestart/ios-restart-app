54550ee076d7584120b77c72c97ea45c8d2bf8c5
Albert Hernández López <albert@letgo.com>
2016-11-21 16:23:48 +0100

    ABIOS-1864 added SignUpViewModelSpec

---
+++ letgoTests/Unit/ViewModel/SignUpViewModelSpec.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewModelSpec.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 21/11/16.
+//  Copyright © 2016 Ambatana. All rights reserved.
+//
+
+@testable import LetGo
+import LGCoreKit
+import Quick
+import Nimble
+
+class SignUpViewModelSpec: QuickSpec {
+    override func spec() {
+
+        describe("SignUpViewModelSpec") {
+
+            beforeEach {
+                
+            }
+
+            context ("") {
+
+            }
+        }
+    }
+}
