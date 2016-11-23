a1032702354db97c22c2c51632ebe15eb5dcc19c
Albert Hernández López <albert@letgo.com>
2016-11-23 09:27:51 +0100

    ABIOS-1902 added remove commercializer popup after posting AB test

--- Ambatana/src/Manager/FeatureFlags/BumperFeatures.swift
+++ Ambatana/src/Manager/FeatureFlags/BumperFeatures.swift
@@ -12,9 +12,9 @@ import bumper
 
 extension Bumper  {
     static func initialize() {
-        Bumper.initialize([WebsocketChat.self, NotificationsSection.self, UserReviews.self, ShowNPSSurvey.self, MessageOnFavoriteRound2Mode.self, InterestedUsersMode.self, FiltersReorder.self, DirectPostInOnboarding.self, ShareButtonWithIcon.self, ProductDetailShareMode.self, PeriscopeChat.self, ChatHeadBubbles.self, ExpressChatBanner.self, ShowLiquidProductsToNewUser.self, KeywordsTravelCollection.self])
+        Bumper.initialize([WebsocketChat.self, NotificationsSection.self, UserReviews.self, ShowNPSSurvey.self, MessageOnFavoriteRound2Mode.self, InterestedUsersMode.self, FiltersReorder.self, DirectPostInOnboarding.self, ShareButtonWithIcon.self, ProductDetailShareMode.self, PeriscopeChat.self, ChatHeadBubbles.self, ExpressChatBanner.self, ShowLiquidProductsToNewUser.self, KeywordsTravelCollection.self, CommercializerAfterPosting.self])
     } 
-	
+
     static var websocketChat: Bool {
         guard let value = Bumper.valueForKey(WebsocketChat.key) else { return false }
         return WebsocketChat(rawValue: value)?.asBool ?? false
@@ -88,6 +88,11 @@ extension Bumper  {
     static var keywordsTravelCollection: KeywordsTravelCollection {
         guard let value = Bumper.valueForKey(KeywordsTravelCollection.key) else { return .Standard }
         return KeywordsTravelCollection(rawValue: value) ?? .Standard 
+    }
+
+    static var commercializerAfterPosting: Bool {
+        guard let value = Bumper.valueForKey(CommercializerAfterPosting.key) else { return false }
+        return CommercializerAfterPosting(rawValue: value)?.asBool ?? false
     } 
 }
 
@@ -252,5 +257,14 @@ enum KeywordsTravelCollection: String, BumperFeature  {
             default: return .Standard
         }
     }
+}
+
+enum CommercializerAfterPosting: String, BumperFeature  {
+    case No, Yes
+    static var defaultValue: String { return CommercializerAfterPosting.No.rawValue }
+    static var enumValues: [CommercializerAfterPosting] { return [.No, .Yes]}
+    static var values: [String] { return enumValues.map{$0.rawValue} }
+    static var description: String { return "Commercializer after posting" } 
+    var asBool: Bool { return self == .Yes }
 }
 
