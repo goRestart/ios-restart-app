d1242e11a2885f50d3f1f2a2d925c307caae3fb9
Juan Iglesias <juan.iglesias@letgo.com>
2016-12-22 14:56:33 +0100

    ABIOS-1900: Update naming on mockFeatureFlags

--- letgoTests/Unit/ViewModel/MockFeatureFlags.swift
+++ letgoTests/Unit/ViewModel/MockFeatureFlags.swift
@@ -35,5 +35,5 @@ class MockFeatureFlags: FeatureFlaggeable {
     var newQuickAnswers: Bool = false
     var favoriteWithBadgeOnProfile: Bool = false
     var favoriteWithBubbleToChat: Bool = false
-    var locationMatchesCountry: Bool = false
+    var locationNoMatchesCountry: Bool = false
 }
