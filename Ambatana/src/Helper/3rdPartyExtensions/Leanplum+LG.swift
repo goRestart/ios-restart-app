8d39a01e7e165316a928ffc5e8f2d1fbb6da3907
Eli Kohen <eli.kohen@letgo.com>
2017-01-10 18:52:51 +0100

    Leanplum+LG migration

--- Ambatana/src/Helper/3rdPartyExtensions/Leanplum+LG.swift
+++ Ambatana/src/Helper/3rdPartyExtensions/Leanplum+LG.swift
@@ -28,16 +28,18 @@ extension Leanplum {
         let argumentImage = LPActionArg(named: imageIdentifier, withFile: nil)
         let argumentButton = LPActionArg(named: buttonTextIdentifier, with: "")
         let argumentAction = LPActionArg(named: actionIdentifier, withAction: nil)
-        let arguments = [argumentTitle, argumentMessage, argumentImage, argumentButton, argumentAction]
+        let arguments = [argumentTitle, argumentMessage, argumentImage, argumentButton, argumentAction].flatMap { $0 }
         // ofKind: LeanplumActionKind | kLeanplumActionKindAction  need to be set as rawValue.
-        Leanplum.defineAction(leanplumCustomPopUp, ofKind: LeanplumActionKind(rawValue: 0b11), withArguments: arguments, withResponder:  { context in
+
+        Leanplum.defineAction(Leanplum.leanplumCustomPopUp, of: LeanplumActionKind(rawValue: 0b11),
+                              withArguments: arguments, withOptions: [:], withResponder:  { (context: LPActionContext?) -> Bool in
             guard let context = context else { return false }
             guard let message = context.stringNamed(messageTextIdentifier) else { return false}
             guard let image = context.fileNamed(imageIdentifier) else { return false}
             guard let buttonText = context.stringNamed(buttonTextIdentifier) else { return false}
             
             let title = context.stringNamed(titleIdentifier)
-            let okAction = UIAction(interface: .styledText(buttonText, .default),
+            let okAction = UIAction(interface: .styledText(buttonText, .standard),
                                     action: { context.runTrackedActionNamed(actionIdentifier) },
                                     accessibilityId: .acceptPopUpButton)
             presenter.showLeanplumAlert(title, text:message, image:image, action: okAction)
