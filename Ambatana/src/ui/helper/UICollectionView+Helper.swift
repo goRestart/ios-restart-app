d285c890c77bf9b3446f801c1196d0d1f3a4837b
Eli Kohen <eli.kohen@letgo.com>
2017-01-10 16:46:13 +0100

    ABIOS-1872 ui folder re-migrated

--- Ambatana/src/ui/helper/UICollectionView+Helper.swift
+++ Ambatana/src/ui/helper/UICollectionView+Helper.swift
@@ -25,11 +25,11 @@ extension UICollectionView {
     private func handleChange<T>(_ change: CollectionChange<T>) {
         switch change {
         case .remove(let index, _):
-            let indexPath = IndexPath(forRow: index, inSection: 0)
-            deleteItemsAtIndexPaths([indexPath])
+            let indexPath = IndexPath(row: index, section: 0)
+            deleteItems(at: [indexPath])
         case .insert(let index, _):
-            let indexPath = IndexPath(forRow: index, inSection: 0)
-            insertItemsAtIndexPaths([indexPath])
+            let indexPath = IndexPath(row: index, section: 0)
+            insertItems(at: [indexPath])
         case .composite(let changes):
             changes.forEach { [weak self] change in
                 self?.handleChange(change)
