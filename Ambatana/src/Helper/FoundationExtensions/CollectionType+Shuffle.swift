3c5755415f40036812a238c76a37d970ae8528be
Eli Kohen <eli.kohen@letgo.com>
2017-01-11 10:51:37 +0100

    ABIOS-1872 Fixed CollectionType+Shuffle

--- Ambatana/src/Helper/FoundationExtensions/CollectionType+Shuffle.swift
+++ Ambatana/src/Helper/FoundationExtensions/CollectionType+Shuffle.swift
@@ -8,21 +8,21 @@
 
 extension Collection {
     /// Return a copy of `self` with its elements shuffled
-    func shuffle() -> [Iterator.Element] {
+    func shuffled() -> [Iterator.Element] {
         var list = Array(self)
-        list.shuffleInPlace()
+        list.shuffle()
         return list
     }
 }
 
 extension MutableCollection where Index == Int {
     /// Shuffle the elements of `self` in-place.
-    mutating func shuffleInPlace() {
+    mutating func shuffle() {
         // empty and single-element collections don't shuffle
         if count < 2 { return }
 
-        for i in 0..<count - 1 {
-            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+        for i in startIndex ..< endIndex - 1 {
+            let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
             guard i != j else { continue }
             swap(&self[i], &self[j])
         }
