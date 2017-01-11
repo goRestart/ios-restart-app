ba148a28fc4429fcbc27a559e43a49756737a31e
Albert Hernández López <albert@letgo.com>
2017-01-11 10:33:08 +0100

    hotfix: userReviews ab test default value to false

--- Ambatana/src/Manager/ABTest/ABTests.swift
+++ Ambatana/src/Manager/ABTest/ABTests.swift
@@ -24,7 +24,7 @@ public struct ABTests {
     static var shareAfterPosting = BoolABDynamicVar(key: "shareAfterPosting", defaultValue: false)
     static var postingMultiPictureEnabled = BoolABDynamicVar(key: "postingMultiPictureEnabled", defaultValue: false)
     static var periscopeImprovement = BoolABDynamicVar(key: "periscopeImprovement", defaultValue: false)
-    static var userReviews = BoolABDynamicVar(key: "userReviews", defaultValue: true)
+    static var userReviews = BoolABDynamicVar(key: "userReviews", defaultValue: false)
     static var newQuickAnswers = BoolABDynamicVar(key: "newQuickAnswers", defaultValue: false)
     static var favoriteWithBadgeOnProfile = BoolABDynamicVar(key: "favoriteWithBadgeOnProfile", defaultValue: false)
     static var favoriteWithBubbleToChat = BoolABDynamicVar(key: "favoriteWithBubbleToChat", defaultValue: false)
