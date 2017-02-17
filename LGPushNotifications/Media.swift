cec32378fa91211bcb3596b89ccd9aa668806509
Juan Iglesias <juan.iglesias@letgo.com>
2017-02-17 15:20:20 +0100

    ABIOS-2207: refactor pushService. Not need media-type since UINotificationAttachment handle all the types accepted.

--- LGPushNotifications/Media.swift
+++ LGPushNotifications/Media.swift
@@ -9,56 +9,29 @@
 import UserNotifications
 import UIKit
 
-public enum MediaType: String {
-    case image = "image"
-    case gif = "gif"
-    case video = "video"
-    case audio = "audio"
-}
 
 struct Media {
     private var data: Data
     private var ext: String
-    private var type: MediaType
     
-    init(forMediaType mediaType: MediaType, withData data: Data, fileExtension ext: String) {
-        self.type = mediaType
+    init(withData data: Data, fileExtension ext: String) {
         self.data = data
         self.ext = ext
     }
     
     var attachmentOptions: [String: Any?] {
-        switch(self.type) {
-        case .image:
-            return [UNNotificationAttachmentOptionsThumbnailClippingRectKey: CGRect(x: 0.0, y: 0.0, width: 1.0, height: 0.50).dictionaryRepresentation]
-        case .gif:
-            return [UNNotificationAttachmentOptionsThumbnailTimeKey: 0]
-        case .video:
-            return [UNNotificationAttachmentOptionsThumbnailTimeKey: 0]
-        case .audio:
-            return [UNNotificationAttachmentOptionsThumbnailHiddenKey: 1]
-        }
+        return [UNNotificationAttachmentOptionsThumbnailTimeKey: 0]
     }
     
     var fileIdentifier: String {
-        return self.type.rawValue
+        return self.ext
     }
     
-    var fileExt: String {
+    var fileExt: String? {
         if self.ext.characters.count > 0 {
             return self.ext
-        } else {
-            switch(self.type) {
-            case .image:
-                return "jpg"
-            case .gif:
-                return "gif"
-            case .video:
-                return "mp4"
-            case .audio:
-                return "mp3"
-            }
         }
+        return nil
     }
     
     var mediaData: Data? {
--- LGPushNotifications/NotificationService.swift
+++ LGPushNotifications/NotificationService.swift
@@ -10,6 +10,7 @@
 
 import UIKit
 import UserNotifications
+import MobileCoreServices
 
 class NotificationService: UNNotificationServiceExtension {
     
@@ -22,26 +23,23 @@ class NotificationService: UNNotificationServiceExtension {
         
         if let bestAttemptContent = bestAttemptContent {
             var urlResource: String?
-            var mediaType: MediaType?
             
             // Check if it is a leanplum notification
             if let leanplumURL = request.content.userInfo["LP_URL"] as? String {
                 urlResource = leanplumURL
-                mediaType = .image
             } else if let notificationData = request.content.userInfo["aps"] as? [String : Any], let notiData = notificationData["data"] as? [String : String] {
-                if let urlString = notiData["attachment-url"], let rawMediaType = notificationData["media-type"] as? String {
+                if let urlString = notiData["attachment-url"] {
                     urlResource = urlString
-                    mediaType = MediaType(rawValue: rawMediaType)
                 }
-        }
-        loadAttachment(forMediaType: mediaType, withUrlString: urlResource, completionHandler: { attachment in
-            if let attachment = attachment {
-                bestAttemptContent.attachments = [attachment]
             }
-            contentHandler(bestAttemptContent)
-        })
+            loadAttachment(withUrlString: urlResource, completionHandler: { attachment in
+                if let attachment = attachment {
+                    bestAttemptContent.attachments = [attachment]
+                }
+                contentHandler(bestAttemptContent)
+            })
+        }
     }
-}
     
     
     override func serviceExtensionTimeWillExpire() {
@@ -55,24 +53,20 @@ class NotificationService: UNNotificationServiceExtension {
 
 fileprivate extension UNNotificationAttachment {
     static func create(fromMedia media: Media) -> UNNotificationAttachment? {
+        guard let fileExtension = media.fileExt else { return nil }
+        guard let data = media.mediaData else { return nil }
+        
         let fileManager = FileManager.default
         let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
         let tmpSubFolderURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
-        do {
-            try fileManager.createDirectory(at: tmpSubFolderURL, withIntermediateDirectories: true, attributes: nil)
-            let fileIdentifier = "\(media.fileIdentifier).\(media.fileExt)"
-            let fileURL = tmpSubFolderURL.appendingPathComponent(fileIdentifier)
-            
-            guard let data = media.mediaData else {
-                return nil
-            }
+
+        try? fileManager.createDirectory(at: tmpSubFolderURL, withIntermediateDirectories: true, attributes: nil)
+        let fileIdentifier = "\(media.fileIdentifier).\(fileExtension)"
+        let fileURL = tmpSubFolderURL.appendingPathComponent(fileIdentifier)
         
-            try data.write(to: fileURL)
-            return self.create(fileIdentifier: fileIdentifier, fileUrl: fileURL, options: media.attachmentOptions)
-        } catch {
-            print("error " + error.localizedDescription)
-        }
-        return nil
+        try? data.write(to: fileURL)
+        return self.create(fileIdentifier: fileIdentifier, fileUrl: fileURL, options: media.attachmentOptions)
+
     }
     
     static func create(fileIdentifier: String, fileUrl: URL, options: [String : Any]? = nil) -> UNNotificationAttachment? {
@@ -85,23 +79,19 @@ private func resourceURL(forUrlString urlString: String) -> URL? {
     return URL(string: urlString)
 }
 
-fileprivate func loadAttachment(forMediaType mediaType: MediaType?, withUrlString urlString: String?, completionHandler: ((UNNotificationAttachment?) -> Void)) {
-    guard let mediaType = mediaType, let urlString = urlString else { return }
-    guard let url = resourceURL(forUrlString: urlString) else {
+fileprivate func loadAttachment(withUrlString urlString: String?, completionHandler: ((UNNotificationAttachment?) -> Void)) {
+    guard let urlString = urlString, let url = resourceURL(forUrlString: urlString) else {
         completionHandler(nil)
         return
     }
-    do {
-        let data = try Data(contentsOf: url)
-        let media = Media(forMediaType: mediaType, withData: data, fileExtension: url.pathExtension)
+    
+    if let data = try? Data(contentsOf: url) {
+        let media = Media(withData: data, fileExtension: url.pathExtension)
         if let attachment = UNNotificationAttachment.create(fromMedia: media) {
             completionHandler(attachment)
             return
         }
-        completionHandler(nil)
-    } catch {
-        print("error " + error.localizedDescription)
-        completionHandler(nil)
     }
+    completionHandler(nil)
 }
 
