64bf0b0279e1b50c94ab4c00724d3ea96444ffb5
Eli Kohen <eli.kohen@letgo.com>
2017-02-23 13:14:24 +0100

    ABIOS-2323 Added conversation list refresh after chat status becomes available

--- Ambatana/src/ViewControllers/Chat/ChatList/WSChatListViewModel.swift
+++ Ambatana/src/ViewControllers/Chat/ChatList/WSChatListViewModel.swift
@@ -141,6 +141,14 @@ class WSChatListViewModel: BaseChatGroupedListViewModel<ChatConversation>, ChatL
         }.bindNext { [weak self] event in
             self?.refresh(completion: nil)
         }.addDisposableTo(disposeBag)
+
+        chatRepository.chatStatus.bindNext { [weak self] wsChatStatus in
+            guard let strongSelf = self else { return }
+            //Reload messages if active, otherwise it will reload when active
+            if wsChatStatus.available && strongSelf.active {
+                strongSelf.refresh(completion: nil)
+            }
+        }.addDisposableTo(disposeBag)
     }
 }
 
