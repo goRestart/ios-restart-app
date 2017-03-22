0e90010657905ff0b6fa243143ba9b978e73d3e2
Juan Iglesias <juan.iglesias@letgo.com>
2017-03-22 14:37:58 +0100

    ABIOS-2097: add mockMediaPermissions

---
+++ letgoTests/Mock/Manager/MockMediaPermissions.swift
@@ -0,0 +1,24 @@
+//
+//  MockMediaPermissions.swift
+//  LetGo
+//
+//  Created by Juan Iglesias on 22/03/17.
+//  Copyright © 2017 Ambatana. All rights reserved.
+//
+
+
+@testable import LetGoGodMode
+import Foundation
+import RxSwift
+
+class MockMediaPermissions: MediaPermissions {
+    
+    var isCameraAvailable: Bool = false
+    var libraryAuthorizationStatus: PHAuthorizationStatus = .authorized
+    
+    func requestAccess(forMediaType mediaType: MediaType, completionHandler handler: @escaping ((Bool) -> Void)) { }
+    
+    func authorizationStatus(forMediaType mediaType: MediaType) -> AVAuthorizationStatus {
+        return .authorized
+    }
+}
