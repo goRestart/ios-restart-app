c26a45a4c36ecd9c212fffc15369224631f5e217
Nestor Garcia <nestor.garcia@letgo.com>
2017-05-09 17:46:58 +0200

    ABIOS-2598 refactor car details to separate content from navigation

--- Ambatana/src/ViewControllers/Sell/Post/PostDetails/PostCarDetailsView.swift
+++ Ambatana/src/ViewControllers/Sell/Post/PostDetails/PostCarDetailsView.swift
@@ -26,6 +26,9 @@ func ==(lhs: PostCarDetailState, rhs: PostCarDetailState) -> Bool {
 }
 
 class PostCarDetailsView: UIView {
+    let navigationView = UIView()
+    let contentView = UIView()
+    
     let navigationBackButton = UIButton()
     private let navigationTitle = UILabel()
     let navigationMakeButton = UIButton()
@@ -42,7 +45,7 @@ class PostCarDetailsView: UIView {
     private let priceRowEnabled: Bool
     
     private var progressTopConstraint: NSLayoutConstraint = NSLayoutConstraint()
-    private static let progressTopConstraintConstantSelectDetail = Metrics.screenHeight*1/3
+    private static let progressTopConstraintConstantSelectDetail = Metrics.screenHeight*1/4
     
     var state: PostCarDetailState = .selectDetail
     
@@ -58,6 +61,7 @@ class PostCarDetailsView: UIView {
         setupUI()
         setupAccessibilityIds()
         setupLayout()
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -73,6 +77,8 @@ class PostCarDetailsView: UIView {
     }
     
     private func setupUI() {
+        contentView.clipsToBounds = true
+        
         navigationBackButton.setImage(UIImage(named: "ic_back"), for: .normal)
         
         navigationTitle.font = UIFont.systemSemiBoldFont(size: 17)
@@ -124,53 +130,72 @@ class PostCarDetailsView: UIView {
     }
     
     private func setupLayout() {
-        let subviews = [navigationBackButton, navigationTitle, navigationMakeButton, navigationModelButton,
-                        navigationYearButton, navigationOkButton, descriptionLabel, progressView, makeRowView,
-                        modelRowView, yearRowView, priceRowView, doneButton, tableView]
-        setTranslatesAutoresizingMaskIntoConstraintsToFalse(for: subviews)
-        addSubviews(subviews)
+        let rootViews = [navigationView, contentView]
+        let navigationSubviews = [navigationBackButton, navigationTitle, navigationMakeButton, navigationModelButton,
+                                  navigationYearButton, navigationOkButton]
+        let contentSubviews = [descriptionLabel, progressView, makeRowView,
+                               modelRowView, yearRowView, priceRowView, doneButton, tableView]
+        setTranslatesAutoresizingMaskIntoConstraintsToFalse(for: rootViews)
+        setTranslatesAutoresizingMaskIntoConstraintsToFalse(for: navigationSubviews)
+        setTranslatesAutoresizingMaskIntoConstraintsToFalse(for: contentSubviews)
+        addSubviews(rootViews)
+        navigationView.addSubviews(navigationSubviews)
+        contentView.addSubviews(contentSubviews)
         
-        layoutMargins = UIEdgeInsets(top: 0, left: Metrics.margin*2, bottom: 0, right: Metrics.margin*2)
+        navigationView.layoutMargins = UIEdgeInsets(top: 0, left: Metrics.margin*2, bottom: 0, right: Metrics.margin*2)
+        contentView.layoutMargins = UIEdgeInsets(top: 0, left: Metrics.margin*2, bottom: 0, right: Metrics.margin*2)
         
-        navigationBackButton.layout(with: self)
+        navigationView.layout().height(44)
+        navigationView.layout(with: self)
+            .left()
+            .right()
+            .top()
+        navigationView.layout(with: contentView)
+            .bottom(to: .top)
+        contentView.layout(with: self)
+            .left()
+            .right()
+            .bottom()
+        
+        navigationBackButton.layout(with: navigationView)
             .left(by: Metrics.margin)
             .top(by: Metrics.margin)
-        navigationTitle.layout(with: self)
+        navigationTitle.layout(with: navigationView)
             .leading(to: .leadingMargin, by: Metrics.margin)
             .trailing(to: .trailingMargin, by: -Metrics.margin)
         navigationTitle.layout(with: navigationMakeButton)
             .centerY()
-        navigationMakeButton.layout(with: self)
+        navigationMakeButton.layout(with: navigationView)
             .leading(to: .leadingMargin, by: Metrics.margin, relatedBy: .greaterThanOrEqual)
             .top(by: Metrics.shortMargin)
         navigationMakeButton.layout(with: navigationModelButton)
             .trailing(to: .leading, by: -Metrics.margin)
         navigationModelButton.layout()
             .width(UIScreen.main.bounds.width*2/5, relatedBy: .lessThanOrEqual)
-        navigationModelButton.layout(with: self)
+        navigationModelButton.layout(with: navigationView)
             .top(by: Metrics.shortMargin)
             .centerX()
         navigationModelButton.layout(with: navigationYearButton)
             .trailing(to: .leading, by: -Metrics.margin)
-        navigationYearButton.layout(with: self)
+        navigationYearButton.layout(with: navigationView)
             .top(by: Metrics.shortMargin)
         navigationYearButton.layout(with: navigationOkButton)
             .trailing(to: .leading, by: -Metrics.margin, relatedBy: .lessThanOrEqual)
         navigationYearButton.setContentCompressionResistancePriority(UILayoutPriorityRequired, for: .horizontal)
         
-        navigationOkButton.layout(with: self)
+        navigationOkButton.layout(with: navigationView)
             .right(by: -Metrics.margin)
             .top(by: Metrics.shortMargin)
         navigationOkButton.setContentCompressionResistancePriority(UILayoutPriorityRequired, for: .horizontal)
         
-        descriptionLabel.layout(with: self)
+        descriptionLabel.layout(with: contentView)
             .leadingMargin()
             .trailingMargin()
         progressView.layout(with: descriptionLabel)
             .below(by: Metrics.bigMargin)
-        progressView.layout(with: self)
+        progressView.layout(with: contentView)
             .centerX()
-            .top(by: PostCarDetailsView.progressTopConstraintConstantSelectDetail, constraintBlock: { [weak self] in
+            .top(by: PostCarDetailsView.progressTopConstraintConstantSelectDetail, relatedBy: .equal, constraintBlock: { [weak self] in
                 self?.progressTopConstraint = $0
             })
         
@@ -178,21 +203,21 @@ class PostCarDetailsView: UIView {
             .below(by: Metrics.bigMargin)
         makeRowView.layout()
             .height(50)
-        makeRowView.layout(with: self)
+        makeRowView.layout(with: contentView)
             .leadingMargin()
             .trailingMargin()
         modelRowView.layout(with: makeRowView)
             .below()
         modelRowView.layout()
             .height(50)
-        modelRowView.layout(with: self)
+        modelRowView.layout(with: contentView)
             .leadingMargin()
             .trailingMargin()
         yearRowView.layout(with: modelRowView)
             .below()
         yearRowView.layout()
             .height(50)
-        yearRowView.layout(with: self)
+        yearRowView.layout(with: contentView)
             .leadingMargin()
             .trailingMargin()
         
@@ -201,7 +226,7 @@ class PostCarDetailsView: UIView {
                 .below()
             priceRowView.layout()
                 .height(50)
-            priceRowView.layout(with: self)
+            priceRowView.layout(with: contentView)
                 .leadingMargin()
                 .trailingMargin()
             doneButton.layout(with: priceRowView)
@@ -213,13 +238,13 @@ class PostCarDetailsView: UIView {
         
         doneButton.layout()
             .height(Metrics.buttonHeight)
-        doneButton.layout(with: self)
+        doneButton.layout(with: contentView)
             .leadingMargin()
             .trailingMargin()
         
         tableView.layout(with: progressView)
             .below(by: Metrics.bigMargin)
-        tableView.layout(with: self)
+        tableView.layout(with: contentView)
             .leading()
             .trailing()
             .bottom()
@@ -319,6 +344,14 @@ class PostCarDetailsView: UIView {
     
     // MARK: Animations
     
+    func moveContentUpward(by constant: CGFloat) {
+        if constant < 0 {
+            progressTopConstraint.constant = constant
+        } else {
+            progressTopConstraint.constant = PostCarDetailsView.progressTopConstraintConstantSelectDetail
+        }
+    }
+    
     private func selectDetailVisibleViews() -> [UIView] {
         return [navigationTitle, descriptionLabel, makeRowView, modelRowView, yearRowView, priceRowView, doneButton]
     }
@@ -361,7 +394,7 @@ class PostCarDetailsView: UIView {
             self.selectDetailValueVisibleViews().forEach { $0.alpha = 1 }
         }, completion: nil)
         
-        self.progressTopConstraint.constant = navigationTitle.frame.maxY + Metrics.veryBigMargin
+        self.progressTopConstraint.constant = Metrics.veryBigMargin
         UIView.animate(withDuration: 0.2, delay: 0.05, options: .curveEaseIn, animations: {
             self.layoutIfNeeded()
         }, completion: nil)
