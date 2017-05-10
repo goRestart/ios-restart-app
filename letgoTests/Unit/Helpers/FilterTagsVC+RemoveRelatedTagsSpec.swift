d4567d237d7c63ec106375ca966e771b7022c3e4
Dídac <didac@letgo.com>
2017-05-10 10:27:59 +0200

    ABIOS-2512 related tags removal UTs

--- letgoTests/Unit/Helpers/FilterTagsVC+RemoveRelatedTagsSpec.swift
+++ letgoTests/Unit/Helpers/FilterTagsVC+RemoveRelatedTagsSpec.swift
@@ -16,23 +16,64 @@ class FilterTagsVCRemoveRelatedTagsSpec: QuickSpec {
         var sut: FilterTagsViewController!
         var relatedTagsToRemove: [IndexPath]!
 
-        fdescribe("Remove Related Tags") {
+        describe("Remove Related Tags") {
             beforeEach {
-                sut = FilterTagsViewController(collectionView: UICollectionView())
+                let layout = UICollectionViewFlowLayout.init()
+                let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+                sut = FilterTagsViewController(collectionView: collectionView)
                 relatedTagsToRemove = []
             }
-            context("No related tags to remove") {
+            context("Removing not-category, no related tags to remove") {
                 beforeEach {
                     sut.tags = [.within(.week), .category(.homeAndGarden), .distance(distance: 10), .make(id: "makeId", name: "makeName"), .model(id: "modelId", name: "modelName")]
-
                     relatedTagsToRemove = sut.removeRelatedTags(forTag: .within(.week))
-
                 }
                 it ("tagsToRemove should be empty") {
                     expect(relatedTagsToRemove.isEmpty) == true
                 }
             }
-
+            context("Removing category, no related tags to remove") {
+                beforeEach {
+                    sut.tags = [.within(.week), .category(.homeAndGarden), .distance(distance: 10), .make(id: "makeId", name: "makeName"), .model(id: "modelId", name: "modelName")]
+                    relatedTagsToRemove = sut.removeRelatedTags(forTag: .category(.homeAndGarden))
+                }
+                it ("tagsToRemove should be empty") {
+                    expect(relatedTagsToRemove.isEmpty) == true
+                }
+            }
+            context("Removing category cars, no related tags to remove") {
+                beforeEach {
+                    sut.tags = [.within(.week), .category(.cars), .distance(distance: 10)]
+                    relatedTagsToRemove = sut.removeRelatedTags(forTag: .category(.cars))
+                }
+                it ("tagsToRemove should be empty") {
+                    expect(relatedTagsToRemove.isEmpty) == true
+                }
+            }
+            context("Removing category cars, 2 related tags to remove") {
+                beforeEach {
+                    sut.tags = [.within(.week), .category(.cars), .distance(distance: 10), .make(id: "makeId", name: "makeName"), .model(id: "modelId", name: "modelName")]
+                    relatedTagsToRemove = sut.removeRelatedTags(forTag: .category(.cars))
+                }
+                it ("tagsToRemove should not be empty") {
+                    expect(relatedTagsToRemove.isEmpty) == false
+                }
+                it ("tagsToRemove has indexes for make & model") {
+                    expect(relatedTagsToRemove) == [IndexPath(item: 3, section: 0), IndexPath(item: 4, section: 0)]
+                }
+            }
+            context("Removing make tag, 1 related tags to remove") {
+                beforeEach {
+                    sut.tags = [.within(.week), .category(.cars), .distance(distance: 10), .make(id: "makeId", name: "makeName"), .model(id: "modelId", name: "modelName")]
+                    relatedTagsToRemove = sut.removeRelatedTags(forTag: .make(id: "makeId", name: "makeName"))
+                }
+                it ("tagsToRemove should not be empty") {
+                    expect(relatedTagsToRemove.isEmpty) == false
+                }
+                it ("tagsToRemove has indexes for make & model") {
+                    expect(relatedTagsToRemove) == [IndexPath(item: 4, section: 0)]
+                }
+            }
         }
     }
 }
