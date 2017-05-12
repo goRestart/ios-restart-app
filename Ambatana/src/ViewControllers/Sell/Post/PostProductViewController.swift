ddaf1c0b5572d13cdba9306ee088b410794739ec
Juan Iglesias <juan.iglesias@letgo.com>
2017-05-12 12:07:35 +0200

    ABIOS-2620: add logic to show/hide loading view on small devices.

--- Ambatana/src/ViewControllers/Sell/Post/PostProductViewController.swift
+++ Ambatana/src/ViewControllers/Sell/Post/PostProductViewController.swift
@@ -352,22 +352,24 @@ class PostProductViewController: BaseViewController, PostProductViewModelDelegat
             guard origin > 0 else { return }
             guard let strongSelf = self else { return }
             let keyboardHeight = origin - strongSelf.view.height
-            strongSelf.detailsContainerBottomConstraint.constant = keyboardHeight/2
+            guard let viewHeight = self?.view.height else { return }
+            self?.detailsContainerBottomConstraint.constant = (origin - viewHeight)/2
             if strongSelf.carDetailsView.state == .selectDetail {
                 strongSelf.carDetailsView.moveContentUpward(by: keyboardHeight)
             }
             UIView.animate(withDuration: Double(strongSelf.keyboardHelper.animationTime), animations: {
                 strongSelf.view.layoutIfNeeded()
             })
-            let showingKeyboard = keyboardHeight > 0
-            strongSelf.loadingViewHidden(hide: !showingKeyboard)
+            let showingKeyboard = keyboardHeight >= 0
+            strongSelf.loadingViewHidden(show: showingKeyboard)
         }.addDisposableTo(disposeBag)
     }
     
-    private func loadingViewHidden(hide: Bool) {
+    private func loadingViewHidden(show: Bool) {
         guard !DeviceFamily.current.isWiderOrEqualThan(.iPhone6) else { return }
+        guard !priceView.isHidden else { return }
         UIView.animate(withDuration: 0.3, animations: { () -> Void in
-            self.customLoadingView.alpha = hide ? 0.0 : 1.0
+            self.customLoadingView.alpha = show ? 1.0 : 0.0
         })
     }
 }
@@ -603,16 +605,17 @@ extension PostProductViewController {
         } else if state.priceViewShouldResignFirstResponder() {
             priceView.resignFirstResponder()
         }
- 
     }
     
     private func viewToAdjustDetailsScrollContentInset(state: PostListingState) -> UIView? {
         switch state.step {
+        case .detailsSelection:
+            return customLoadingView
         case .categorySelection:
             return categorySelectionView
         case .carDetailsSelection:
             return carDetailsView
-        case .imageSelection, .errorUpload, .finished,.detailsSelection, .uploadSuccess, .uploadingImage:
+        case .imageSelection, .uploadingImage, .errorUpload, .finished, .uploadSuccess:
             return nil
         }
     }
