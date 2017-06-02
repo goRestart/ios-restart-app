bcf4af6b03d1071641f84c730497942c09953ec5
Dídac <didac@letgo.com>
2017-06-02 16:45:47 +0200

    Added tests for services errors

--- letgoTests/Unit/ViewModel/LocationFromZipCodeViewModelSpec.swift
+++ letgoTests/Unit/ViewModel/LocationFromZipCodeViewModelSpec.swift
@@ -14,7 +14,7 @@ import LGCoreKit
 class LocationFromZipCodeViewModelSpec: BaseViewModelSpec {
 
     override func spec() {
-        fdescribe("LocationFromZipCodeViewModelSpec") {
+        describe("LocationFromZipCodeViewModelSpec") {
 
             var locationManager: LocationManager!
             var searchService: MockSearchLocationSuggestionsService!
@@ -23,129 +23,218 @@ class LocationFromZipCodeViewModelSpec: BaseViewModelSpec {
             var sut: LocationFromZipCodeViewModel!
 
             context ("no initial place") {
-                beforeEach {
-                    locationManager = MockLocationManager()
-
-                    let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
-                    let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
-                    let place = Place(postalAddress: postalAddress, location: location)
-
-                    searchService = MockSearchLocationSuggestionsService(value: [place])
-
-                    postalAddressService = MockPostalAddressRetrievalService(value: place)
-
-                    sut = LocationFromZipCodeViewModel(initialPlace: nil,
-                                                       locationManager: locationManager,
-                                                       searchService: searchService,
-                                                       postalAddressService: postalAddressService)
-                }
-                context ("zip code has a correct format") {
+                context ("services work ok") {
                     beforeEach {
-                        sut.zipCode.value = "12345"
-                    }
-                    context ("get address from zip code") {
-                        beforeEach {
-                            sut.updateAddressFromZipCode()
+                        locationManager = MockLocationManager()
 
+                        let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
+                        let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
+                        let place = Place(postalAddress: postalAddress, location: location)
+
+                        searchService = MockSearchLocationSuggestionsService(value: [place])
+
+                        postalAddressService = MockPostalAddressRetrievalService(value: place)
+
+                        sut = LocationFromZipCodeViewModel(initialPlace: nil,
+                                                           locationManager: locationManager,
+                                                           searchService: searchService,
+                                                           postalAddressService: postalAddressService)
+                    }
+                    context ("zip code has a correct format") {
+                        beforeEach {
+                            sut.zipCode.value = "12345"
                         }
-                        it ("full address has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
                         }
                     }
-                    context ("get Address From Current Location") {
+                    context ("zip code has NOT a correct format") {
                         beforeEach {
-                            sut.updateAddressFromCurrentLocation()
+                            sut.zipCode.value = "abcd"
                         }
-                        it ("full address has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("there's no full address") {
+                                expect(sut.fullAddress.value).toEventually(beNil())
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
                         }
                     }
                 }
-                context ("zip code has NOT a correct format") {
+                context ("services don't work") {
                     beforeEach {
-                        sut.zipCode.value = "abcd"
-                    }
-                    context ("get address from zip code") {
-                        beforeEach {
-                            sut.updateAddressFromZipCode()
+                        locationManager = MockLocationManager()
 
-                        }
-                        it ("there's no full address") {
-                            expect(sut.fullAddress.value).toEventually(beNil())
-                        }
+                        let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
+                        let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
+                        let place = Place(postalAddress: postalAddress, location: location)
+
+                        searchService = MockSearchLocationSuggestionsService(error: .notFound)
+
+                        postalAddressService = MockPostalAddressRetrievalService(error: .network)
+
+                        sut = LocationFromZipCodeViewModel(initialPlace: nil,
+                                                           locationManager: locationManager,
+                                                           searchService: searchService,
+                                                           postalAddressService: postalAddressService)
                     }
-                    context ("get Address From Current Location") {
+                    context ("zip code has a correct format") {
                         beforeEach {
-                            sut.updateAddressFromCurrentLocation()
+                            sut.zipCode.value = "12345"
                         }
-                        it ("full address has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(beNil())
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(beNil())
+                            }
                         }
                     }
+
                 }
             }
             context ("we have an initial place") {
-                beforeEach {
-                    locationManager = MockLocationManager()
-
-                    let initialPostalAddress = PostalAddress(address: "", city: "Palo Bajo", zipCode: "06660", state: "", countryCode: "us", country: "")
-                    let initialLocation = LGLocationCoordinates2D(latitude: 43.38, longitude: 12.18)
-                    let initialPlace = Place(postalAddress: initialPostalAddress, location: initialLocation)
-
-                    let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
-                    let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
-                    let resultPlace = Place(postalAddress: postalAddress, location: location)
-
-                    searchService = MockSearchLocationSuggestionsService(value: [resultPlace])
-
-                    postalAddressService = MockPostalAddressRetrievalService(value: resultPlace)
-
-                    sut = LocationFromZipCodeViewModel(initialPlace: initialPlace,
-                                                       locationManager: locationManager,
-                                                       searchService: searchService,
-                                                       postalAddressService: postalAddressService)
-                }
-                context ("zip code has a correct format") {
+                context ("services work OK") {
                     beforeEach {
-                        sut.zipCode.value = "12345"
-                    }
-                    context ("get address from zip code") {
-                        beforeEach {
-                            sut.updateAddressFromZipCode()
+                        locationManager = MockLocationManager()
 
+                        let initialPostalAddress = PostalAddress(address: "", city: "Palo Bajo", zipCode: "06660", state: "", countryCode: "us", country: "")
+                        let initialLocation = LGLocationCoordinates2D(latitude: 43.38, longitude: 12.18)
+                        let initialPlace = Place(postalAddress: initialPostalAddress, location: initialLocation)
+
+                        let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
+                        let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
+                        let resultPlace = Place(postalAddress: postalAddress, location: location)
+
+                        searchService = MockSearchLocationSuggestionsService(value: [resultPlace])
+
+                        postalAddressService = MockPostalAddressRetrievalService(value: resultPlace)
+
+                        sut = LocationFromZipCodeViewModel(initialPlace: initialPlace,
+                                                           locationManager: locationManager,
+                                                           searchService: searchService,
+                                                           postalAddressService: postalAddressService)
+                    }
+                    context ("zip code has a correct format") {
+                        beforeEach {
+                            sut.zipCode.value = "12345"
                         }
-                        it ("full address has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
                         }
                     }
-                    context ("get Address From Current Location") {
+                    context ("zip code has NOT a correct format") {
                         beforeEach {
-                            sut.updateAddressFromCurrentLocation()
+                            sut.zipCode.value = "abcd"
                         }
-                        it ("full address has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("full address is the initial one, has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("06660, Palo Bajo"))
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address is the initial one, has the zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                            }
                         }
                     }
                 }
-                context ("zip code has NOT a correct format") {
+                context ("services don't work") {
                     beforeEach {
-                        sut.zipCode.value = "abcd"
-                    }
-                    context ("get address from zip code") {
-                        beforeEach {
-                            sut.updateAddressFromZipCode()
+                        locationManager = MockLocationManager()
 
-                        }
-                        it ("full address is the initial one, has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("06660, Palo Bajo"))
-                        }
+                        let initialPostalAddress = PostalAddress(address: "", city: "Palo Bajo", zipCode: "06660", state: "", countryCode: "us", country: "")
+                        let initialLocation = LGLocationCoordinates2D(latitude: 43.38, longitude: 12.18)
+                        let initialPlace = Place(postalAddress: initialPostalAddress, location: initialLocation)
+
+                        let postalAddress = PostalAddress(address: "", city: "New York", zipCode: "12345", state: "", countryCode: "us", country: "")
+                        let location = LGLocationCoordinates2D(latitude: 41.38, longitude: 2.18)
+                        let place = Place(postalAddress: postalAddress, location: location)
+
+                        searchService = MockSearchLocationSuggestionsService(error: .notFound)
+
+                        postalAddressService = MockPostalAddressRetrievalService(error: .network)
+
+                        sut = LocationFromZipCodeViewModel(initialPlace: initialPlace,
+                                                           locationManager: locationManager,
+                                                           searchService: searchService,
+                                                           postalAddressService: postalAddressService)
                     }
-                    context ("get Address From Current Location") {
+                    context ("zip code has a correct format") {
                         beforeEach {
-                            sut.updateAddressFromCurrentLocation()
+                            sut.zipCode.value = "12345"
                         }
-                        it ("full address is the initial one, has the zip and city separated by comma") {
-                            expect(sut.fullAddress.value).toEventually(equal("12345, New York"))
+                        context ("get address from zip code") {
+                            beforeEach {
+                                sut.updateAddressFromZipCode()
+
+                            }
+                            it ("full address has the initial zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("06660, Palo Bajo"))
+                            }
+                        }
+                        context ("get Address From Current Location") {
+                            beforeEach {
+                                sut.updateAddressFromCurrentLocation()
+                            }
+                            it ("full address has the initial zip and city separated by comma") {
+                                expect(sut.fullAddress.value).toEventually(equal("06660, Palo Bajo"))
+                            }
                         }
                     }
                 }
