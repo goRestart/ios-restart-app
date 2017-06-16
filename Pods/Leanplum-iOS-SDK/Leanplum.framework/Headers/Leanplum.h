0cc286086660da121f7620cfa546b557097a3b39
Juan Iglesias <juan.iglesias@letgo.com>
2017-06-16 16:22:19 +0200

    ABIOS-2676: added new leanplum sdk. 2.0.1

--- Pods/Leanplum-iOS-SDK/Leanplum.framework/Headers/Leanplum.h
+++ Pods/Leanplum-iOS-SDK/Leanplum.framework/Headers/Leanplum.h
@@ -1,9 +1,25 @@
 //
 //  Leanplum.h
-//  Leanplum iOS SDK Version 1.6.0
+//  Leanplum iOS SDK Version 2.0.1
 //
-//  Copyright (c) 2016 Leanplum. All rights reserved.
+//  Copyright (c) 2012 Leanplum, Inc. All rights reserved.
 //
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
@@ -213,12 +229,12 @@ typedef enum {
  * Needed in development mode to enable the interface editor, as well as in production to allow
  * changes to be applied.
  */
-+ (void)allowInterfaceEditing;
++ (void)allowInterfaceEditing __attribute__((deprecated("Use LeanplumUIEditor pod instead.")));
 
 /**
  * Check if interface editing is enabled.
  */
-+ (BOOL)interfaceEditingEnabled;
++ (BOOL)interfaceEditingEnabled __attribute__((deprecated("Use LeanplumUIEditor pod instead.")));
 
 /**@}*/
 
@@ -879,6 +895,15 @@ typedef enum {
  * Tracks an event in the context of the current message.
  */
 - (void)track:(NSString *)event withValue:(double)value andParameters:(NSDictionary *)params;
+
+/**
+ * Tracks an event in the conext of the current message, with any parent actions prepended to the
+ * message event name.
+ */
+- (void)trackMessageEvent:(NSString *)event
+                withValue:(double)value
+                  andInfo:(NSString *)info
+            andParameters:(NSDictionary *)params;
 
 /**
  * Prevents the currently active message from appearing again in the future.
