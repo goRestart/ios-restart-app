e24d2f3779c115c7376a5d3f7ab72bf79ac034c0
nestorgt <nestor.garcia@letgo.com>
2017-06-16 14:35:51 +0200

    ABIOS-2635 update core kit

--- Pods/LGCoreKit/mocchacino/Randomizable/UIKit Types/UIImage+Randomizable.swift
+++ Pods/LGCoreKit/mocchacino/Randomizable/UIKit Types/UIImage+Randomizable.swift
@@ -15,32 +15,13 @@ public extension UIImage {
                             blue: CGFloat(Float.makeRandom(min: 0, max: 1)),
                             alpha: 1)
         
-        if let image = UIImage.imageWithColor(color, size: rect.size) {
-            return self.init(cgImage: image.cgImage!)
-        } else {
-            return self.init(cgImage: UIImage().cgImage!)
-        }
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
         
-        
-//        UIGraphicsBeginImageContext(rect.size)
-//        let context = UIGraphicsGetCurrentContext()
-//        context?.setFillColor(color.cgColor)
-//        context?.fill(rect)
-//        
-//        let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-//        UIGraphicsEndImageContext()
-//        return self.init(cgImage: image.cgImage!)
-    }
-}
-
-extension UIImage {
-    static func imageWithColor(_ color: UIColor, size: CGSize) -> UIImage? {
-        let rect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
+        let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
         UIGraphicsEndImageContext()
-        return image
+        return self.init(cgImage: image.cgImage!)
     }
 }
