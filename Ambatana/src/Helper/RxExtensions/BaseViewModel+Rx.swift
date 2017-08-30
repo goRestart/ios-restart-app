6f712623d38125c84ff3bff004989ecdf36a82f1
Albert Hernández López <albert@letgo.com>
2017-08-30 12:06:42 +0200

    ABIOs-2955 added base view model reactive extension to bind active var

---
+++ Ambatana/src/Helper/RxExtensions/BaseViewModel+Rx.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewModel+Rx.swift
+//  LetGo
+//
+//  Created by Albert Hernández López on 30/08/2017.
+//  Copyright © 2017 Ambatana. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: BaseViewModel {
+    var active: UIBindingObserver<Base, Bool> {
+        return UIBindingObserver<Base, Bool>(UIElement: self.base) { (view, active) -> () in
+            view.active = active
+        }
+    }
+}
