eafb1219fa202b1f1a3f16e39b9be2cc5858ed23
nestorgt <nestor.garcia@letgo.com>
2017-09-26 11:23:28 +0200

    HOTFIX fix assets cleaning not being commited

--- fastlane/actions/lg_assets.rb
+++ fastlane/actions/lg_assets.rb
@@ -9,20 +9,36 @@ module Fastlane
         path_to_assets = path_to_repository + "Ambatana/res/img"
         path_to_src = path_to_repository + "Ambatana/src"
 
+        shouldCommit = false
         Dir[path_to_assets + "/*.xcassets"].each do |asset_path|
           Dir[asset_path + "/*.imageset"].each do |imageset_path|
             filename = File.basename(imageset_path, ".*")
             ocurrences = find_text(path_to_src, filename)
             if (ocurrences == 0)
               puts "removing asset: " + imageset_path
-              FileUtils.rm_rf Dir.glob("#{imageset_path}/*")
+              FileUtils.rm_rf Dir.glob("#{imageset_path}")
+              shouldCommit = true
             end
           end
+        end
+        if shouldCommit
+          pushChanges(path_to_repository, path_to_assets)  
         end
       end
 
       def self.find_text(path, text) # -> int
         return `grep -rnw #{path} -e #{text} | wc -l`.to_i
+      end
+
+      def self.pushChanges(path_to_repository, path_to_assets)
+        UI.message "Pushing changes...".blue
+        pushChangeCommand = "(cd #{path_to_repository}" +
+          " && git add #{path_to_assets}" +
+          " && git commit -m 'remove un-used assets'" +
+          " && git pull && git push)"
+        UI.message pushChangeCommand
+        Actions.sh pushChangeCommand
+        UI.success "Version changes pushed successfully!"
       end
 
       #####################################################
