1c3b5638455d991e65ab363c856c718cd8f82654
Raúl de Oñate <raul.deonate@letgo.com>
2017-10-11 10:48:21 +0200

    ABIOS-2931 millisecondsSince1970 from Double type to TimeInterval

--- Ambatana/src/Helper/FoundationExtensions/Date+LG.swift
+++ Ambatana/src/Helper/FoundationExtensions/Date+LG.swift
@@ -180,7 +180,7 @@ extension Date {
         return Calendar.current.component(.year, from: self)
     }
     
-    var millisecondsSince1970: Double {
+    var millisecondsSince1970: TimeInterval {
         return (self.timeIntervalSince1970 * 1000.0).rounded()
     }
 }
