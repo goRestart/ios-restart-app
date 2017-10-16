cc9c26453a84433d00ea56e2bf065410fe16528e
Facundo Menzella <facumenzella@gmail.com>
2017-10-16 14:52:12 +0200

    Removed alert when not necessary

--- Ambatana/src/Coordinator/Tabs/TabNavigator.swift
+++ Ambatana/src/Coordinator/Tabs/TabNavigator.swift
@@ -55,7 +55,7 @@ protocol ListingDetailNavigator: TabNavigator {
     func closeProductDetail()
     func editListing(_ listing: Listing)
     func openListingChat(_ listing: Listing, source: EventParameterTypePage)
-    func closeAfterDelete()
+    func closeListingAfterDelete(_ listing: Listing)
     func openFreeBumpUp(forListing listing: Listing, socialMessage: SocialMessage, paymentItemId: String)
     func openPayBumpUp(forListing listing: Listing,
                        purchaseableProduct: PurchaseableProduct,
