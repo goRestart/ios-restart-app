58367298860001cff8d09632dcc3f2373f2487e1
Juan Iglesias <juan.iglesias@letgo.com>
2017-10-27 13:42:43 +0200

    ABIOS-3081 allow updating price from addingDetails. added tests to PostListingStateSpec

--- letgoTests/Unit/ViewModel/PostListingStateSpec.swift
+++ letgoTests/Unit/ViewModel/PostListingStateSpec.swift
@@ -200,6 +200,40 @@ class PostProductStateSpec: BaseViewModelSpec {
                     }
                 }
                 
+                fcontext("adding details") {
+                    beforeEach {
+                        sut = PostListingState(postCategory: .realEstate)
+                        sut = sut.updatingStepToUploadingImages()
+                        sut = sut.updatingToSuccessUpload(uploadedImages: [MockFile].makeMocks())
+                        sut = sut.updatingAfterUploadingSuccess()
+                        sut = sut.updating(price: ListingPrice.makeMock())
+                    }
+                    
+                    it("returns the same state when updating category") {
+                        expect(sut.updating(category: .realEstate)) === sut
+                    }
+                    
+                    it("returns the same state when updating pending to upload images") {
+                        expect(sut.updating(pendingToUploadImages: [UIImage].makeRandom())) === sut
+                    }
+                    
+                    it("returns the same state when updating uploaded images") {
+                        expect(sut.updatingToSuccessUpload(uploadedImages: [MockFile].makeMocks())) === sut
+                    }
+                    
+                    it("returns the same state when updating upload error") {
+                        expect(sut.updating(uploadError: .notFound)) === sut
+                    }
+                    
+                    it("returns a new state with different price") {
+                        expect(sut.updating(price: ListingPrice.makeMock())) !== sut
+                    }
+                    
+                    it("returns the same state when updating car info") {
+                        expect(sut.updating(carInfo: CarAttributes.emptyCarAttributes())) === sut
+                    }
+                }
+                
                 context("details selection") {
                     beforeEach {
                         sut = sut
