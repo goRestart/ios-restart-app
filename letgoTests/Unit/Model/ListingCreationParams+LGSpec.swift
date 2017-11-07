e46257e6cae132b81ccb4cdb6da7ddea94191cab
Juan Iglesias <juan.iglesias@letgo.com>
2017-11-07 14:33:45 +0100

    ABIOS-3168 include price when params creation. tests

---
+++ letgoTests/Unit/Model/ListingCreationParams+LGSpec.swift
@@ -0,0 +1,77 @@
+//
+//  ListingCreationParamsSpec.swift
+//  LetGo
+//
+//  Created by Juan Iglesias on 07/11/2017.
+//  Copyright © 2017 Ambatana. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Argo
+import LGCoreKit
+@testable import LetGoGodMode
+
+
+class ListingCreationParamsLGSpec: QuickSpec {
+    
+    override func spec() {
+        
+        var sut : ListingCreationParams!
+        
+        describe("make with price") {
+            beforeEach {
+                let postListingState = PostListingState(postCategory: .motorsAndAccessories)
+                postListingState = postListingState.updatingStepToUploadingImages()
+                postListingState = postListingState.updatingToSuccessUpload(uploadedImages: [MockFile].makeMocks())
+                postListingState = postListingState.updatingAfterUploadingSuccess()
+                postListingState = postListingState.updating(price: .normal(100))
+                sut = ListingCreationParams.make(title: "title",
+                                                 description: "description",
+                                                 currency: Currency.makeMock(),
+                                                 location: LGLocationCoordinates2D.makeMock(),
+                                                 postalAddress: PostalAddress.makeMock(),
+                                                 postListingState: postListingState)
+            }
+            it("price is normal 100") {
+                expect(sut.price).to(equal(.normal(100)))
+            }
+        }
+        describe("make with negotiable") {
+            beforeEach {
+                let postListingState = PostListingState(postCategory: .motorsAndAccessories)
+                postListingState = postListingState.updatingStepToUploadingImages()
+                postListingState = postListingState.updatingToSuccessUpload(uploadedImages: [MockFile].makeMocks())
+                postListingState = postListingState.updatingAfterUploadingSuccess()
+                postListingState = postListingState.updating(price: .negotiable(0))
+                sut = ListingCreationParams.make(title: "title",
+                                                 description: "description",
+                                                 currency: Currency.makeMock(),
+                                                 location: LGLocationCoordinates2D.makeMock(),
+                                                 postalAddress: PostalAddress.makeMock(),
+                                                 postListingState: postListingState)
+            }
+            it("price is negotiable zero") {
+                expect(sut.price).to(equal(.negotiable(0)))
+            }
+        }
+        describe("make with free") {
+            beforeEach {
+                let postListingState = PostListingState(postCategory: .motorsAndAccessories)
+                postListingState = postListingState.updatingStepToUploadingImages()
+                postListingState = postListingState.updatingToSuccessUpload(uploadedImages: [MockFile].makeMocks())
+                postListingState = postListingState.updatingAfterUploadingSuccess()
+                postListingState = postListingState.updating(price: .free)
+                sut = ListingCreationParams.make(title: "title",
+                                                 description: "description",
+                                                 currency: Currency.makeMock(),
+                                                 location: LGLocationCoordinates2D.makeMock(),
+                                                 postalAddress: PostalAddress.makeMock(),
+                                                 postListingState: postListingState)
+            }
+            it("price is negotiable zero") {
+                expect(sut.price).to(equal(.free))
+            }
+        }
+    }
+}
