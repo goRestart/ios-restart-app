6f6929dfb236d410a8e46b6931b68943a21ef198
Juan Iglesias <juan.iglesias@letgo.com>
2017-11-28 14:14:07 +0100

    ABIOS-3080: use constants instead of direct numbers.

--- Ambatana/src/ViewControllers/Sell/Post/PostListingViewController.swift
+++ Ambatana/src/ViewControllers/Sell/Post/PostListingViewController.swift
@@ -215,8 +215,8 @@ class PostListingViewController: BaseViewController, PostListingViewModelDelegat
     
     private func setupLoadingStackView() {
         guard let category = viewModel.postCategory, category == .realEstate else { return }
-        loadingViewRealEstate = LoadingIndicator(frame: CGRect(x: 0, y: 0, width: PostListingViewController.loadingViewHeight, height: PostListingViewController.loadingViewHeight))
-        loadingViewRealEstate?.layout().height(100).width(PostListingViewController.loadingViewWidth)
+        loadingViewRealEstate = LoadingIndicator(frame: CGRect(x: 0, y: 0, width: PostListingViewController.loadingViewWidth, height: PostListingViewController.loadingViewHeight))
+        loadingViewRealEstate?.layout().height(PostListingViewController.loadingViewHeight).width(PostListingViewController.loadingViewWidth)
         if let loadingView = loadingViewRealEstate {
             uploadImageStackView.addArrangedSubview(loadingView)
         }
