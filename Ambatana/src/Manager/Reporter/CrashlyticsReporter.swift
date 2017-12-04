bdff79ccadf2ae38b322856be3f0c0470f5e62bd
nestorgt <nestor.garcia@letgo.com>
2017-12-04 16:22:40 +0100

    ABIOS-3099 fix @objc inference

--- Ambatana/src/Manager/Reporter/CrashlyticsReporter.swift
+++ Ambatana/src/Manager/Reporter/CrashlyticsReporter.swift
@@ -14,8 +14,8 @@ class CrashlyticsReporter: Reporter {
     static let appDomain: Domain = "com.letgo.ios"
 
     func report(_ domain: Domain, code: Int, message: String) {
-        let userInfo: [AnyHashable: Any] = ["message": message]
-        let error = NSError(domain: domain, code: code, userInfo: userInfo as? [AnyHashable : Any])
+        let userInfo: [String: Any] = ["message": message]
+        let error = NSError(domain: domain, code: code, userInfo: userInfo)
         Crashlytics.sharedInstance().recordError(error)
     }
 }
