2194adc3ea7f4b29a8df44e9a683c87dfdcee331
Juan Iglesias <juan.iglesias@letgo.com>
2017-12-12 11:19:27 +0100

    ABIOS-2309: update corekit to 3.27.2

--- Pods/LGCoreKit/LGCoreKit/Model/ListingRepositoryParams.swift
+++ Pods/LGCoreKit/LGCoreKit/Model/ListingRepositoryParams.swift
@@ -91,8 +91,12 @@ public struct RetrieveListingParams {
         params["since"] = timeCriteria?.string
         
         // Real Estate attributes
-        params["propertyType"] = [propertyType ?? nil].flatMap { $0 }
-        params["offerType"] = [offerType ?? nil].flatMap { $0 }
+        if let propertyType = propertyType {
+            params["propertyType"] = [propertyType]
+        }
+        if let offerType = offerType {
+            params["offerType"] = [offerType]
+        }
         params["numberOfBedrooms"] = numberOfBedrooms
         params["numberOfBathrooms"] = numberOfBathrooms
        
