ba03d4d4460ba291e4566b6b506d7891051f8942
nestorgt <nestor.garcia@letgo.com>
2017-12-14 13:56:57 +0100

    ABIOS-3099 rename optionalText to text

--- Ambatana/src/ViewControllers/User/UserViewController.swift
+++ Ambatana/src/ViewControllers/User/UserViewController.swift
@@ -438,7 +438,7 @@ extension UserViewController {
     private func setupHeaderRxBindings() {
         // Name, location, avatar & bg
         viewModel.userName.asObservable().bind(to: userNameLabel.rx.text).disposed(by: disposeBag)
-        viewModel.userLocation.asObservable().bind(to: userLocationLabel.rx.optionalText).disposed(by: disposeBag)
+        viewModel.userLocation.asObservable().bind(to: userLocationLabel.rx.text).disposed(by: disposeBag)
 
         Observable.combineLatest(viewModel.userAvatarURL.asObservable(),
             viewModel.userAvatarPlaceholder.asObservable()) { ($0, $1) }
