ee6a930a8dde03ffc5357fd055ceee607ac8ec20
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-24 12:59:47 +0100

    Initialize project 🚀

---
+++ RestartTests/RestartTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Restart
+
+class RestartTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testExample() {}
+}
