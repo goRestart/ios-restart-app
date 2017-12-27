f3ab42d6f81bdfc26d93c0ed36affbcea90d28cb
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-27 16:03:21 +0100

    Add playground with use cases

---
+++ App/Features/Foundation/Application/Application/Playgrounds/Playground.playground/Pages/Authenticate.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+import Application
+import Domain
+import PlaygroundSupport
+
+/*:
+ ## Authenticate user (Login)
+ 
+ This use case is usted to authenticate an user with **Restart** backend. It uses `BasicCredentials` to authenticate with the server.
+ */
+
+// ⚠️ Update with your credentials
+let credentials = BasicCredentials(
+  username: "username",
+  password: "password"
+)
+
+async { fulfill in
+  
+  let authenticate = Authenticate()
+  authenticate.execute(with: credentials).subscribe(onCompleted: {
+    print("Logged in as `\(credentials.username)` 🖖🏽")
+    fulfill()
+  }) { error in
+    switch error as? AuthError {
+    case .invalidCredentials?:
+      print("🚨 Invalid credentials = \(credentials)")
+    default:
+      print("Unknown error")
+    }
+    fulfill()
+  }
+}
+//: [Previous](@previous)
+//: [Next](@next)
---
+++ App/Features/Foundation/Application/Application/Playgrounds/Playground.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+/*:
+ # Restart
+ ## List of available use cases for working with the Restart application
+ 
+ - [Authenticate user](Authenticate)
+
+ */
