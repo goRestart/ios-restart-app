5d41f50a9b4911873bd04d696631b0429eb0822e
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-28 20:28:39 +0100

    Add style structs for Margin & Radius

---
+++ App/Features/Foundation/UI/UI/Components/Style/Margin.swift
@@ -0,0 +1,8 @@
+public struct Margin {
+  public static let smallest = 4
+  public static let small = 8
+  public static let medium = 16
+  public static let big = 20
+  public static let huge = 24
+  public static let `super` = 32
+}
---
+++ App/Features/Foundation/UI/UI/Components/Style/Radius.swift
@@ -0,0 +1,5 @@
+public struct Radius {
+  public static let small = 2
+  public static let medium = 4
+  public static let big = 8
+}
