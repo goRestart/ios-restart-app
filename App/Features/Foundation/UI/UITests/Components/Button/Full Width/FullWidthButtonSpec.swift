46a1c573685e75db0946d3e2733696f540412a21
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-28 23:15:35 +0100

    Add tests for FullWidthButton with all the button states

---
+++ App/Features/Foundation/UI/UITests/Components/Button/Full Width/FullWidthButtonSpec.swift
@@ -0,0 +1,35 @@
+import XCTest
+import Snap
+@testable import UI
+
+class FullWidthButtonSpec: XCTestCase {
+
+  func test_button_with_normal_state_is_valid() {
+    let button = givenButton(named: "normal", for: .normal)
+    expect(button).toMatchSnapshot(named: "normal_button")
+  }
+
+  func test_button_with_highlighted_state_is_valid() {
+    let button = givenButton(named: "highlighted", for: .highlighted)
+    expect(button).toMatchSnapshot(named: "highlighted_button")
+  }
+  
+  func test_button_with_disabled_state_is_valid() {
+    let button = givenButton(named: "disabled", for: .disabled)
+    expect(button).toMatchSnapshot(named: "disabled_button")
+  }
+  
+  private func givenButton(named: String, for state: UIControlState) -> FullWidthButton {
+    let button = FullWidthButton(frame: CGRect(origin: .zero, size: CGSize(width: 375, height: 56)))
+    button.setTitle(named.uppercased(), for: state)
+    switch state {
+    case .highlighted:
+      button.isHighlighted = true
+    case .disabled:
+      button.isEnabled = false
+    default: break
+    }
+    button.layoutIfNeeded()
+    return button
+  }
+}
