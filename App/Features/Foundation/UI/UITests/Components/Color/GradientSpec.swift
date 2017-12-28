4452d7991bed1c8be5bf5870b4427b3eae362a50
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-28 19:56:09 +0100

    Update color palette using color literals as these are more visual when coding

--- App/Features/Foundation/UI/UITests/Components/Color/GradientSpec.swift
+++ App/Features/Foundation/UI/UITests/Components/Color/GradientSpec.swift
@@ -3,16 +3,14 @@ import Snap
 @testable import UI
 
 class GradientSpec: XCTestCase {
- 
+
   func test_default_gradient_is_valid() {
-    expect(view).toMatchSnapshot(named: "default_gradient")
+    expect(gradient).toMatchSnapshot(named: "default_gradient")
   }
   
-  private var view: UIView {
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+  private var gradient: CAGradientLayer {
     let gradient = CAGradientLayer.default
-    gradient.frame = view.bounds
-    view.layer.insertSublayer(gradient, at: 0)
-    return view
+    gradient.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+    return gradient
   }
 }
