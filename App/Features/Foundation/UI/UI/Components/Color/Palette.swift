f18bc14515eb59374e48389ac8d025acd4083bbe
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-28 14:11:00 +0100

    Add application color palette

---
+++ App/Features/Foundation/UI/UI/Components/Color/Palette.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+/// Main application color palette
+public protocol Palette {
+  static var primary: UIColor { get }
+  static var primaryAlt: UIColor { get }
+  static var danger: UIColor { get }
+  static var darkScript: UIColor { get }
+  static var softScript: UIColor { get }
+  static var turquoise: UIColor { get }
+  static var pinkishGrey: UIColor { get }
+  static var darkWhite: UIColor { get }
+  static var darkGrey: UIColor { get }
+  static var grease: UIColor { get }
+  static var softGrey: UIColor { get }
+}
