37999f85983e4b70f7698035124253ed52744916
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-29 01:58:44 +0100

    Add Input & InputTextField components

---
+++ App/Features/Foundation/UI/UI/Components/Input/Input.swift
@@ -0,0 +1,99 @@
+import UIKit
+
+private struct InputConstraints {
+  static let border = CGFloat(1)
+  static let xPadding = CGFloat(10)
+  static let yPadding = CGFloat(8)
+}
+
+open class Input: UITextField {
+  open override var placeholder: String? {
+    didSet {
+      set(placeholder: placeholder)
+    }
+  }
+  
+  public var placeholderColor: UIColor? {
+    didSet {
+      set(placeholder: placeholder)
+    }
+  }
+  
+  // MARK: - Init
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  // MARK: - Setup
+  
+  private func setup() {
+    font = .body(.regular)
+    layer.cornerRadius = Radius.big
+  }
+  
+  // MARK: - Placeholder
+  
+  private func set(placeholder: String?) {
+    let attributes: [NSAttributedStringKey: Any] = [
+      .foregroundColor: placeholderColor ?? UIColor.softScript,
+      .font: UIFont.body(.regular)
+    ]
+    attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: attributes)
+  }
+  
+  // MARK: - Responder
+  
+  @discardableResult
+  open override func becomeFirstResponder() -> Bool {
+    handleResponder(isFirst: true)
+    return super.becomeFirstResponder()
+  }
+  
+  @discardableResult
+  open override func resignFirstResponder() -> Bool {
+    handleResponder(isFirst: false)
+    let resigned = super.resignFirstResponder()
+    layoutIfNeeded() // Little dirty hack around UITextField bug that glitch when resigning keyboard 💩
+    return resigned
+  }
+  
+  func applyBorder() {
+    layer.borderColor = tintColor.cgColor
+    layer.borderWidth = InputConstraints.border
+  }
+  
+  func removeBorder() {
+    layer.borderColor = UIColor.clear.cgColor
+    layer.borderWidth = 0
+  }
+  
+  private func handleResponder(isFirst: Bool) {
+    guard isFirst else {
+      removeBorder(); return
+    }
+    applyBorder()
+  }
+  
+  // MARK: - Custom placeholder padding
+  
+  open override func textRect(forBounds bounds: CGRect) -> CGRect {
+    let extraPaddingForX =
+      clearButtonMode != .never ? InputConstraints.xPadding * 2 : 0
+    
+    return CGRect(x: bounds.origin.x + InputConstraints.xPadding,
+                  y: bounds.origin.y + InputConstraints.yPadding,
+                  width: bounds.size.width - InputConstraints.xPadding - extraPaddingForX,
+                  height: bounds.size.height - InputConstraints.yPadding * 2)
+  }
+  
+  open override func editingRect(forBounds bounds: CGRect) -> CGRect {
+    return textRect(forBounds: bounds)
+  }
+}
---
+++ App/Features/Foundation/UI/UI/Components/Input/InputTextField.swift
@@ -0,0 +1,114 @@
+import Foundation
+
+private struct InputTextFieldConstraints {
+  static let inputHeight = 48
+}
+
+open class InputTextField: UIView {
+  
+  // MARK: - Public
+  
+  public enum State {
+    case normal
+    case errored
+  }
+  
+  public var title: String? {
+    didSet {
+      titleLabel.text = title
+    }
+  }
+  
+  public var error: String? {
+    didSet {
+      errorLabel.text = error
+      set(state: .errored)
+    }
+  }
+  
+  // MARK: - Private
+  
+  private lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .body(.semibold)
+    label.textColor = .darkScript
+    return label
+  }()
+
+  private lazy var errorLabel: UILabel = {
+    let label = UILabel()
+    label.font = .tiny
+    label.textColor = .danger
+    return label
+  }()
+  
+  public lazy var input: Input = {
+    let input = Input()
+    input.clearButtonMode = .whileEditing
+    return input
+  }()
+  
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.spacing = Margin.smallest
+    return stackView
+  }()
+  
+  public var state: State? {
+    didSet {
+      set(state: state!)
+    }
+  }
+  
+  // MARK: - Init
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  // MARK: - Setup
+  
+  private func setup() {
+    stackView.addArrangedSubview(titleLabel)
+    stackView.addArrangedSubview(input)
+    stackView.addArrangedSubview(errorLabel)
+    addSubview(stackView)
+    set(state: .normal)
+    applyConstraints()
+  }
+  
+  // MARK: - State
+  
+  private func set(state: State) {
+    switch state {
+    case .normal:
+      input.backgroundColor = .softGrey
+      input.tintColor = .primary
+      input.textColor = .primary
+      input.removeBorder()
+    case .errored:
+      input.backgroundColor = UIColor.danger.withAlphaComponent(0.05)
+      input.tintColor = .danger
+      input.textColor = .danger
+      input.applyBorder()
+    }
+  }
+  
+  // MARK: - Layout
+  
+  private func applyConstraints() {
+    stackView.snp.makeConstraints { make in
+      make.edges.equalTo(self)
+    }
+    input.snp.makeConstraints { make in
+      make.height.equalTo(InputTextFieldConstraints.inputHeight)
+    }
+  }
+}
