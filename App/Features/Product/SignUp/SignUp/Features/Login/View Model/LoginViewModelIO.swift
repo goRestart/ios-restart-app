b4935994156c7f343d8eeb36599d3671f2697037
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-31 18:45:05 +0100

    Add view model definitions for input & output

---
+++ App/Features/Product/SignUp/SignUp/Features/Login/View Model/LoginViewModelIO.swift
@@ -0,0 +1,19 @@
+import RxSwift
+
+protocol LoginViewModelInput {
+  func signUpButtonPressed()
+}
+
+protocol LoginViewModelOutput {
+  var username: Variable<String> { get }
+  var password: Variable<String> { get }
+  var isLoggingIn: Variable<Bool> { get }
+  
+  var userInteractionDisabled: Observable<Bool> { get }
+  var signUpEnabled: Observable<Bool> { get }
+}
+
+protocol LoginViewModelIO {
+  var input: LoginViewModelInput { get }
+  var output: LoginViewModelOutput { get }
+}
