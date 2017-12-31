2c2a208e4849e1c856257f00bbb75fdff08da452
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-31 18:48:42 +0100

    Add login view controller with bindings for the UI elements (two way bindings)

---
+++ App/Features/Product/SignUp/SignUp/Features/Login/View/View Controller/LoginViewController.swift
@@ -0,0 +1,66 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import UI
+
+public final class LoginViewController: ViewController {
+ 
+  var viewModel: LoginViewModel!
+  
+  private let loginView = LoginView()
+  
+  public override func loadView() {
+    self.view = loginView
+  }
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override public func bindViewModel() {
+    loginView.usernameInput.rx.value
+      .orEmpty
+      .bind(to: viewModel.output.username)
+      .disposed(by: bag)
+    
+    loginView.passwordInput.rx.value
+      .orEmpty
+      .bind(to: viewModel.output.password)
+      .disposed(by: bag)
+ 
+    loginView.usernameInput.rx
+      .controlEvent(.editingDidEndOnExit)
+      .subscribe(onNext: { [weak self] _ in
+        self?.loginView.passwordInput.becomeFirstResponder()
+      })
+      .disposed(by: bag)
+    
+    viewModel.output.userInteractionDisabled
+      .bind(to: loginView.usernameInput.rx.isUserInteractionEnabled)
+      .disposed(by: bag)
+    
+    viewModel.output.userInteractionDisabled
+      .bind(to: loginView.passwordInput.rx.isUserInteractionEnabled)
+      .disposed(by: bag)
+    
+    viewModel.output.isLoggingIn
+      .asObservable()
+      .bind(to: loginView.signInButton.rx.isLoading)
+      .disposed(by: bag)
+    
+    viewModel.output.signUpEnabled
+      .asObservable()
+      .bind(to: loginView.signInButton.rx.isEnabled)
+      .disposed(by: bag)
+    
+    viewModel.output.userInteractionDisabled
+      .bind(to: loginView.signInButton.rx.isUserInteractionEnabled)
+      .disposed(by: bag)
+    
+    loginView.signInButton.rx.tap
+      .subscribe(onNext: { [weak self] _ in
+        self?.viewModel.input.signUpButtonPressed()
+      })
+      .disposed(by: bag)
+  }
+}
