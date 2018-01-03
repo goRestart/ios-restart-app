5108302a22f39cd00e4a95a4d312e3dba837a7ab
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-01-03 17:22:00 +0100

    Add api data source

---
+++ App/Features/Foundation/Data/Data/Infrastructure/Repository/User/Data Source/Api/UserApiDataSource.swift
@@ -0,0 +1,24 @@
+import Domain
+import RxSwift
+import Moya
+import RxMoya
+
+struct UserApiDataSource: UserDataSource {
+  
+  private let provider: MoyaProvider<UserService>
+  private let errorAdapter: RegisterUserErrorAdapter
+  
+  init(provider: MoyaProvider<UserService>,
+       errorAdapter: RegisterUserErrorAdapter)
+  {
+    self.provider = provider
+    self.errorAdapter = errorAdapter
+  }
+  
+  func register(with credentials: UserCredentials) -> Completable {
+    return provider.rx
+      .request(.register(credentials))
+      .map(EmptyResponse.self, errorAdapter.make)
+      .asCompletable()
+  }
+}
