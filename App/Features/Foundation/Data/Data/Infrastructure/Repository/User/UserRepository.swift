6d344d814e7a3b39f200426476ec397103dd597d
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-01-03 17:22:15 +0100

    Add user repository

---
+++ App/Features/Foundation/Data/Data/Infrastructure/Repository/User/UserRepository.swift
@@ -0,0 +1,16 @@
+import Domain
+import Core
+import RxSwift
+
+public struct UserRepository {
+  
+  private let apiDataSource: UserDataSource
+  
+  init(apiDataSource: UserDataSource) {
+    self.apiDataSource = apiDataSource
+  }
+  
+  public func register(with credentials: UserCredentials) -> Completable {
+    return apiDataSource.register(with: credentials)
+  }
+}
