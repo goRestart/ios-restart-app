635a4c6b2cb7fc61bdec91140755a4137f82114c
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-01-03 02:02:16 +0100

    Wire dependencies

--- App/Features/Product/SignUp/Example/AppDelegate.swift
+++ App/Features/Product/SignUp/Example/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    let viewController = resolver.makeLogin()
+    let viewController = resolver.makeNotLogged()
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.rootViewController = viewController
     window?.makeKeyAndVisible()
---
+++ App/Features/Product/SignUp/SignUp/Features/Login/Infrastructure/Assembly/Assembly+NotLogged.swift
@@ -0,0 +1,8 @@
+import Core
+
+extension Assembly {
+  public func makeNotLogged() -> NotLoggedViewController {
+    let viewController = NotLoggedViewController()
+    return viewController
+  }
+}
