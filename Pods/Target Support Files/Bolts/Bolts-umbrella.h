d4cdcf668e548d42af2f285f9647e27bcb1ce912
MacMini <macmini@Mac-mini.local>
2018-01-09 14:46:53 +0100

    Re-install pods

--- Pods/Target Support Files/Bolts/Bolts-umbrella.h
+++ Pods/Target Support Files/Bolts/Bolts-umbrella.h
@@ -1,13 +1,5 @@
 #ifdef __OBJC__
 #import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
 #endif
 
 #import "BFAppLink.h"
@@ -23,7 +15,7 @@
 #import "BFCancellationTokenRegistration.h"
 #import "BFCancellationTokenSource.h"
 #import "BFExecutor.h"
-#import "BFTask+Exceptions.h"
+#import "BFGeneric.h"
 #import "BFTask.h"
 #import "BFTaskCompletionSource.h"
 #import "Bolts.h"
