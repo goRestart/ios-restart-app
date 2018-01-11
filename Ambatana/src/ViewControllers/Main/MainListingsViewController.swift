2032933837fd7336ecdfc6a9c95159d987878d86
Facundo Menzella <facumenzella@users.noreply.github.com>
2018-01-11 12:59:31 +0100

    3452 LGNavBarSearchField fully autolayoutable (#1844)

--- Ambatana/src/ViewControllers/Main/MainListingsViewController.swift
+++ Ambatana/src/ViewControllers/Main/MainListingsViewController.swift
@@ -154,11 +154,6 @@ class MainListingsViewController: BaseViewController, ListingListViewScrollDeleg
         }
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        navbarSearch.endEdit()
-    }
-
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
