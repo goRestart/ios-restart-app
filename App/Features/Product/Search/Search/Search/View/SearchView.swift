600f111ec43a5f10028f92e4f6656d0b56bf7925
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-01-13 21:44:57 +0100

    Add first implementation for searching games (no updates on collection yet)

---
+++ App/Features/Product/Search/Search/Search/View/SearchView.swift
@@ -0,0 +1,45 @@
+import UI
+import SnapKit
+import RxSwift
+
+public final class SearchView: View {
+ 
+  private let collectionView: UICollectionView = {
+    let collectionView = UICollectionView()
+    return collectionView
+  }()
+  
+  private let bag = DisposeBag()
+  private var viewModel: SearchViewModelType!
+  
+  init(viewModel: SearchViewModelType) {
+    self.viewModel = viewModel
+    super.init()
+  }
+  
+  public override func setupView() {
+    addSubview(collectionView)
+  }
+  
+  public override func setupConstraints() {
+    collectionView.snp.makeConstraints { make in
+      make.edges.equalTo(self)
+    }
+  }
+  
+  public func bind(textField: UITextField) {
+    let textFieldObserver = textField.rx.value
+      .orEmpty
+      .debounce(0.5, scheduler: MainScheduler.instance)
+      .asObservable()
+    viewModel.output.bind(to: textFieldObserver)
+    
+    viewModel.output.results
+      .asObservable()
+      .subscribe(onNext: { games in
+        print("Games = \(games)")
+      }, onError: { error in
+        print("Error = \(error)")
+      }).disposed(by: bag)
+  }
+}
