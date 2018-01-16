ce6799d912df98852b9c764c801f1f1d8b822187
Dídac <didac@letgo.com>
2018-01-16 13:27:27 +0100

    ABIOS-3460 Added new Advertisement cell +  drawer

---
+++ Ambatana/src/ViewControllers/Listings/CellDrawers/AdvertisementCellDrawer.swift
@@ -0,0 +1,9 @@
+//
+//  AdvertisementCellDrawer.swift
+//  LetGo
+//
+//  Created by Dídac on 11/01/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import Foundation
---
+++ Ambatana/src/ViewControllers/Listings/Cells/AdvertisementCell.swift
@@ -0,0 +1,18 @@
+//
+//  AdvertisementCell.swift
+//  LetGo
+//
+//  Created by Dídac on 12/01/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import UIKit
+
+class AdvertisementCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
