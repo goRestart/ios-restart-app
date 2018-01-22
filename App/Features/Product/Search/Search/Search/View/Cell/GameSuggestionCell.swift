f6dcbfc7678c942ca1f6356c0a2170d14ff9b7fd
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-01-22 19:48:03 +0100

    Add game suggestion cell with IGListKit magic ✨

---
+++ App/Features/Product/Search/Search/Search/View/Cell/GameSuggestionCell.swift
@@ -0,0 +1,37 @@
+import UI
+
+final class GameSuggestionCell: CollectionViewCell {
+
+  static let height = CGFloat(48)
+
+  private let suggestionLabel: UILabel = {
+    let label = UILabel()
+    label.font = .body(.regular)
+    label.textColor = .darkScript
+    return label
+  }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+  override func setupView() {
+    addSubview(suggestionLabel)
+  }
+  
+  override func setupConstraints() {
+    suggestionLabel.snp.makeConstraints { make in
+      make.left.equalTo(self).offset(Margin.medium)
+      make.right.equalTo(self).offset(-Margin.medium)
+      make.centerY.equalTo(self)
+    }
+  }
+  
+  func configure(with suggestion: GameSuggestionViewRender) {
+    suggestionLabel.attributedText = suggestion.attributedTitle
+  }
+}
