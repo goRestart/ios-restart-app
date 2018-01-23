d30dd04012822a219da1a66be8aaddb14e8f80fd
Raúl de Oñate <raul.deonate@letgo.com>
2018-01-23 17:03:13 +0100

    ABIOS-3386 Clean up MostSearchedItemsListVC setupUI useless code

--- Ambatana/src/ViewControllers/Sell/Post/MostSearchedItems/MostSearchedItemsListViewController.swift
+++ Ambatana/src/ViewControllers/Sell/Post/MostSearchedItems/MostSearchedItemsListViewController.swift
@@ -44,9 +44,6 @@ class MostSearchedItemsListViewController: BaseViewController, UITableViewDelega
     }
     
     private func setupUI() {
-        // TODO: Check further flow for navigationBar behaviours (didLoad vs didAppear)
-        //navigationController?.setNavigationBarHidden(false, animated: false)
-        
         closeButton.image = UIImage(named: "navbar_close")
         closeButton.style = .plain
         closeButton.target = self
