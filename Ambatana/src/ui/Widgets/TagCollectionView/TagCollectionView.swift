dcba7e51b640da82a21ae291faf8130206669d93
Facundo Menzella <facumenzella@users.noreply.github.com>
2018-01-24 13:23:28 +0100

    3479 - Added reports through all the setupUI code inside MoreInfoView & thorough TagCollectionView (#1912)

--- Ambatana/src/ui/Widgets/TagCollectionView/TagCollectionView.swift
+++ Ambatana/src/ui/Widgets/TagCollectionView/TagCollectionView.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import LGCoreKit
 
 class TagCollectionView: UICollectionView, TagCollectionViewModelDelegate {
     
@@ -8,29 +9,40 @@ class TagCollectionView: UICollectionView, TagCollectionViewModelDelegate {
     }
     
     func defaultSetup() {
+        report(AppReport.uikit(error: .breadcrumb), message: "TagCollectionView defaultSetup")
         backgroundColor = .clear
         let flowLayout = LeftAlignedCollectionViewFlowLayout()
         flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
         flowLayout.minimumInteritemSpacing = 5
         flowLayout.minimumLineSpacing = 5
         collectionViewLayout = flowLayout
+        report(AppReport.uikit(error: .breadcrumb), message: "TagCollectionView defaultSetup end")
     }
 
     override var intrinsicContentSize: CGSize {
+        report(AppReport.uikit(error: .breadcrumb), message: "TagCollectionView intrinsicContentSize")
         if collectionViewLayout.collectionViewContentSize == .zero {
             // we need to return something different than zero or the datasource `cellForItemAt` method won't get called.
             return CGSize(width: 1, height: 1)
         }
+        let height = collectionViewLayout.collectionViewContentSize.height
+        let width = collectionViewLayout.collectionViewContentSize.width
+
+        let msg = "TagCollectionView intrinsicContentSize \(height), \(width)"
+        report(AppReport.uikit(error: .breadcrumb), message: msg)
+
         return collectionViewLayout.collectionViewContentSize
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         invalidateIntrinsicContentSize()
+        report(AppReport.uikit(error: .breadcrumb), message: "invalidateIntrinsicContentSize")
     }
     
     func vmReloadData(_ vm: TagCollectionViewModel) {
         reloadData()
+        report(AppReport.uikit(error: .breadcrumb), message: "TagCollectionView vmReloadData invalidateLayout")
         collectionViewLayout.invalidateLayout()
     }
 }
