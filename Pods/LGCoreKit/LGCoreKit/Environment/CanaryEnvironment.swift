27ad5d30ad8b62864ee696049c6d7b6e25d6e1f5
Juan Iglesias <juan.iglesias@letgo.com>
2018-01-25 17:39:04 +0100

    ABIOS-3516: include https on canary hosts

--- Pods/LGCoreKit/LGCoreKit/Environment/CanaryEnvironment.swift
+++ Pods/LGCoreKit/LGCoreKit/Environment/CanaryEnvironment.swift
@@ -7,16 +7,16 @@
 //
 
 struct CanaryEnvironment: Environment {
-    let apiBaseURL = "http://canary.api.letgo.com"
+    let apiBaseURL = "https://canary.api.letgo.com"
     let realEstateBaseURL = "https://listingrealestate.canary.letgo.com"
-    let bouncerBaseURL = "http://bouncerv2.canary.letgo.com/api"
+    let bouncerBaseURL = "https://bouncerv2.canary.letgo.com/api"
     let userRatingsBaseURL = "http://rating.canary.letgo.com/api"
     let chatBaseURL = "https://chat.letgo.com"
     let webSocketURL = "wss://chat.letgo.com/socket"
     let notificationsBaseURL = "http://notifications.canary.letgo.com/api"
     let paymentsBaseURL = "https://payment.canary.letgo.com/payment"
     let suggestiveSearchBaseURL = "https://suggestivesearch.letgo.com"
-    let searchProductsBaseURL = "http://search-products.canary.letgo.com"
+    let searchProductsBaseURL = "https://search-products.canary.letgo.com"
     let searchRealEstateBaseURL = "https://searchrealestate.canary.letgo.com"
     let niordBaseURL = "https://niord.letgo.com"
 }
