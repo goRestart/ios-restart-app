8debe77aa2084a3d7f3e38adccbf72248032eaea
Raúl de Oñate <raul.deonate@letgo.com>
2018-01-26 16:36:58 +0100

    ABIOS-3390 MostSearchedItems camera badge variant

--- Ambatana/src/Coordinator/Tabs/MainTabNavigator.swift
+++ Ambatana/src/Coordinator/Tabs/MainTabNavigator.swift
@@ -19,6 +19,5 @@ protocol MainTabNavigator: TabNavigator {
     func openTaxonomyList(withViewModel viewModel: TaxonomiesViewModel)
 
     func openRelatedItems(relatedToListing listing: Listing)
-    func openMostSearchedItems(source: MostSearchedItemsSource,
-                               enableSearch: Bool)
+    func openMostSearchedItems(source: MostSearchedItemsSource, enableSearch: Bool)
 }
