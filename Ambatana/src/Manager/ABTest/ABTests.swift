94be0e94d9bf23c610492d06454798a9988fabf9
nestorgt <nestor.garcia@letgo.com>
2018-02-06 13:58:54 +0100

    ABIOS-3263 add feature flag for inactive conversations

--- Ambatana/src/Manager/ABTest/ABTests.swift
+++ Ambatana/src/Manager/ABTest/ABTests.swift
@@ -51,6 +51,7 @@ class ABTests {
     let removeCategoryWhenClosingPosting = IntABDynamicVar(key: "20180126RemoveCategoryWhenClosingPosting", defaultValue: 0, abGroupType: .legacyABTests)
     let realEstateNewCopy = IntABDynamicVar(key: "20180126RealEstateNewCopy", defaultValue: 0, abGroupType: .realEstate)
     let dummyUsersInfoProfile = IntABDynamicVar(key: "20180130DummyUsersInfoProfile", defaultValue: 0, abGroupType: .retention)
+    let showInactiveConversations = BoolABDynamicVar(key: "20180206ShowInactiveConversations", defaultValue: false, abGroupType: .chat)
     
     init() {
     }
@@ -94,6 +95,7 @@ class ABTests {
         result.append(removeCategoryWhenClosingPosting)
         result.append(realEstateNewCopy)
         result.append(dummyUsersInfoProfile)
+        result.append(showInactiveConversations)
         
         return result
     }
