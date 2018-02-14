d1c3fb0db808e67f1df9859d5d9e83a0e19b7746
Dídac <didac@letgo.com>
2018-02-14 14:29:20 +0100

    ABIOS-3546 Updated UTs

--- letgoTests/Unit/Manager/Tracking/TrackerEventSpec.swift
+++ letgoTests/Unit/Manager/Tracking/TrackerEventSpec.swift
@@ -4201,6 +4201,7 @@ class TrackerEventSpec: QuickSpec {
                 beforeEach {
                     var product = MockProduct.makeMock()
                     product.objectId = "12345"
+                    product.status = .pending
                     sut = TrackerEvent.listingBumpUpStart(.product(product), price: .free, type: .free,
                                                           storeProductId: nil, isPromotedBump: .falseParameter)
                 }
@@ -4221,6 +4222,9 @@ class TrackerEventSpec: QuickSpec {
                 }
                 it("promotedBump param is false") {
                     expect(sut.params?.stringKeyParams["promoted-bump"] as? String) == "false"
+                }
+                it("has listing status") {
+                    expect(sut.params?.stringKeyParams["product-status"] as? String) == "Pending"
                 }
             }
             describe("bump up complete") {
@@ -4254,6 +4258,9 @@ class TrackerEventSpec: QuickSpec {
                 }
                 it("promotedBump param is false") {
                     expect(sut.params?.stringKeyParams["promoted-bump"] as? String) == "false"
+                }
+                it("has listing status") {
+                    expect(sut.params?.stringKeyParams["product-status"] as? String) == "Pending"
                 }
             }
             describe("bump up fail") {
