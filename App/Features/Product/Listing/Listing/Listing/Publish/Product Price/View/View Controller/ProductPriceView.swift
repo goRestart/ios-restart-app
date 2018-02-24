895feccf3caefd77f24ddccb47788dd6360b64ea
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-02-24 17:25:19 +0100

    Add product price view

---
+++ App/Features/Product/Listing/Listing/Listing/Publish/Product Price/View/View Controller/ProductPriceView.swift
@@ -0,0 +1,30 @@
+import UI
+import Search
+import SnapKit
+import Core
+import Domain
+
+final class ProductPriceView: View {
+
+  var inputTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = Localize("product_price.price.input.placeholder", table: Table.productPrice, in: .framework)
+    textField.font = .h2
+    textField.textColor = .darkScript
+    textField.keyboardType = .decimalPad
+    return textField
+  }()
+
+  override func setupView() {
+    addSubview(inputTextField)
+    inputTextField.becomeFirstResponder()
+  }
+
+  override func setupConstraints() {
+    inputTextField.snp.makeConstraints { make in
+      make.leading.equalTo(self).offset(Margin.medium)
+      make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(Margin.medium)
+      make.trailing.equalTo(self).offset(-Margin.medium)
+    }
+  }
+}
