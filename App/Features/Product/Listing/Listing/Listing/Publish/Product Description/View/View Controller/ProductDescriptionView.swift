ae014e758c69c621cd3061e51e848c8c6db6a8bc
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-02-24 17:49:10 +0100

    Add product description view

---
+++ App/Features/Product/Listing/Listing/Listing/Publish/Product Description/View/View Controller/ProductDescriptionView.swift
@@ -0,0 +1,25 @@
+import UI
+
+final class ProductDescriptionView: View {
+
+  var inputTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = Localize("product_description.input.placeholder", table: Table.productDescription, in: .framework)
+    textField.font = .h2
+    textField.textColor = .darkScript
+    return textField
+  }()
+
+  override func setupView() {
+    addSubview(inputTextField)
+    inputTextField.becomeFirstResponder()
+  }
+
+  override func setupConstraints() {
+    inputTextField.snp.makeConstraints { make in
+      make.leading.equalTo(self).offset(Margin.medium)
+      make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(Margin.medium)
+      make.trailing.equalTo(self).offset(-Margin.medium)
+    }
+  }
+}
