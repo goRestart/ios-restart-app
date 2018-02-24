8b06136ecb98401dcb6ccb4736d4622f3b163bdc
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-02-24 17:37:11 +0100

    Add view model protocol for product description

---
+++ App/Features/Product/Listing/Listing/Listing/Publish/Product Description/View Model/ProductDescriptionViewModelType.swift
@@ -0,0 +1,15 @@
+import RxSwift
+
+protocol ProductDescriptionViewModelInput {
+  func onNextStepPressed()
+}
+
+protocol ProductDescriptionViewModelOutput {
+  var description: BehaviorSubject<String> { get }
+  var nextStepEnabled: Observable<Bool> { get }
+}
+
+protocol ProductDescriptionViewModelType {
+  var input: ProductDescriptionViewModelInput { get }
+  var output: ProductDescriptionViewModelOutput { get }
+}
