64fa7a4e48dab45d07ce2e44d693ac6d1a69fbc0
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-02-24 17:30:31 +0100

    Add product price view controller + view binder

---
+++ App/Features/Product/Listing/Listing/Listing/Publish/Product Price/View/View Controller/ProductPriceViewController.swift
@@ -0,0 +1,30 @@
+import UI
+import Domain
+import RxSwift
+
+public final class ProductPriceViewController: ViewController {
+
+  private let productPriceView = ProductPriceView()
+  private let viewBinder: ProductPriceViewBinder
+
+  var viewModel: ProductPriceViewModelType!
+
+  init(viewBinder: ProductPriceViewBinder) {
+    self.viewBinder = viewBinder
+    super.init(nibName: nil, bundle: nil)
+  }
+  required public init?(coder aDecoder: NSCoder) { fatalError() }
+
+  public override func loadView() {
+    self.view = productPriceView
+  }
+
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    title = Localize("product_price.title", table: Table.productPrice, in: .framework)
+  }
+
+  override public func bindViewModel() {
+    viewBinder.bind(view: productPriceView, to: viewModel, using: bag)
+  }
+}
