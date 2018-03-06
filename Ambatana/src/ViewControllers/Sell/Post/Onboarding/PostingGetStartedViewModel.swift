85df1865f912f8204af9fcf5659ebc7381339ec2
Dídac <didac@letgo.com>
2018-03-06 18:07:41 +0100

    ABIOS-3087 Get Started view WiP

--- Ambatana/src/ViewControllers/Sell/Post/Onboarding/PostingGetStartedViewModel.swift
+++ Ambatana/src/ViewControllers/Sell/Post/Onboarding/PostingGetStartedViewModel.swift
@@ -6,14 +6,42 @@
 //  Copyright © 2018 Ambatana. All rights reserved.
 //
 
+import LGCoreKit
+
 class PostingGetStartedViewModel: BaseViewModel {
     
     weak var navigator: PostingHastenedCreateProductNavigator?
 
+    var myUserRepository: MyUserRepository
+    var userAvatarURL: URL? {
+        return myUserRepository.myUser?.avatar?.fileURL
+    }
+    var userName: String? {
+        return myUserRepository.myUser?.shortName
+    }
+
+    var welcomeText: String {
+        guard let name = userName else { return "_Welcome" }
+        return "Welcome \(name)"
+    }
+
+    var infoText: String {
+        return "_ See how easy it is to sell on letgo, post something you got around you"
+    }
+
+    var buttonText: String {
+        return "_ Get Started"
+    }
     
     // MARK: - Lifecycle
     
-    override init() {
+    override convenience init() {
+        self.init(myUserRepository: Core.myUserRepository)
+    }
+
+    init(myUserRepository: MyUserRepository) {
+        self.myUserRepository = myUserRepository
+        super.init()
     }
     
     
