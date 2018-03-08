a005b94d70ab5cd8024435000ba274021a36ca8e
Dídac <didac@letgo.com>
2018-03-08 18:40:41 +0100

    ABIOS-3087 Added Assets and new cell type

---
+++ Ambatana/src/ViewControllers/Sell/Post/Onboarding/PostingCategoriesPickViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PostingCategoriesPickViewController.swift
+//  LetGo
+//
+//  Created by Dídac on 08/03/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import Foundation
---
+++ Ambatana/src/ViewControllers/Sell/Post/Onboarding/PostingCategoriesPickViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PostingCategoriesPickViewModel.swift
+//  LetGo
+//
+//  Created by Dídac on 08/03/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import Foundation
