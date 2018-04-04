a7a1ae2d26bdffe6abd48daf6cfafaab1bf35061
Facundo Menzella <facumenzella@gmail.com>
2018-04-04 12:39:24 +0200

    Moved to own file

--- letgoTests/Unit/ViewModel/ListingDeck/ListingDeckViewModelSpec.swift
+++ letgoTests/Unit/ViewModel/ListingDeck/ListingDeckViewModelSpec.swift
@@ -847,20 +847,6 @@ final class ListingDeckViewModelSpec: BaseViewModelSpec {
     }
 }
 
-extension MockProduct {
-    static func makeProductMocks(_ count: Int, allowDiscarded: Bool) -> [MockProduct] {
-        guard !allowDiscarded else {
-            return MockProduct.makeMocks(count: count)
-        }
-        var result: [MockProduct] = []
-        repeat {
-            result.append(contentsOf: MockProduct.makeMocks(count: count).filter { !$0.status.isDiscarded })
-        } while result.count < count
-        return Array(result[0...count-1])
-    }
-}
-
-
 extension ListingDeckViewModelSpec: ListingDeckViewModelDelegate {
     func vmRemoveMoreInfoTooltip() { }
     func vmShowOnboarding() { }
