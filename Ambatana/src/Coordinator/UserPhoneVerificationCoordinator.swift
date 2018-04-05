063149ffc3f55fb2811ea47fd2fe68c887f7c6a9
Sergi Gracia <sergigram@gmail.com>
2018-04-05 12:00:47 +0200

    Add UserPhoneVerification Coordinator and Navigator

---
+++ Ambatana/src/Coordinator/UserPhoneVerificationCoordinator.swift
@@ -0,0 +1,51 @@
+//
+//  UserPhoneVerificationCoordinator.swift
+//  LetGo
+//
+//  Created by Sergi Gracia on 05/04/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import LGCoreKit
+
+final class UserPhoneVerificationCoordinator: Coordinator {
+
+    var child: Coordinator?
+    let viewController: UIViewController
+    weak var coordinatorDelegate: CoordinatorDelegate?
+    weak var presentedAlertController: UIAlertController?
+    let bubbleNotificationManager: BubbleNotificationManager
+    let sessionManager: SessionManager
+
+    init(bubbleNotificationManager: BubbleNotificationManager = LGBubbleNotificationManager.sharedInstance,
+         sessionManager: SessionManager = Core.sessionManager) {
+        self.bubbleNotificationManager = bubbleNotificationManager
+        self.sessionManager = sessionManager
+
+        let viewModel = UserPhoneVerificationNumberInputViewModel()
+        let viewController = UserPhoneVerificationNumberInputViewController(viewModel: viewModel)
+        self.viewController = viewController
+        viewModel.navigator = self
+    }
+
+    func presentViewController(parent: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        guard let parentNavigationController = parent as? UINavigationController else { return }
+        parentNavigationController.pushViewController(viewController, animated: true)
+    }
+
+    func dismissViewController(animated: Bool, completion: (() -> Void)?) {
+        viewController.popViewController(animated: true)
+    }
+
+}
+
+extension UserPhoneVerificationCoordinator: UserPhoneVerificationNavigator {
+
+    func openCountrySelector() {
+        // FIXME: -
+    }
+
+    func openCodeInput() {
+        // FIXME: -
+    }
+}
---
+++ Ambatana/src/Coordinator/UserPhoneVerificationNavigator.swift
@@ -0,0 +1,14 @@
+//
+//  UserPhoneVerificationNavigator.swift
+//  LetGo
+//
+//  Created by Sergi Gracia on 05/04/2018.
+//  Copyright © 2018 Ambatana. All rights reserved.
+//
+
+import LGCoreKit
+
+protocol UserPhoneVerificationNavigator: class {
+    func openCountrySelector()
+    func openCodeInput()
+}
