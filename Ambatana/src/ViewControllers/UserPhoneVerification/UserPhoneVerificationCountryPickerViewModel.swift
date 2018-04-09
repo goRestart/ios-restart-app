d3224d337aa3fd9de84acc9867b1c83bea60d421
Sergi Gracia <sergigram@gmail.com>
2018-04-09 12:32:26 +0200

    Update UserPhoneVerification countryPicker vm to load countries list

--- Ambatana/src/ViewControllers/UserPhoneVerification/UserPhoneVerificationCountryPickerViewModel.swift
+++ Ambatana/src/ViewControllers/UserPhoneVerification/UserPhoneVerificationCountryPickerViewModel.swift
@@ -7,10 +7,41 @@
 //
 
 import LGCoreKit
+import RxSwift
+import RxCocoa
 
 final class UserPhoneVerificationCountryPickerViewModel: BaseViewModel {
 
+    let filteredCountries = Variable<[CountryPhoneCode]>([])
+    private var allCountries: [CountryPhoneCode] = []
+
     init(fake: String? = "") {
         super.init()
+    }
+
+    func filterCountries(by query: String) {
+        filteredCountries.value = allCountries.filter { country -> Bool in
+            return country.name.lowercased().contains(query.lowercased())
+                || String(country.code).contains(query.lowercased())
+        }
+    }
+
+    func didSelect(country: CountryPhoneCode) {
+        // FIXME: implement it
+    }
+
+
+    func loadCountriesList() {
+        allCountries = getCountriesList()
+        filteredCountries.value = allCountries // FIXME: fake data
+    }
+
+    private func getCountriesList() -> [CountryPhoneCode] {
+        return [
+            CountryPhoneCode(code: 355, name: "Albania"),
+            CountryPhoneCode(code: 342, name: "Chile"),
+            CountryPhoneCode(code: 342, name: "Nigeria"),
+            CountryPhoneCode(code: 342, name: "Lesotho"),
+        ]
     }
 }
