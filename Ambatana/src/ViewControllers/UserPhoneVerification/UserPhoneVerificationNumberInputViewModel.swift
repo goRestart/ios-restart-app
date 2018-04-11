18015ff1d8900362e74c46bce5b4970d96608b52
Sergi Gracia <sergigram@gmail.com>
2018-04-11 10:36:30 +0200

    Update PhoneVerificationInput VM to use CoreTelephony and LocationManager to retrieve device country

--- Ambatana/src/ViewControllers/UserPhoneVerification/UserPhoneVerificationNumberInputViewModel.swift
+++ Ambatana/src/ViewControllers/UserPhoneVerification/UserPhoneVerificationNumberInputViewModel.swift
@@ -9,21 +9,86 @@
 import LGCoreKit
 import RxSwift
 import RxCocoa
+import CoreTelephony
 
-struct CountryPhoneCode {
-    let code: Int
-    let name: String
+struct Country {
+    let regionCode: String
+    let callingCode: Int
+
+    private let currentLocale: Locale
+
+    init(regionCode: String,
+         callingCode: Int,
+         locale: Locale = Locale.current) {
+        self.regionCode = regionCode
+        self.callingCode = callingCode
+        self.currentLocale = locale
+    }
+
+    var name: String {
+        return currentLocale.localizedString(forRegionCode: regionCode) ?? ""
+    }
 }
 
 final class UserPhoneVerificationNumberInputViewModel: BaseViewModel {
 
     weak var navigator: UserPhoneVerificationNavigator?
 
-    var country: Driver<CountryPhoneCode?> { return .just(nil) }
-    var isContinueActionEnabled: Driver<Bool> { return .just(false) }
+    private let locationManager: LocationManager
+    private let locationRepository: LocationRepository
+    private let telephonyNetworkInfo: CTTelephonyNetworkInfo
 
-    init(fake: String? = "") {
+    var country = Variable<Country?>(nil)
+    var isContinueActionEnabled = Variable<Bool>(false)
+
+    init(locationManager: LocationManager = Core.locationManager,
+         locationRepository: LocationRepository = Core.locationRepository,
+         telephonyNetworkInfo: CTTelephonyNetworkInfo = CTTelephonyNetworkInfo()) {
+        self.locationManager = locationManager
+        self.locationRepository = locationRepository
+        self.telephonyNetworkInfo = telephonyNetworkInfo
         super.init()
+        retrieveCurrentLocationCountry()
+    }
+
+    private func retrieveCurrentLocationCountry() {
+        let retrievedFromTelephonyNetwork = retrieveCountryFromTelephonyNetwork()
+        guard !retrievedFromTelephonyNetwork else { return }
+        retrieveCountryFromLocationManager()
+    }
+
+    private func retrieveCountryFromTelephonyNetwork() -> Bool {
+        guard let countryCode = telephonyNetworkInfo.countryCode else { return false }
+        updateCurrentLocationCountry(with: countryCode)
+        return true
+    }
+
+    private func retrieveCountryFromLocationManager() {
+        guard let location = locationManager.currentAutoLocation?.location else { return }
+        locationRepository.retrievePostalAddress(location: location) { [weak self] result in
+            switch result {
+            case .success(let place):
+                guard let countryCode = place.postalAddress?.countryCode else { return }
+                self?.updateCurrentLocationCountry(with: countryCode)
+            case .failure(_):
+                // FIXME: waiting for product specs
+                break
+            }
+        }
+    }
+
+    private func updateCurrentLocationCountry(with countryCode: String) {
+        guard let callingCode = retrieveCallingCode(for: countryCode) else { return }
+        country.value = Country(regionCode: countryCode, callingCode: callingCode)
+    }
+
+    private func retrieveCallingCode(for regionCode: String) -> Int? {
+        // FIXME: waiting for product specs
+        return 0
+    }
+
+    func didUpdatePhoneNumber(number: String) {
+
     }
 
     func didTapCountryButton() {
