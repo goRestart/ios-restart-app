30c0b4b6a9b51a3ad2ac2fc7bfdd956f678f1876
Dídac Calventus <didac-calventus@users.noreply.github.com>
2018-05-15 16:20:01 +0200

    Moved blocked users to chat bar button menu (#2596)
    * add abtest and classes
    
    * add navigator protocol
    
    * rename
    
    * fix tests
    
    * add tab bar
    
    * fix tab bar not appearing
    
    * finally fix the tab bar :)
    
    * Moved blocked users to chat bar button menu
    
    * ABIOS-4053 Added unblock & visit profile features
    
    * Added final to classes, use of contentInsetAdjustmentBehavior for table
    
    * fixed rounded corners without layoutIfNeeded()
    
    * vars to lets, flatMap to filter

--- Ambatana/src/ViewControllers/Chat/ChatList/ConversationsList/ChatConversationsListViewController.swift
+++ Ambatana/src/ViewControllers/Chat/ChatList/ConversationsList/ChatConversationsListViewController.swift
@@ -6,15 +6,14 @@
 //  Copyright © 2018 Ambatana. All rights reserved.
 //
 
-final class ChatConversationsListViewController: BaseViewController {
-    
+final class ChatConversationsListViewController: BaseViewController, ChatConversationsListViewModelDelegate {
+
     private let viewModel: ChatConversationsListViewModel
     private let conversationsListView = ChatConversationsListView()
     
     private let optionsButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setImage(#imageLiteral(resourceName: "ic_more_options"), for: .normal)
-        button.addTarget(self, action: #selector(optionsButtonPressed), for: .touchUpInside)
         return button
     }()
     
@@ -26,6 +25,7 @@ final class ChatConversationsListViewController: BaseViewController {
         automaticallyAdjustsScrollViewInsets = false
         hidesBottomBarWhenPushed = false
         hasTabBar = true
+        self.viewModel.delegate = self
     }
     
     override func viewDidLoad() {
@@ -40,12 +40,13 @@ final class ChatConversationsListViewController: BaseViewController {
     // MARK: Navigation Bar
     
     private func setupNavigationBar() {
+        optionsButton.addTarget(self, action: #selector(optionsButtonPressed), for: .touchUpInside)
         setNavigationBarRightButtons([optionsButton])
     }
     
     // MARK: Actions
     
     @objc private func optionsButtonPressed() {
-        viewModel.openBlockedUsers()
+        viewModel.optionsButtonPressed()
     }
 }
