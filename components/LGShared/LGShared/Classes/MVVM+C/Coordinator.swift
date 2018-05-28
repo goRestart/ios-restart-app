a8e84599d159e0add9fee1079264070761de3ff5
Albert Hernández López <albert@letgo.com>
2018-05-28 10:18:09 +0200

    ABIOS-4080 remove commented method

--- components/LGShared/LGShared/Classes/MVVM+C/Coordinator.swift
+++ components/LGShared/LGShared/Classes/MVVM+C/Coordinator.swift
@@ -108,29 +108,6 @@ extension Coordinator {
 }
 
 
-// TODO: To be moved to the corresponding component when it is created
-//// MARK: - Login
-//
-//extension Coordinator {
-//    func openLoginIfNeeded(from source: EventParameterLoginSourceValue, style: LoginStyle,
-//                           loggedInAction: @escaping (() -> Void), cancelAction: (() -> Void)?) {
-//        guard !sessionManager.loggedIn else {
-//            loggedInAction()
-//            return
-//        }
-//        let coordinator = LoginCoordinator(source: source,
-//                                           style: style,
-//                                           loggedInAction: loggedInAction,
-//                                           cancelAction: cancelAction)
-//        openChild(coordinator: coordinator,
-//                  parent: viewController,
-//                  animated: true,
-//                  forceCloseChild: true,
-//                  completion: nil)
-//    }
-//}
-
-
 // MARK: - Alerts
 
 extension Coordinator {
