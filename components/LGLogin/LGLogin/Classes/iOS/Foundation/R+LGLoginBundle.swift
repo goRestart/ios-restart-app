6ae3f61dd60807a1deec404fcb8d9af24e688c3f
Albert Hernández López <albert@letgo.com>
2018-05-30 11:49:16 +0200

    ABIOS-4080 revert login bundle name

--- components/LGLogin/LGLogin/Classes/iOS/Foundation/R+LGLoginBundle.swift
+++ components/LGLogin/LGLogin/Classes/iOS/Foundation/R+LGLoginBundle.swift
@@ -5,7 +5,7 @@ private final class BundleToken {}
 extension R {
     static let loginBundle: Bundle = {
         let frameworkBundle = Bundle(for: BundleToken.self)
-        let bundleURL = frameworkBundle.url(forResource: "LGResourcesBundle",
+        let bundleURL = frameworkBundle.url(forResource: "LGLoginBundle",
                                             withExtension: "bundle")!
         return Bundle(url: bundleURL)!
     }()
