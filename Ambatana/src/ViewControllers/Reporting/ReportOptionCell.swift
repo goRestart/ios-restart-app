7e4ba15af8621020a7411f92e6978c2ec1c4aa5d
isaacroldan <isaac.roldan@gmail.com>
2018-07-02 11:32:38 +0200

    Create generic cell for Reporting

--- Ambatana/src/ViewControllers/Reporting/ReportOptionCell.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportOptionCell.swift
@@ -1,14 +1,17 @@
-//
-//  ReportOptionCell.swift
-//  LetGo
-//
-//  Created by Isaac Roldan on 29/6/18.
-//  Copyright © 2018 Ambatana. All rights reserved.
-//
-
 import Foundation
+import LGComponents
 
 final class ReportOptionCell: UITableViewCell, ReusableCell {
+
+    private enum Layout {
+        static let iconSize: CGFloat = 38
+        static let accessoryHeight: CGFloat = 13
+        static let accessoryWidth: CGFloat = 8
+        static let labelMargin: CGFloat = 19
+        static let labelRightMargin: CGFloat = 38
+        static let labelLeftBigMargin: CGFloat = 68
+        static let labelLeftSmallMargin: CGFloat = 15
+    }
 
     private let iconImageView: UIImageView = {
         let imageView = UIImageView()
@@ -24,7 +27,54 @@ final class ReportOptionCell: UITableViewCell, ReusableCell {
         return label
     }()
 
+    private let accessoryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = R.Asset.IconsButtons.rightChevron.image
+        return imageView
+    }()
+
+    var shouldShowIcon: Bool = true {
+        didSet {
+            iconImageView.isHidden = !shouldShowIcon
+            titleLabelLeftConstraint?.constant = shouldShowIcon ? Layout.labelLeftBigMargin : Layout.labelLeftSmallMargin
+        }
+    }
+
+    private var titleLabelLeftConstraint: NSLayoutConstraint?
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        contentView.addSubviewsForAutoLayout([iconImageView, titleLabel, accessoryImageView])
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        var constraints: [NSLayoutConstraint] = [
+            iconImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            iconImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: Metrics.margin),
+            iconImageView.heightAnchor.constraint(equalToConstant: Layout.iconSize),
+            iconImageView.widthAnchor.constraint(equalToConstant: Layout.iconSize),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Layout.labelMargin),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: Layout.labelMargin),
+            titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: Layout.labelRightMargin),
+            accessoryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            accessoryImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -Metrics.margin),
+            accessoryImageView.heightAnchor.constraint(equalToConstant: Layout.accessoryHeight),
+            accessoryImageView.widthAnchor.constraint(equalToConstant: Layout.accessoryWidth)
+        ]
+
+        let labelLeft = titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: Layout.labelLeftBigMargin)
+        titleLabelLeftConstraint = labelLeft
+
+        constraints.append(labelLeft)
+        NSLayoutConstraint.activate(constraints)
     }
 }
