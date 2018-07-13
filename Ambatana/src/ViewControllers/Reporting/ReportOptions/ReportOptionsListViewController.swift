d6562c1a0515e7301fc2e3fd361eea81ac8f1076
Sergi Gracia <sergigram@gmail.com>
2018-07-13 10:21:18 +0200

    Update ReportingOptionsList vc to handle rx exposed by vm and animate additional notes

--- Ambatana/src/ViewControllers/Reporting/ReportOptions/ReportOptionsListViewController.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportOptions/ReportOptionsListViewController.swift
@@ -1,17 +1,27 @@
 import Foundation
 import LGComponents
+import RxSwift
 
 final class ReportOptionsListViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
 
     private enum Layout {
         static let buttonAreaHeight: CGFloat = 80
-        static let additionalNotesAreaHeight: CGFloat = 118
+        static let additionalNotesAreaHeight: CGFloat = 100
         static let buttonHeight: CGFloat = 50
         static let estimatedRowHeight: CGFloat = 60
         static let borderWidth: CGFloat = 1
     }
 
     private let viewModel: ReportOptionsListViewModel
+    private let disposeBag = DisposeBag()
+    private var mutableConstraints: [NSLayoutConstraint] = []
+
+    private var tableBottomInset: CGFloat = 0 {
+        didSet {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tableBottomInset, right: 0)
+            tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: tableBottomInset, right: 0)
+        }
+    }
 
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -27,13 +37,10 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
 
     private let additionalNotesTextView: UITextView = {
         let textView = UITextView()
-        textView.addTopViewBorderWith(width: Layout.borderWidth,
-                                      color: .grayLight,
-                                      leftMargin: Metrics.margin,
-                                      rightMargin: Metrics.margin)
         textView.font = .bigBodyFont
         textView.textColor = .placeholder
         textView.text = "Write here any additional notes that might help us to resolve this issue" // FIXME: localize
+        textView.alpha = 0
         return textView
     }()
 
@@ -50,20 +57,13 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
         return view
     }()
 
-    private var selectedOption: ReportOption?
-    private var mutableConstraints: [NSLayoutConstraint] = []
-
-    private var tableBottomInset: CGFloat {
-        didSet{
-            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tableBottomInset, right: 0)
-            tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: tableBottomInset, right: 0)
-        }
-    }
+    private var bottomContainerTopBorder: UIView?
 
     init(viewModel: ReportOptionsListViewModel) {
         self.viewModel = viewModel
         super.init(viewModel: viewModel, nibName: nil)
         setupUI()
+        setupRx()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -81,11 +81,23 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
         bottomContainer.addSubviewsForAutoLayout([additionalNotesTextView, reportButton])
         view.addSubviewsForAutoLayout([tableView, bottomContainer])
         view.backgroundColor = .white
+
         tableView.delegate = self
         tableView.dataSource = self
         additionalNotesTextView.delegate = self
+
         reportButton.addTarget(self, action: #selector(reportButtonTapped), for: .touchUpInside)
+
+        setupBottomContainerBorder()
         setupConstraints()
+    }
+
+    private func setupBottomContainerBorder() {
+        bottomContainerTopBorder = bottomContainer.addTopViewBorderWith(width: Layout.borderWidth,
+                                                                        color: .grayLight,
+                                                                        leftMargin: Metrics.margin,
+                                                                        rightMargin: Metrics.margin)
+        bottomContainerTopBorder?.alpha = 0
     }
 
     private func setupConstraints() {
@@ -97,6 +109,9 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
             bottomContainer.leftAnchor.constraint(equalTo: view.leftAnchor),
             bottomContainer.rightAnchor.constraint(equalTo: view.rightAnchor),
             bottomContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            additionalNotesTextView.topAnchor.constraint(equalTo: bottomContainer.topAnchor, constant: Metrics.margin),
+            additionalNotesTextView.leftAnchor.constraint(equalTo: bottomContainer.leftAnchor, constant: Metrics.margin),
+            additionalNotesTextView.rightAnchor.constraint(equalTo: bottomContainer.rightAnchor, constant: -Metrics.margin),
             reportButton.leftAnchor.constraint(equalTo: bottomContainer.leftAnchor, constant: Metrics.margin),
             reportButton.rightAnchor.constraint(equalTo: bottomContainer.rightAnchor, constant: -Metrics.margin),
             reportButton.heightAnchor.constraint(equalToConstant: Layout.buttonHeight)
@@ -105,13 +120,15 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
         mutableConstraints = constraintsForAdditionalNotes(visible: false)
 
         NSLayoutConstraint.activate(constraints + mutableConstraints)
+
     }
 
     private func constraintsForAdditionalNotes(visible: Bool) -> [NSLayoutConstraint] {
         if visible {
             return [
-                bottomContainer.topAnchor.constraint(equalTo: safeBottomAnchor, constant: -Layout.buttonAreaHeight + Layout.additionalNotesAreaHeight),
-                reportButton.topAnchor.constraint(equalTo: bottomContainer.topAnchor, constant: Metrics.margin),
+                additionalNotesTextView.heightAnchor.constraint(equalToConstant: Layout.additionalNotesAreaHeight),
+                bottomContainer.topAnchor.constraint(equalTo: safeBottomAnchor, constant: -(Layout.buttonAreaHeight + Layout.additionalNotesAreaHeight + Metrics.margin)),
+                reportButton.topAnchor.constraint(equalTo: additionalNotesTextView.bottomAnchor, constant: Metrics.margin),
             ]
         } else {
             return [
@@ -121,13 +138,58 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
         }
     }
 
+    private func setupRx() {
+        viewModel
+            .showAdditionalNotes
+            .asDriver()
+            .drive(onNext: { [weak self] showAdditionalNotes in
+                self?.makeAdditionalNotes(visible: showAdditionalNotes)
+            })
+            .disposed(by: disposeBag)
+
+        viewModel
+            .showReportButtonActive
+            .asDriver()
+            .drive(onNext: { [weak self] showButtonActive in
+                self?.reportButton.isEnabled = showButtonActive
+            })
+            .disposed(by: disposeBag)
+    }
+
     @objc private func reportButtonTapped() {
-        guard let option = selectedOption else { return }
-        viewModel.didTapReport(with: option)
+        viewModel.didTapReport()
     }
 
     @objc private func didTapClose() {
         viewModel.didTapClose()
+    }
+
+    func makeAdditionalNotes(visible: Bool) {
+        guard additionalNotesTextView.alpha != (visible ? 1 : 0) else { return }
+
+        mutableConstraints.forEach { $0.isActive = false }
+        let constraints = constraintsForAdditionalNotes(visible: visible)
+        mutableConstraints = constraints
+        NSLayoutConstraint.activate(self.mutableConstraints)
+
+        UIView.animate(withDuration: 0.6,
+                       delay: visible ? 0 : 0.1,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0.6,
+                       options: .curveLinear,
+                       animations: { [weak self] in
+                        self?.view.layoutIfNeeded() },
+                       completion: nil)
+
+        UIView.animate(withDuration: 0.4,
+                       delay: visible ? 0.2 : 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0.6,
+                       options: .curveLinear,
+                       animations: { [weak self] in
+                        self?.bottomContainerTopBorder?.alpha = visible ? 1 : 0
+                        self?.additionalNotesTextView.alpha = visible ? 1 : 0 },
+                       completion: nil)
     }
 
     // MARK: TableView Delegate & DataSource
@@ -146,12 +208,6 @@ final class ReportOptionsListViewController: BaseViewController, UITableViewDele
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let option = viewModel.optionGroup.options[indexPath.row]
         viewModel.didSelect(option: option)
-        reportButton.isEnabled = option.childOptions == nil
-        selectedOption = option
-    }
-
-    func showAdditionalNotes() {
-
     }
 }
 
