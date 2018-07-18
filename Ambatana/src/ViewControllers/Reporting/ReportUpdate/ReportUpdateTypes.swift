6efc41262aa20bc0666935d76d109fcd0df6be63
isaacroldan <isaac.roldan@gmail.com>
2018-07-18 10:32:39 +0200

    Add Report Update Types

--- Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateTypes.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateTypes.swift
@@ -1,9 +1,18 @@
-//
-//  ReportUpdateTypes.swift
-//  LetGo
-//
-//  Created by Isaac Roldan on 17/7/18.
-//  Copyright © 2018 Ambatana. All rights reserved.
-//
-
 import Foundation
+import LGComponents
+
+enum ReportUpdateType {
+    case product(productname: String, username: String)
+    case userA(username: String)
+    case userB(username: String)
+    case userC(username: String)
+
+    var text: String {
+        switch self {
+        case .product(let productname, let username): return R.Strings.reportingListingUpdateText(productname, username)
+        case .userA(let username): return R.Strings.reportingUserUpdateTextA(username)
+        case .userB(let username): return R.Strings.reportingUserUpdateTextB(username)
+        case .userC(let username): return R.Strings.reportingUserUpdateTextC(username)
+        }
+    }
+}
