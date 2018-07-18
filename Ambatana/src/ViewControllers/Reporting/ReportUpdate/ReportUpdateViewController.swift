596dbf3babb1285b5f260a51868fdf0ad4521561
isaacroldan <isaac.roldan@gmail.com>
2018-07-18 12:49:08 +0200

    Remove comments

--- Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateViewController.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateViewController.swift
@@ -1,9 +1,1 @@
-//
-//  ReportUpdateViewController.swift
-//  LetGo
-//
-//  Created by Isaac Roldan on 17/7/18.
-//  Copyright © 2018 Ambatana. All rights reserved.
-//
-
 import Foundation
--- Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateViewModel.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportUpdate/ReportUpdateViewModel.swift
@@ -1,9 +1,1 @@
-//
-//  ReportUpdateViewModel.swift
-//  LetGo
-//
-//  Created by Isaac Roldan on 17/7/18.
-//  Copyright © 2018 Ambatana. All rights reserved.
-//
-
 import Foundation
