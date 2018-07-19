bfbc17624155ec732a3f3629348f475831e00e48
Sergi Gracia <sergigram@gmail.com>
2018-07-19 11:45:56 +0200

    Update ReportSent vm to handle actions from vc to block and review

--- Ambatana/src/ViewControllers/Reporting/ReportSent/ReportSentViewModel.swift
+++ Ambatana/src/ViewControllers/Reporting/ReportSent/ReportSentViewModel.swift
@@ -1,17 +1,35 @@
 import Foundation
 import LGComponents
+import RxSwift
 
 final class ReportSentViewModel: BaseViewModel {
 
     let type: ReportSentType
     var navigator: ReportNavigator?
 
+    let showBlockAction = Variable<Bool>(false)
+    let showReviewAction = Variable<Bool>(false)
+
     init(type: ReportSentType) {
         self.type = type
         super.init()
+        setupActions()
+    }
+
+    private func setupActions() {
+        showBlockAction.value = type.allowsBlockUser
+        showReviewAction.value = true
     }
 
     func didTapClose() {
         navigator?.closeReporting()
+    }
+
+    func didTapBlock() {
+
+    }
+
+    func didTapReview() {
+        
     }
 }
