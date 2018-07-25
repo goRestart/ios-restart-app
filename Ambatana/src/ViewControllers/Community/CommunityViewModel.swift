9f0345d3e78798fefbee886fc277cc08e88c52aa
Sergi Gracia <sergigram@gmail.com>
2018-07-25 11:28:34 +0200

    Update Community vm to use the CommunityRepository from core to build the request

--- Ambatana/src/ViewControllers/Community/CommunityViewModel.swift
+++ Ambatana/src/ViewControllers/Community/CommunityViewModel.swift
@@ -5,12 +5,18 @@ import LGCoreKit
 class CommunityViewModel: BaseViewModel {
 
     //weak var navigator:
+    let communityRepository: CommunityRepository
 
     var urlRequest: URLRequest?
 
-    init() {
-        // Get request from repository
-        urlRequest = URLRequest()
+    init(communityRepository: CommunityRepository) {
+        self.communityRepository = communityRepository
+        super.init()
+        setupRequest()
+    }
+
+    private func setupRequest() {
+        urlRequest = communityRepository.buildCommunityURLRequest()
     }
 
     func didTapClose() {
