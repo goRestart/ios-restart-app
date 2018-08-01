0bbd65528fb4ead6cbcd70b435278f84ecded9f4
Álvaro Murillo del Puerto <alvaromurillop@gmail.com>
2018-08-01 17:53:57 +0200

    ABIOS-4709 Trying to solve the crashes

--- Ambatana/src/Manager/MachineLearning/LGVision.swift
+++ Ambatana/src/Manager/MachineLearning/LGVision.swift
@@ -18,44 +18,33 @@ protocol MachineLearningVision {
 
 @available(iOS 11.0, *)
 final class LGVision: MachineLearningVision {
-    var numberOfObservationsToReturn: Int = 3
-    
-    static let shared = LGVision()
-    
-    private var request: VNCoreMLRequest?
-    private var requestCompletion: MachineLearningVisionCompletion?
-    
-    private let model = MobileNetLetgov7final()
-    
-    // MARK: - Lifecycle
-    
-    private init() {
-        guard let visionModel = try? VNCoreMLModel(for: model.model) else {
-            logMessage(.error, type: .parsing, message: "Could not create Vision model")
-            return
-        }
-        request = VNCoreMLRequest(model: visionModel, completionHandler: requestDidComplete)
-        request?.imageCropAndScaleOption = .centerCrop
-    }
-    
+    static let numberOfObservationsToReturn: Int = 3
+
+    private lazy var model: VNCoreMLModel? = {
+        return try? VNCoreMLModel(for: MobileNetLetgov7final().model)
+    }()
+
     // MARK: - Vision prediction
     
     func predict(pixelBuffer: CVPixelBuffer, completion: MachineLearningVisionCompletion?) {
-        guard let request = request else { return }
-        requestCompletion = completion
-        let requestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
-        try? requestHandler.perform([request]) // no need to catch, requestDidComplete will also be called
-    }
-    
-    func requestDidComplete(request: VNRequest, error: Error?) {
-        if let observations = request.results as? [VNClassificationObservation] {
-            // The observations appear to be sorted by confidence already, so we
-            // take the top X and map them to an array of (String, Double) tuples.
-            let topObservations = observations.prefix(through: numberOfObservationsToReturn-1)
-                .map { MachineLearningVisionObservation(identifier: $0.identifier, confidence: Double($0.confidence)) }
-            requestCompletion?(topObservations)
-        } else {
-            requestCompletion?(nil)
+        guard let model = model else {
+            completion?(nil)
+            return
         }
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        let completionHandler : VNRequestCompletionHandler = { (request, error) in
+            if let observations = request.results as? [VNClassificationObservation] {
+                // The observations appear to be sorted by confidence already, so we
+                // take the top X and map them to an array of (String, Double) tuples.
+                let topObservations = observations.prefix(through: LGVision.numberOfObservationsToReturn-1)
+                    .map { MachineLearningVisionObservation(identifier: $0.identifier, confidence: Double($0.confidence)) }
+                completion?(topObservations)
+            } else {
+                completion?(nil)
+            }
+        }
+        let request = VNCoreMLRequest(model: model, completionHandler: completionHandler)
+        request.imageCropAndScaleOption = .centerCrop
+        try? requestHandler.perform([request]) // no need to catch, requestDidComplete will also be called
     }
 }
