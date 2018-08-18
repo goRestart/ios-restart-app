380cb17e6cf2122061b404018a4f923bd1fa56ee
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-08-18 15:17:48 +0200

    Localizable is now working

--- App/Features/Foundation/UI/UI/Utils/Localizable/Localize.swift
+++ App/Features/Foundation/UI/UI/Utils/Localizable/Localize.swift
@@ -1,9 +1,19 @@
 import Foundation
 
-public func Localize(_ key: String, _ table: String? = nil, in bundle: Bundle, comments: String = "") -> String {
-  return NSLocalizedString(key, tableName: table, bundle: bundle, comment: comments)
+/// Returns a localized string from the application main bundle
+///
+/// - Parameter key: localizable key identifier
+/// - Returns: localized string
+public func Localize(_ key: String, _ table: String? = nil, comments: String = "") -> String {
+    return NSLocalizedString(key, tableName: table, comment: comments)
 }
 
-public func Localize(_ key: String, table: String? = nil, in bundle: Bundle, arguments: CVarArg...) -> String {
-  return String(format: Localize(key, table, in: bundle, comments: ""), arguments: arguments)
+/// Returns a string created by using a given format string as a template into which the remaining argument values are substituted according to the user's default locale
+///
+/// - Parameters:
+///   - key: localizable key identifier
+///   - arguments: list of string arguments
+/// - Returns: localized string
+public func Localize(_ key: String, table: String? = nil, arguments: CVarArg...) -> String {
+    return String(format: Localize(key, table), arguments: arguments)
 }
