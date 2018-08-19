ab4fcaec0aef3b19e5e47562866764a64fa34705
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-08-19 21:57:57 +0200

    Add Rx extension for firebase `createUser` method

---
+++ App/Features/Foundation/Data/Data/Infrastructure/Extension/Firebase/Auth+Rx.swift
@@ -0,0 +1,18 @@
+import RxSwift
+import FirebaseAuth
+
+extension Reactive where Base: Auth {
+  public func createUser(email: String, password: String) -> Single<AuthDataResult> {
+    return Single.create { event in
+      Auth.auth().createUser(withEmail: email, password: password, completion: { (result, error) in
+        if let result = result {
+          event(.success(result))
+        }
+        if let error = error {
+          event(.error(error))
+        }
+      })
+      return Disposables.create()
+    }
+  }
+}
