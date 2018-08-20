9d6bd44a1ab4b4bb169a2174151a8bde1e28f0c9
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-08-20 19:15:55 +0200

    Add firebase Auth Rx extension for signing in an existing user with email + password

--- App/Features/Foundation/Data/Data/Infrastructure/Extension/Firebase/Auth+Rx.swift
+++ App/Features/Foundation/Data/Data/Infrastructure/Extension/Firebase/Auth+Rx.swift
@@ -15,4 +15,18 @@ extension Reactive where Base: Auth {
       return Disposables.create()
     }
   }
+  
+  public func signIn(email: String, password: String) -> Single<AuthDataResult> {
+    return Single.create { event in
+      Auth.auth().signIn(withEmail: email, password: password, completion: { (result, error) in
+        if let result = result {
+          event(.success(result))
+        }
+        if let error = error {
+          event(.error(error))
+        }
+      })
+      return Disposables.create()
+    }
+  }
 }
