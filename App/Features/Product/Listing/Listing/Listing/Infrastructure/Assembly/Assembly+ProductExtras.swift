823536d91e414f0c7ddc8d6a10b77c1ba4970cca
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-08-25 15:43:50 +0200

    Expose product extras dependency

---
+++ App/Features/Product/Listing/Listing/Listing/Infrastructure/Assembly/Assembly+ProductExtras.swift
@@ -0,0 +1,26 @@
+import Core
+import Application
+
+extension Assembly {
+  public var productExtras: ProductExtrasViewController {
+    let viewController = ProductExtrasViewController(
+      viewBinder: viewBinder
+    )
+    viewController.viewModel = viewModel
+    return viewController
+  }
+  
+  private var viewBinder: ProductExtrasViewBinder {
+    return ProductExtrasViewBinder()
+  }
+  
+  private var viewModel: ProductExtrasViewModelType {
+    return ProductExtrasViewModel(
+      getProductExtras: getProductExtras
+    )
+  }
+  
+  private var getProductExtras: GetProductExtras {
+    return GetProductExtras()
+  }
+}
