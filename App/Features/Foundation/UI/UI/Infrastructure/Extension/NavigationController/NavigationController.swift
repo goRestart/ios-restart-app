edee6e90c8982bce7e78ea85ef76123483798c97
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-09-03 21:21:15 +0200

    Fix keyboard issues on iOS 12

--- App/Features/Foundation/UI/UI/Infrastructure/Extension/NavigationController/NavigationController.swift
+++ App/Features/Foundation/UI/UI/Infrastructure/Extension/NavigationController/NavigationController.swift
@@ -3,9 +3,14 @@ import UIKit
 public final class NavigationController: UINavigationController {
   public override func viewDidLoad() {
     super.viewDidLoad()
+    configureNavigationBar()
+  }
+  
+  private func configureNavigationBar() {
     navigationBar.setBackgroundImage(UIImage(), for: .compact)
     navigationBar.shadowImage = UIImage()
     navigationBar.barTintColor = .white
     navigationBar.isOpaque = true
+    navigationBar.isTranslucent = false
   }
 }
