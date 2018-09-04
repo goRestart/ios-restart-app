56234e9a11f8b5367e4d26b87e692febde17f6c0
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-09-04 21:09:34 +0200

    Use Driver on product price

--- App/Features/Product/Listing/Listing/Listing/Publish/Product Price/View/View Controller/ProductPriceViewBinder.swift
+++ App/Features/Product/Listing/Listing/Listing/Publish/Product Price/View/View Controller/ProductPriceViewBinder.swift
@@ -4,15 +4,16 @@ import RxCocoa
 struct ProductPriceViewBinder {
   func bind(view: ProductPriceView, to viewModel: ProductPriceViewModelType, using bag: DisposeBag) {
     view.rx.productPrice
-      .bind(to: viewModel.input.price)
-      .disposed(by: bag)
+      .subscribe(onNext: { price in
+        viewModel.input.onChange(price: price)
+      }).disposed(by: bag)
     
-    viewModel.input.price
-      .bind(to: view.rx.productPrice)
+    viewModel.output.price
+      .drive(view.rx.productPrice)
       .disposed(by: bag)
 
     viewModel.output.nextStepEnabled
-      .bind(to: view.rx.nextButtonIsEnabled)
+      .drive(view.rx.nextButtonIsEnabled)
       .disposed(by: bag)
     
     view.rx.nextButtonWasTapped.subscribe { _  in
