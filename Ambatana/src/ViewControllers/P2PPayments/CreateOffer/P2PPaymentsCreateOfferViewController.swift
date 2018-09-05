0a50f03a0e45175e6796f5adc8708895531f18b6
Julian Oliver <juolgon@gmail.com>
2018-09-05 03:35:28 +0200

    Merge make an offer view into view controller

--- Ambatana/src/ViewControllers/P2PPayments/CreateOffer/P2PPaymentsCreateOfferViewController.swift
+++ Ambatana/src/ViewControllers/P2PPayments/CreateOffer/P2PPaymentsCreateOfferViewController.swift
@@ -3,6 +3,8 @@ import LGComponents
 import RxSwift
 import RxCocoa
 
+// TODO: @juolgon Locaalize all texts
+
 final class P2PPaymentsCreateOfferViewController: BaseViewController {
     var viewModel: P2PPaymentsCreateOfferViewModel
     private let disposeBag = DisposeBag()
@@ -16,12 +18,26 @@ final class P2PPaymentsCreateOfferViewController: BaseViewController {
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
     override func loadView() {
-        let createOfferView = P2PPaymentsCreateOfferView()
-        view = createOfferView
+        view = UIView()
+        setupUI()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+    }
+
+    private func setupUI() {
+    }
+
+    override func viewWillAppearFromBackground(_ fromBackground: Bool) {
+        super.viewWillAppearFromBackground(fromBackground)
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        setNavBarCloseButton(#selector(closeButtonPressed), icon: R.Asset.P2PPayments.close.image)
+        setNavBarTitleStyle(NavBarTitleStyle.text("Offer"))
+        setNavBarBackgroundStyle(NavBarBackgroundStyle.transparent(substyle: NavBarTransparentSubStyle.light))
     }
 }
