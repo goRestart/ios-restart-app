67c6e009fe27a2415599c85743c2217cbfb4af54
Julian Oliver <juolgon@gmail.com>
2018-09-06 08:26:27 +0200

    Create empty view model/controller for offer status screen

---
+++ Ambatana/src/ViewControllers/P2PPayments/OfferStatus/P2PPaymentsOfferStatusViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+import LGComponents
+import RxSwift
+import RxCocoa
+
+// TODO: @juolgon Localize all texts
+
+final class P2PPaymentsOfferStatusViewController: BaseViewController {
+}
