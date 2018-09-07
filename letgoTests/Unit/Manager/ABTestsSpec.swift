4d0d70bf17eab6dac6bef930d0a80a43df515f42
Facundo Menzella <facumenzella@gmail.com>
2018-09-07 17:04:16 +0200

    67 instead of 66

--- letgoTests/Unit/Manager/ABTestsSpec.swift
+++ letgoTests/Unit/Manager/ABTestsSpec.swift
@@ -46,7 +46,7 @@ final class ABTestsSpec: QuickSpec {
                     sut.registerVariables()
                 }
                 it("registers all the variables") {
-                    expect(syncer.syncedCount) == 66
+                    expect(syncer.syncedCount) == 67
                 }
             }
             context("registering all the variables") {
