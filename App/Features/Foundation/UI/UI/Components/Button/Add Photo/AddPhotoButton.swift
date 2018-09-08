9b2227eb359a0f36b3b02a6a8dc0cf1ed0313998
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-09-08 02:34:12 +0200

    Add photo button state variant so it can show the selected image

--- App/Features/Foundation/UI/UI/Components/Button/Add Photo/AddPhotoButton.swift
+++ App/Features/Foundation/UI/UI/Components/Button/Add Photo/AddPhotoButton.swift
@@ -3,6 +3,11 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
+public enum AddPhotoButtonState {
+  case filled(UIImage)
+  case empty
+}
+
 public final class AddPhotoButton: View {
   fileprivate let addButton: UIButton = {
     let button = UIButton()
@@ -11,17 +16,41 @@ public final class AddPhotoButton: View {
     button.tintColor = .softScript
     return button
   }()
+  
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.isHidden = true
+    return imageView
+  }()
 
   @available(*, unavailable)
   public override func setupView() {
     backgroundColor = .grease
     layer.cornerRadius = Radius.big
+    clipsToBounds = true
     addSubview(addButton)
+    addSubview(imageView)
   }
 
+  @available(*, unavailable)
   public override func setupConstraints() {
     addButton.snp.makeConstraints { make in
       make.edges.equalTo(self)
+    }
+    imageView.snp.makeConstraints { make in
+      make.edges.equalTo(self)
+    }
+  }
+  
+  public func set(state: AddPhotoButtonState) {
+    switch state {
+    case .filled(let image):
+      imageView.isHidden = false
+      imageView.image = image
+    case .empty:
+      imageView.isHidden = true
+      imageView.image = nil
     }
   }
 }
