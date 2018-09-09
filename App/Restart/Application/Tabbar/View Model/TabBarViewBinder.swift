c898eabc2708189895b27da8c89e9d81059e2196
Oscar Antonio Duran Grillo <hi@oduran.me>
2018-09-09 11:15:52 +0200

    Force everything!

--- App/Restart/Application/Tabbar/View Model/TabBarViewBinder.swift
+++ App/Restart/Application/Tabbar/View Model/TabBarViewBinder.swift
@@ -8,9 +8,10 @@ struct TabBarViewBinder {
     view.rx.didSelect
       .debounce(0.1, scheduler: MainScheduler.instance)
       .map { $0.tabBarItem }
+      .map { MenuItem(rawValue: $0!.tag)! }
       .subscribe(onNext: { item in
-        switch item?.tag {
-        case MenuItem.publish.rawValue:
+        switch item {
+        case .publish:
           viewModel.input.didTapAddProduct()
         default: break
         }
