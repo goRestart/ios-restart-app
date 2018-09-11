f26e36a4d9142873b8f525bab9017e5dccc8563d
Julian Oliver <juolgon@gmail.com>
2018-09-11 22:59:49 +0200

    Properly configure step list view to show numbers and lines

--- Ambatana/src/ViewControllers/P2PPayments/OfferStatus/Subviews/P2PPaymentsOfferStatusStepListView.swift
+++ Ambatana/src/ViewControllers/P2PPayments/OfferStatus/Subviews/P2PPaymentsOfferStatusStepListView.swift
@@ -11,6 +11,13 @@ struct P2PPaymentsOfferStatusStepListState {
     enum CurrentStep {
         case completed(Int)
         case failed(Int)
+
+        var index: Int {
+            switch self {
+            case .completed(let index): return index
+            case .failed(let index): return index
+            }
+        }
     }
 
     let steps: [Step]
@@ -32,12 +39,8 @@ final class P2PPaymentsOfferStatusStepListView: UIView {
         return stackView
     }()
 
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.alwaysBounceVertical = true
-        return scrollView
-    }()
+    private var numberViews = [P2PPaymentsNumberView]()
+    private var lineViews = [P2PPaymentsStepLineView]()
 
     init() {
         super.init(frame: .zero)
@@ -48,26 +51,90 @@ final class P2PPaymentsOfferStatusStepListView: UIView {
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
     private func setup() {
-        addSubviewForAutoLayout(scrollView)
-        scrollView.addSubviewForAutoLayout(stepsStackView)
+        addSubviewForAutoLayout(stepsStackView)
         setupConstraints()
     }
 
     private func setupConstraints() {
-        scrollView.constraintToEdges(in: self)
         NSLayoutConstraint.activate([
-            stepsStackView.widthAnchor.constraint(equalTo: widthAnchor, constant: -48),
-            stepsStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            stepsStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            stepsStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 24),
-            stepsStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -24),
+            stepsStackView.topAnchor.constraint(equalTo: topAnchor),
+            stepsStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stepsStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
     }
 
     private func configureForCurrentState() {
+        removeOldViews()
         let stepViews = state.steps.map { P2PPaymentsOfferStatusStepView(state: $0) }
-        stepsStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
         stepsStackView.addArrangedSubviews(stepViews)
+        createNumberViews(stepViews: stepViews)
+        createLineViews()
+    }
+
+    private func removeOldViews() {
+        stepsStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        numberViews.forEach { $0.removeFromSuperview() }
+        lineViews.forEach { $0.removeFromSuperview() }
+        numberViews.removeAll()
+        lineViews.removeAll()
+    }
+
+    private func createNumberViews(stepViews: [P2PPaymentsOfferStatusStepView]) {
+        for i in 0..<state.steps.count {
+            let numberState: P2PPaymentsNumberView.State = {
+                if i < state.currentStep.index {
+                    return .success
+                }
+                if i > state.currentStep.index {
+                    return .number(i + 1)
+                }
+                switch state.currentStep {
+                case .completed: return .success
+                case .failed: return .fail
+                }
+            }()
+            let numberView = P2PPaymentsNumberView(state: numberState)
+            numberViews.append(numberView)
+        }
+        addSubviewsForAutoLayout(numberViews)
+        if let firstNumberView = numberViews.first {
+            firstNumberView.setContentCompressionResistancePriority(.required, for: .horizontal)
+            NSLayoutConstraint.activate([
+                firstNumberView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                firstNumberView.trailingAnchor.constraint(equalTo: stepsStackView.leadingAnchor, constant: -24),
+            ])
+            numberViews.dropFirst().forEach { view in
+                NSLayoutConstraint.activate([
+                    view.centerXAnchor.constraint(equalTo: firstNumberView.centerXAnchor)
+                ])
+            }
+        }
+        zip(numberViews, stepViews).forEach { numberAndStep in
+            let (numberView, stepView) = numberAndStep
+            NSLayoutConstraint.activate([
+                numberView.topAnchor.constraint(equalTo: stepView.topAnchor),
+            ])
+        }
+    }
+
+    private func createLineViews() {
+        zip(numberViews.dropLast(), numberViews.dropFirst()).enumerated().forEach { index, firstAndSecond in
+            let (firstNumber, secondNumber) = firstAndSecond
+            let line = P2PPaymentsStepLineView()
+            let currentStepIndex = state.currentStep.index
+            switch (index, state.currentStep) {
+            case let (i, _) where i < currentStepIndex: line.completePercentage = 1
+            case let (i, .completed(currentIndex)) where i == currentIndex: line.completePercentage = 0.75
+            default: line.completePercentage = 0
+            }
+            lineViews.append(line)
+            addSubviewForAutoLayout(line)
+            NSLayoutConstraint.activate([
+                line.centerXAnchor.constraint(equalTo: firstNumber.centerXAnchor),
+                line.topAnchor.constraint(equalTo: firstNumber.bottomAnchor, constant: 4),
+                line.bottomAnchor.constraint(equalTo: secondNumber.topAnchor, constant: -4),
+            ])
+        }
     }
 }
 
