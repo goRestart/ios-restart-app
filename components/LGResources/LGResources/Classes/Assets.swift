7ee4c308a9f8f49834bd2998e71afe75407e31cd
Julian Oliver <juolgon@gmail.com>
2018-09-11 15:58:51 +0200

    Add check asset

--- components/LGResources/LGResources/Classes/Assets.swift
+++ components/LGResources/LGResources/Classes/Assets.swift
@@ -920,6 +920,7 @@ extension R {
     }
     public enum P2PPayments {
       public static let close = ImageAsset(name: "close")
+      public static let icCheck = ImageAsset(name: "ic_check")
       public static let icTrust = ImageAsset(name: "ic_trust")
       public static let onboardingStep1 = ImageAsset(name: "onboardingStep1")
       public static let onboardingStep2 = ImageAsset(name: "onboardingStep2")
@@ -930,6 +931,7 @@ extension R {
       ]
       public static let allImages: [ImageAsset] = [
         close,
+        icCheck,
         icTrust,
         onboardingStep1,
         onboardingStep2,
