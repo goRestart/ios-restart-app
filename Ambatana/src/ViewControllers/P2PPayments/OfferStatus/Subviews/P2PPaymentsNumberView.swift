530e5f84e525053c0acf0c073765464b59c96bf2
Julian Oliver <juolgon@gmail.com>
2018-09-11 23:00:03 +0200

    Modify number view to show succes and fail

--- Ambatana/src/ViewControllers/P2PPayments/OfferStatus/Subviews/P2PPaymentsNumberView.swift
+++ Ambatana/src/ViewControllers/P2PPayments/OfferStatus/Subviews/P2PPaymentsNumberView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import LGComponents
 
 final class P2PPaymentsNumberView: UIView {
     enum State {
@@ -7,21 +8,25 @@ final class P2PPaymentsNumberView: UIView {
         case fail
     }
 
-    var state: State = .number(0)
-
     private let numberLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.systemHeavyFont(size: 12)
+        label.textColor = UIColor.primaryColor
+        label.textAlignment = .center
         return label
     }()
 
     private let iconImageView: UIImageView = {
         let iconImageView = UIImageView()
+        iconImageView.tintColor = UIColor.white
         return iconImageView
     }()
 
     private let backgrounView: UIView = UIView()
+    private let state: State
 
-    init() {
+    init(state: State) {
+        self.state = state
         super.init(frame: .zero)
         setup()
     }
@@ -31,15 +36,56 @@ final class P2PPaymentsNumberView: UIView {
 
     private func setup() {
         addSubviewsForAutoLayout([numberLabel, iconImageView])
-        layer.borderWidth = 4
-        layer.borderColor = UIColor.black.cgColor
-        layer.cornerRadius = 16
-        backgroundColor = UIColor.red
+        layer.borderColor = UIColor.grayRegular.cgColor
+        clipsToBounds = true
+        setupConstraints()
+        configureForCurrentState()
+    }
+
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
+            numberLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            numberLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            iconImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            iconImageView.widthAnchor.constraint(equalToConstant: 18),
+            iconImageView.heightAnchor.constraint(equalToConstant: 18),
         ])
     }
 
+    private func configureForCurrentState() {
+        switch state {
+        case .number(let number):
+            numberLabel.text = "\(number)"
+            layer.borderWidth = 4
+            layer.cornerRadius = 16
+            backgroundColor = UIColor.white
+            numberLabel.isHidden = false
+            iconImageView.isHidden = true
+        case .success:
+            iconImageView.image = R.Asset.P2PPayments.icCheck.image
+            layer.borderWidth = 0
+            layer.cornerRadius = 14
+            backgroundColor = UIColor.p2pPaymentsPositive
+            numberLabel.isHidden = true
+            iconImageView.isHidden = false
+        case .fail:
+            iconImageView.image = R.Asset.P2PPayments.close.image.withRenderingMode(.alwaysTemplate)
+            layer.borderWidth = 0
+            layer.cornerRadius = 14
+            backgroundColor = UIColor.primaryColor
+            numberLabel.isHidden = true
+            iconImageView.isHidden = false
+        }
+    }
+
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 32, height: 32)
+        switch state {
+        case .number:
+            return CGSize(width: 32, height: 32)
+        case .success, .fail:
+            return CGSize(width: 28, height: 28)
+        }
     }
 }
