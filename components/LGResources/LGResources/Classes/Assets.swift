e9cb0590941f4c204013d496a647a039cee20077
Julian Oliver <juolgon@gmail.com>
2018-09-12 10:45:11 +0200

    Add P2P payment asset to the project

--- components/LGResources/LGResources/Classes/Assets.swift
+++ components/LGResources/LGResources/Classes/Assets.swift
@@ -994,6 +994,7 @@ extension R {
     }
     public enum P2PPayments {
       public static let close = ImageAsset(name: "close")
+      public static let icError = ImageAsset(name: "ic_Error")
       public static let icCheck = ImageAsset(name: "ic_check")
       public static let icTrust = ImageAsset(name: "ic_trust")
       public static let onboardingStep1 = ImageAsset(name: "onboardingStep1")
@@ -1005,6 +1006,7 @@ extension R {
       ]
       public static let allImages: [ImageAsset] = [
         close,
+        icError,
         icCheck,
         icTrust,
         onboardingStep1,
