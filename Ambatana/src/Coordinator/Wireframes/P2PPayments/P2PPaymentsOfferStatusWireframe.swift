9669ba6205a799c4ee122b5667040ddd4bc24115
Julian Oliver <juolgon@gmail.com>
2018-09-12 11:26:37 +0200

    Update builder and wireframe to create get code view model and controller

--- Ambatana/src/Coordinator/Wireframes/P2PPayments/P2PPaymentsOfferStatusWireframe.swift
+++ Ambatana/src/Coordinator/Wireframes/P2PPayments/P2PPaymentsOfferStatusWireframe.swift
@@ -3,16 +3,26 @@ import LGCoreKit
 
 protocol P2PPaymentsOfferStatusNavigator {
     func close()
+    func openGetPayCode()
 }
 
 final class P2PPaymentsOfferStatusWireframe: P2PPaymentsOfferStatusNavigator {
+    private let offerId: String
     private weak var navigationController: UINavigationController?
 
-    init(navigationController: UINavigationController) {
+    init(offerId: String,
+         navigationController: UINavigationController) {
+        self.offerId = offerId
         self.navigationController = navigationController
     }
 
     func close() {
         navigationController?.dismiss(animated: true)
+    }
+
+    func openGetPayCode() {
+        guard let nc = navigationController else { return }
+        let vc = P2PPaymentsOfferStatusBuilder.standard(nc: nc).buildGetPayCode(offerId: offerId)
+        nc.pushViewController(vc, animated: true)
     }
 }
