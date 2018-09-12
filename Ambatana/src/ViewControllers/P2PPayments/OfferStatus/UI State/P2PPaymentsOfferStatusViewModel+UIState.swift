7c7f2898b9e5517286522817175bfbcc46de9cac
Julian Oliver <juolgon@gmail.com>
2018-09-12 05:47:52 +0200

    Add more details for the seller offer status

--- Ambatana/src/ViewControllers/P2PPayments/OfferStatus/UI State/P2PPaymentsOfferStatusViewModel+UIState.swift
+++ Ambatana/src/ViewControllers/P2PPayments/OfferStatus/UI State/P2PPaymentsOfferStatusViewModel+UIState.swift
@@ -2,11 +2,20 @@ import Foundation
 import LGCoreKit
 import LGComponents
 
+// TODO: @juolgon Localize all texts
+
 extension P2PPaymentsOfferStatusViewModel {
+    private static let currencyHelper = Core.currencyHelper
+    private static let percentageFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        return formatter
+    }()
+
     enum UIState {
         case loading
         case buyerInfoLoaded(offer: P2PPaymentOffer, listing: Listing)
-        case sellerInfoLoaded(offer: P2PPaymentOffer)
+        case sellerInfoLoaded(offer: P2PPaymentOffer, listing: Listing, buyer: User)
 
         var showLoadingIndicator: Bool {
             switch self {
@@ -35,14 +44,25 @@ extension P2PPaymentsOfferStatusViewModel {
                 return nil
             case .buyerInfoLoaded(offer: let offer, listing: _):
                 return offer
-            case .sellerInfoLoaded(offer: let offer):
+            case .sellerInfoLoaded(offer: let offer, listing: _, buyer: _):
                 return offer
             }
         }
 
         var listing: Listing? {
-            guard case let .buyerInfoLoaded(offer: _, listing: listing) = self else { return nil }
-            return listing
+            switch self {
+            case .loading:
+                return nil
+            case .buyerInfoLoaded(offer: _, listing: let listing):
+                return listing
+            case .sellerInfoLoaded(offer: _, listing: let listing, buyer: _):
+                return listing
+            }
+        }
+
+        var buyer: User? {
+            guard case let .sellerInfoLoaded(offer: _, listing: _, buyer: buyer) = self else { return nil }
+            return buyer
         }
 
         var listingTitle: String? {
@@ -51,6 +71,46 @@ extension P2PPaymentsOfferStatusViewModel {
 
         var listingImageURL: URL? {
             return listing?.thumbnail?.fileURL
+        }
+
+        var sellerHeaderImageURL: URL? {
+            return buyer?.avatar?.fileURL
+        }
+
+        var sellerHeaderTitle: String? {
+            guard let buyer = buyer, let offer = offer, let listing = listing else { return nil }
+            let name = buyer.name ?? ""
+            let amount = (offer.fees.amount as NSDecimalNumber).doubleValue
+            let formattedPrice = currencyHelper.formattedAmountWithCurrencyCode(listing.currency.code, amount: amount)
+            let listingName = listing.name ?? ""
+            return "\(name), offering \(formattedPrice) for \"\(listingName)\""
+        }
+
+        var netAmountText: String? {
+            guard let fees = offer?.fees else { return nil }
+            let amount = (fees.amount as NSDecimalNumber).doubleValue
+            let currency = fees.currency
+            return currencyHelper.formattedAmountWithCurrencyCode(currency.code, amount: amount)
+        }
+
+        var feeAmountText: String? {
+            guard let fees = offer?.fees else { return nil }
+            let amount = (fees.serviceFee as NSDecimalNumber).doubleValue
+            let currency = fees.currency
+            return currencyHelper.formattedAmountWithCurrencyCode(currency.code, amount: amount)
+        }
+
+        var grossAmountText: String? {
+            guard let fees = offer?.fees else { return nil }
+            let amount = (fees.total as NSDecimalNumber).doubleValue
+            let currency = fees.currency
+            return currencyHelper.formattedAmountWithCurrencyCode(currency.code, amount: amount)
+        }
+
+        var feePercentageText: String? {
+            guard let fees = offer?.fees else { return nil }
+            let percentage = fees.serviceFeePercentage / 100
+            return P2PPaymentsOfferStatusViewModel.percentageFormatter.string(from: NSNumber(value: percentage))
         }
 
         var actionButtonTitle: String? {
@@ -75,7 +135,7 @@ extension P2PPaymentsOfferStatusViewModel {
         }
 
         var sellerStepList: P2PPaymentsOfferStatusStepListState? {
-            guard case let .sellerInfoLoaded(offer: offer) = self else { return nil }
+            guard case let .sellerInfoLoaded(offer: offer, listing: _, buyer: _) = self else { return nil }
             return P2PPaymentsOfferStatusStepListState.sellerStepList(status: offer.status)
         }
     }
