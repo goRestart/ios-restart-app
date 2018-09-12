ccc73cfa2202cb228a6df823f42766a3bf2982ca
Julian Oliver <juolgon@gmail.com>
2018-09-12 11:25:19 +0200

    Add P2P payments yellow color

--- components/LGShared/LGShared/Classes/iOS/UIKit/UIColor+LG.swift
+++ components/LGShared/LGShared/Classes/iOS/UIKit/UIColor+LG.swift
@@ -354,4 +354,5 @@ public extension UIColor {
 
 public extension UIColor {
     static let p2pPaymentsPositive = UIColor(rgb: 0x00A8A8) // (0, 168, 168)
+    static let p2pPaymentsWarning = UIColor(rgb: 0xffd200) // (255, 210, 0)
 }
