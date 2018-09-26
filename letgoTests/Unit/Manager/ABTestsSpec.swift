783096cfb69da45ef371fd744e38cc89660e174b
Álvaro Murillo del Puerto <alvaromurillop@gmail.com>
2018-09-26 09:11:53 +0200

    Fix tests

--- letgoTests/Unit/Manager/ABTestsSpec.swift
+++ letgoTests/Unit/Manager/ABTestsSpec.swift
@@ -46,7 +46,7 @@ final class ABTestsSpec: QuickSpec {
                     sut.registerVariables()
                 }
                 it("registers all the variables") {
-                    expect(syncer.syncedCount) == 72
+                    expect(syncer.syncedCount) == 73
                 }
             }
             context("registering all the variables") {
