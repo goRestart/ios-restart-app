7e262e593ce9aedfd5b623690af81e3d7dda5a12
Albert Hernández López <albert@letgo.com>
2018-09-28 13:22:36 +0200

    add listing sell complete 24h logic into middleware

--- Ambatana/src/Manager/Tracking/Middleware/AnalyticsSell24hMiddleware.swift
+++ Ambatana/src/Manager/Tracking/Middleware/AnalyticsSell24hMiddleware.swift
@@ -1,17 +1,28 @@
+import LGCoreKit
+
 final class AnalyticsSell24hMiddleware: AnalyticsMiddleware {
-    private let keyValueStorage: KeyValueStorage
+    private static let threshold = TimeInterval.make(days: 1)
+    private let keyValueStorage: KeyValueStorageable
 
 
     // MARK: - Lifecycle
 
-    init(keyValueStorage: KeyValueStorage) {
+    init(keyValueStorage: KeyValueStorageable) {
         self.keyValueStorage = keyValueStorage
     }
 
 
     // MARK: - AnalyticsMiddleware
 
-    func process(event: TrackerEvent, trackNewEvent: (TrackerEvent) -> ()) {
+    func process(event: TrackerEvent,
+                 trackNewEvent: (TrackerEvent) -> ()) {
+        guard event.name == .listingSellComplete else { return }
+        guard let firstOpenDate = keyValueStorage[.firstRunDate],
+            Date().timeIntervalSince(firstOpenDate) <= AnalyticsSell24hMiddleware.threshold &&
+            !keyValueStorage.userTrackingProductSellComplete24hTracked else { return }
 
+        let listing = Listing.makeMock()
+        let event = TrackerEvent.listingSellComplete24h(listing)
+        trackNewEvent(event)
     }
 }
