092280ebfe9f817d60fcd1f360349a85973807df
isaacroldan <isaac.roldan@gmail.com>
2018-10-08 13:17:58 +0200

    Update ABTest specs

--- letgoTests/Unit/Manager/ABTestsSpec.swift
+++ letgoTests/Unit/Manager/ABTestsSpec.swift
@@ -42,7 +42,7 @@ final class ABTestsSpec: QuickSpec {
                     sut.registerVariables()
                 }
                 it("registers all the variables") {
-                    expect(syncer.syncedCount) == 77
+                    expect(syncer.syncedCount) == 76
                 }
             }
             context("registering all the variables") {
@@ -253,7 +253,7 @@ final class ABTestsSpec: QuickSpec {
 
             context("registering all users variables") {
                 it("the users int variable registered are correct") {
-                    expect(users.intVariables.count) == 8
+                    expect(users.intVariables.count) == 7
                 }
 
                 it("the users bool variable registered are correct") {
@@ -276,8 +276,8 @@ final class ABTestsSpec: QuickSpec {
                     syncer.sync(variables: users.stringVariables)
                     syncer.sync(variables: users.floatVariables)
                 }
-                it("the variables registered are 8") {
-                    expect(syncer.syncedCount) == 8
+                it("the variables registered are 7") {
+                    expect(syncer.syncedCount) == 7
                 }
             }
 
