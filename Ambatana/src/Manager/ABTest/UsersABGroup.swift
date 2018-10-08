5dfd7796c310e14e28f22cd987def4468f690092
Sergi Gracia <sergigram@gmail.com>
2018-10-08 10:52:26 +0200

    Don't add ARS 1.2 and 1.3 A/B Tests to leanplum until this is ready to be tested and merged

--- Ambatana/src/Manager/ABTest/UsersABGroup.swift
+++ Ambatana/src/Manager/ABTest/UsersABGroup.swift
@@ -48,9 +48,7 @@ struct UsersABGroup: ABGroupType {
                                          offensiveReportAlert,
                                          reportingFostaSesta,
                                          community,
-                                         advancedReputationSystem11,
-                                         advancedReputationSystem12,
-                                         advancedReputationSystem13])
+                                         advancedReputationSystem11])
     }
 
     static func make() -> UsersABGroup {
